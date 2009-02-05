097e5c62b21a6025475f6de21dafa080f66950ba
rdruelle <rdruelle@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-02-05 13:34:10 +0000

    Add an identifiant for the Entity Interet. We moved META-INF in the src folder with the modified file persistence.xml.
    git-svn-id: http://fsnet.googlecode.com/svn@7 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet.entities/src/fr/univartois/ili/fsnet/entities/Interet.java
+++ trunk/fr.univartois.ili.fsnet.entities/src/fr/univartois/ili/fsnet/entities/Interet.java
@@ -2,55 +2,60 @@ package fr.univartois.ili.fsnet.entities;
 
 import java.util.List;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 public class Interet {
 
-	@OneToOne(mappedBy="groupeDinteret")
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+	@OneToOne(mappedBy = "groupeDinteret")
 	private GroupeDInteret ungroupe;
-	@ManyToMany(mappedBy="entite")
+	@ManyToMany(mappedBy = "entite")
 	private List<EntiteSociale> lesEntites;
 	private String nomInteret;
-	
-	public Interet(GroupeDInteret ungroupe, List<EntiteSociale> lesEntites, String nomInteret) {
+
+	public Interet(GroupeDInteret ungroupe, List<EntiteSociale> lesEntites,
+			String nomInteret) {
 		this.ungroupe = ungroupe;
 		this.lesEntites = lesEntites;
 		this.nomInteret = nomInteret;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public GroupeDInteret getUngroupe() {
 		return ungroupe;
 	}
 
-
 	public void setUngroupe(GroupeDInteret ungroupe) {
 		this.ungroupe = ungroupe;
 	}
-
 
 	public List<EntiteSociale> getLesEntites() {
 		return lesEntites;
 	}
 
-
 	public void setLesEntites(List<EntiteSociale> lesEntites) {
 		this.lesEntites = lesEntites;
 	}
-
 
 	public String getNomInteret() {
 		return nomInteret;
 	}
 
-
 	public void setNomInteret(String nomInteret) {
 		this.nomInteret = nomInteret;
 	}
-	
-	
-	
-	
-	
+
 }
