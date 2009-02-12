a027e0755710a3b25f4404a7185c7cffd742aa82
rdruelle <rdruelle@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-02-12 11:19:39 +0000

    Adding JPA annotation @ Inheritance (strategy = InheritanceType.JOINED) for Class Interaction. Each child class contains the attributes defined in the class. The specialization relationship is through a foreign key from mother table to child table.
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@25 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet.entities/src/fr/univartois/ili/fsnet/entities/Interaction.java
+++ fr.univartois.ili.fsnet.entities/src/fr/univartois/ili/fsnet/entities/Interaction.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Interaction {
 	
 	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE)
