6452070b1002e8b4a740a344ad27c4ebfd6749ad
marie.zinkiewicz <marie.zinkiewicz@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-02-13 20:08:30 +0000

    Getters the IliForumFacade is good in theory
    git-svn-id: http://fsnet.googlecode.com/svn@34 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet.facade.jforum/src/fr/univartois/ili/fsnet/facade/iliforum/IliForumFacade.java
+++ trunk/fr.univartois.ili.fsnet.facade.jforum/src/fr/univartois/ili/fsnet/facade/iliforum/IliForumFacade.java
@@ -3,6 +3,11 @@ package fr.univartois.ili.fsnet.facade.iliforum;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import fr.univartois.ili.fsnet.entities.EntiteSociale;
 import fr.univartois.ili.fsnet.entities.Hub;
 import fr.univartois.ili.fsnet.entities.Message;
@@ -31,14 +36,24 @@ public class IliForumFacade implements ForumFacade {
 
 	@Override
 	public List<Hub> getListHub() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+        EntityManager em = factory.createEntityManager();
+		Query query=em.createQuery("SELECT h FROM Hub h ");
+		List<Hub> mesHubs;
+		mesHubs = (List<Hub>) query.getResultList();
+		return mesHubs;
 	}
 
 	@Override
 	public List<Hub> getListHub(Date dateBegin, Date dateEnd) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+        EntityManager em = factory.createEntityManager();
+		Query query=em.createQuery("SELECT h FROM Hub h WHERE h.dateCreation>?1 and h.dateCreation<?2");
+		query.setParameter(1, dateBegin);
+		query.setParameter(2, dateEnd);
+		List<Hub> mesHubs;
+		mesHubs = (List<Hub>) query.getResultList();
+		return mesHubs;
 	}
 
 	@Override
@@ -49,20 +64,30 @@ public class IliForumFacade implements ForumFacade {
 
 	@Override
 	public List<Message> getListMessage() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+        EntityManager em = factory.createEntityManager();
+		Query query=em.createQuery("SELECT m FROM Message m ");
+		List<Message> mesMess;
+		mesMess = (List<Message>) query.getResultList();
+		return mesMess;
 	}
 
 	@Override
 	public List<Message> getListMessage(Date dateBegin, Date dateEnd) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+        EntityManager em = factory.createEntityManager();
+		Query query=em.createQuery("SELECT m FROM Message m WHERE m.dateMessage>?1 AND m.dateMessage<?2");
+		query.setParameter(1, dateBegin);
+		query.setParameter(2, dateEnd);
+		List<Message> mesMess;
+		mesMess = (List<Message>) query.getResultList();
+		return mesMess;
 	}
 
 	@Override
 	public List<Message> getListMessageByEntiteSocial(
 			EntiteSociale entiteSociale) {
-		// TODO Auto-generated method stub
+		entiteSociale.getLesMessages();
 		return null;
 	}
 
@@ -74,32 +99,39 @@ public class IliForumFacade implements ForumFacade {
 
 	@Override
 	public List<Message> getListMessageByTopic(Topic topic) {
-		// TODO Auto-generated method stub
-		return null;
+		return topic.getLesMessages();		
 	}
 
 	@Override
 	public List<Topic> getListTopic() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+        EntityManager em = factory.createEntityManager();
+		Query query=em.createQuery("SELECT t FROM Topic t");
+		List<Topic> mesTopics;
+		mesTopics = (List<Topic>) query.getResultList();
+		return mesTopics;
 	}
 
 	@Override
 	public List<Topic> getListTopic(Date dateBegin, Date dateEnd) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+        EntityManager em = factory.createEntityManager();
+		Query query=em.createQuery("SELECT t FROM Topic t WHERE dateSujet>?1 AND dateSujet<?2");
+		query.setParameter(1, dateBegin);
+		query.setParameter(2, dateEnd);
+		List<Topic> mesTopics;
+		mesTopics = (List<Topic>) query.getResultList();
+		return mesTopics;
 	}
 
 	@Override
 	public List<Topic> getListTopicByEntiteSociale(EntiteSociale entiteSocial) {
-		// TODO Auto-generated method stub
-		return null;
+		return entiteSocial.getLesTopics();
 	}
 
 	@Override
 	public List<Topic> getListTopicByHub(Hub hub) {
-		// TODO Auto-generated method stub
-		return null;
+		return hub.getLesTopics();
 	}
 
 	@Override
