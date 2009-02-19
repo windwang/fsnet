b887e69ff5a2b6fa6e4b9537eedca43e33640f50
mariezinkiewicz <mariezinkiewicz@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-02-19 15:13:42 +0000

    git-svn-id: http://fsnet.googlecode.com/svn/trunk@83 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet.maquette/src/fr/univartois/ili/fsnet/facade/iliforum/servlet/CreateTopic.java
+++ fr.univartois.ili.fsnet.maquette/src/fr/univartois/ili/fsnet/facade/iliforum/servlet/CreateTopic.java
@@ -3,16 +3,11 @@ package fr.univartois.ili.fsnet.facade.iliforum.servlet;
 import java.io.IOException;
 import java.util.Date;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.sun.xml.internal.bind.CycleRecoverable.Context;
 
 import fr.univartois.ili.fsnet.entities.Hub;
 import fr.univartois.ili.fsnet.entities.Message;
