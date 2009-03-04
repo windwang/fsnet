96d0293bc2a732a9c91e6bee549700bdab241d58
d.stackowiak <d.stackowiak@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-03-04 23:28:51 +0000

    Add "lastMessage" attribute in Hub/TopicDTO class and change the udpate() method.
    git-svn-id: http://fsnet.googlecode.com/svn@144 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet.facade.iliforumtags/src/fr/univartois/ili/fsnet/facade/iliforumtags/TopicDTO.java
+++ trunk/fr.univartois.ili.fsnet.facade.iliforumtags/src/fr/univartois/ili/fsnet/facade/iliforumtags/TopicDTO.java
@@ -1,40 +1,56 @@
 package fr.univartois.ili.fsnet.facade.iliforumtags;
 
+import java.util.List;
+
+import fr.univartois.ili.fsnet.entities.Message;
 import fr.univartois.ili.fsnet.entities.Topic;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.IliForumFacade;
 
 public class TopicDTO {
 
-	private Topic topic;
+    private Topic topic;
 
-	private int nbMessage;
+    private int nbMessage;
 
-	public TopicDTO(Topic top) {
-		this.topic = top;
+    private Message lastMessage;
 
-		update();
-	}
+    public Message getLastMessage() {
+	return lastMessage;
+    }
 
-	private void update() {
-		this.nbMessage = topic.getLesMessages().size();
-	}
+    public void setLastMessage(Message lastMessage) {
+	this.lastMessage = lastMessage;
+    }
 
-	public Topic getTopic() {
-		return topic;
-	}
+    public TopicDTO(Topic top) {
+	this.topic = top;
+	update();
 
-	public void setTopic(Topic topic) {
-		this.topic = topic;
-		update();
-	}
+    }
 
-	public int getNbMessage() {
-		update();
-		return nbMessage;
-	}
+    private void update() {
+	IliForumFacade iff = new IliForumFacade();
+	List<Message> lMessage = iff.getListMessageByTopic(topic);
+	this.nbMessage = lMessage.size();
+	if (!lMessage.isEmpty())
+	    this.lastMessage = lMessage.get(lMessage.size() - 1);
 
-	public void setNbMessage(int nbMessage) {
-		this.nbMessage = nbMessage;
-		update();
-	}
+    }
+
+    public Topic getTopic() {
+	return topic;
+    }
+
+    public void setTopic(Topic topic) {
+	this.topic = topic;
+    }
+
+    public int getNbMessage() {
+	return nbMessage;
+    }
+
+    public void setNbMessage(int nbMessage) {
+	this.nbMessage = nbMessage;
+    }
 
 }
