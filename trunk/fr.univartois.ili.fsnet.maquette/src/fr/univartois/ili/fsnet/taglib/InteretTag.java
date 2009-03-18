ba3175a246cb192eadd9cc8c3eea6d8d0c7ffd45
jbouwy <jbouwy@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-03-18 14:59:27 +0000

    When viewing the profile, the interest already chosen are marked automatically
    git-svn-id: http://fsnet.googlecode.com/svn@259 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet.maquette/src/fr/univartois/ili/fsnet/taglib/InteretTag.java
+++ trunk/fr.univartois.ili.fsnet.maquette/src/fr/univartois/ili/fsnet/taglib/InteretTag.java
@@ -10,6 +10,7 @@ import javax.persistence.Query;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
+import fr.univartois.ili.fsnet.entities.EntiteSociale;
 import fr.univartois.ili.fsnet.entities.Interet;
 
 /**
@@ -22,26 +23,47 @@ public class InteretTag extends TagSupport {
 
 	private static final String FIND_ALL = "SELECT i FROM Interet i ORDER BY i.nomInteret ASC";
 
+	private static final String FIND_ENTITE = "SELECT e FROM EntiteSociale e WHERE e.id=?1";
+
 	private static final String DATABASE_NAME = "fsnetjpa";
 
 	private Iterator<Interet> it;
 
+	private List<Interet> interetsPerso;
+
 	private String var;
+
+	private Integer idLogin;
 
 	public void setVar(final String var) {
 		this.var = var;
+	}
+
+	public Integer getIdLogin() {
+		return idLogin;
+	}
+
+	public void setIdLogin(Integer idLogin) {
+		this.idLogin = idLogin;
 	}
 
 	public int doStartTag() throws JspException {
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory(DATABASE_NAME);
 		EntityManager em = factory.createEntityManager();
-		Query query;
+		Query queryInt, queryEnt;
 		List<Interet> lesInterets;
 
-		query = em.createQuery(FIND_ALL);
-		lesInterets = query.getResultList();
+		queryInt = em.createQuery(FIND_ALL);
+		lesInterets = queryInt.getResultList();
 		it = lesInterets.iterator();
+
+		queryEnt = em.createQuery(FIND_ENTITE);
+		queryEnt.setParameter(1, idLogin.intValue());
+
+		EntiteSociale ent = (EntiteSociale) queryEnt.getSingleResult();
+		interetsPerso = ent.getLesinterets();
+
 		if (it != null && it.hasNext()) {
 			updateContext(it.next());
 			return EVAL_BODY_INCLUDE;
@@ -51,6 +73,11 @@ public class InteretTag extends TagSupport {
 	}
 
 	private void updateContext(Interet interet) {
+		String checked = "";
+		if (interetsPerso.contains(interet)) {
+			checked = "checked";
+		}
+		pageContext.setAttribute("check", checked);
 		pageContext.setAttribute(var, interet);
 	}
 
@@ -64,6 +91,7 @@ public class InteretTag extends TagSupport {
 
 	public int doEndTag() throws JspException {
 		pageContext.removeAttribute(var);
+		pageContext.removeAttribute("check");
 		return super.doEndTag();
 	}
 }
