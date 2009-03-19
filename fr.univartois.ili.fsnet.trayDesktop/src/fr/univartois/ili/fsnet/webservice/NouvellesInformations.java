031d693dfa630b180072a3731c758acb23ad05c4
mouez.hammadi <mouez.hammadi@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-03-19 22:25:33 +0000

    add a web service client, it display a number of new added  event in the current day.
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@314 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fr.univartois.ili.fsnet.trayDesktop/src/fr/univartois/ili/fsnet/webservice/NouvellesInformations.java
@@ -0,0 +1,12 @@
+/**
+ * NouvellesInformations.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package fr.univartois.ili.fsnet.webservice;
+
+public interface NouvellesInformations extends java.rmi.Remote {
+    public int getEvenement() throws java.rmi.RemoteException;
+}
