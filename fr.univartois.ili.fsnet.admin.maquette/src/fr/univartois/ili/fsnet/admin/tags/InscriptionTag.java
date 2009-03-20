ae016258e405ce238228cbf2e6c4e297da7b9bfa
rdruelle <rdruelle@07c314b0-eee8-11dd-b478-27b5dcd53177>
2009-03-20 02:22:04 +0000

    + Removed code that does nothing more. + Removed unused imports.
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@323 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet.admin.maquette/src/fr/univartois/ili/fsnet/admin/tags/InscriptionTag.java
+++ fr.univartois.ili.fsnet.admin.maquette/src/fr/univartois/ili/fsnet/admin/tags/InscriptionTag.java
@@ -3,13 +3,10 @@
  */
 package fr.univartois.ili.fsnet.admin.tags;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
