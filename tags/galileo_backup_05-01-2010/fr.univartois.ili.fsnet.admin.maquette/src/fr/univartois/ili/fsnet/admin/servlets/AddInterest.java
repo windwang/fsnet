c75bcf913bccfee16750d2c2e6af9490dc083c08
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-05 19:42:43 +0000

    Création du tag de sauvegarde de la promo Galileo
    git-svn-id: http://fsnet.googlecode.com/svn@360 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ tags/galileo_backup_05-01-2010/fr.univartois.ili.fsnet.admin.maquette/src/fr/univartois/ili/fsnet/admin/servlets/AddInterest.java
@@ -0,0 +1,115 @@
+package fr.univartois.ili.fsnet.admin.servlets;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.univartois.ili.fsnet.entities.Interet;
+
+/**
+ * @author romuald druelle Servlet. implementation class AddInteret
+ */
+public class AddInterest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final String DATABASE_NAME = "fsnetjpa";
+
+	private EntityManagerFactory factory;
+
+	private EntityManager em;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddInterest() {
+		super();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		factory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+		em = factory.createEntityManager();
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		if (em != null) {
+			em.close();
+		}
+		if (factory != null) {
+			factory.close();
+		}
+	}
+
+	/**
+	 * A method that allow to persist an interest.
+	 * 
+	 * @param interet
+	 */
+	private void persist(Interet interet) {
+		em.getTransaction().begin();
+		em.persist(interet);
+		em.getTransaction().commit();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Interet interet = null;
+		int length = 0;
+		String redirection = request.getParameter("redirection");
+		String[] valuesInterests = (String[]) request.getSession()
+				.getAttribute("interets[]");
+
+		if (valuesInterests != null) {
+			length = valuesInterests.length;
+		}
+
+		interet = (Interet) request.getSession().getAttribute("interet");
+
+		persist(interet);
+
+		if (length != 0) {
+			for (int i = 0; i < length; i++) {
+				System.out.println("Pour interet " + i + " == est vide ? "
+						+ valuesInterests[i].isEmpty());
+				if(!valuesInterests[i].isEmpty()){
+					interet = new Interet();
+					interet.setNomInteret(valuesInterests[i]);
+					persist(interet);
+				}
+			}
+		}
+
+		RequestDispatcher disp = getServletContext()
+				.getRequestDispatcher(
+						"/"
+								+ redirection
+								+ "?interest=current&showHide=show&deploy=[-]&titleDeploy=R%E9duire la liste");
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
