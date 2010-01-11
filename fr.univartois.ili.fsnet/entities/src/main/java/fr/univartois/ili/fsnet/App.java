17f29e9077ad8c503bf0cd67416772f15ac493b3
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-11 09:06:06 +0000

    cleaned trunk/
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@372 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fr.univartois.ili.fsnet/entities/src/main/java/fr/univartois/ili/fsnet/App.java
@@ -0,0 +1,13 @@
+package fr.univartois.ili.fsnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
