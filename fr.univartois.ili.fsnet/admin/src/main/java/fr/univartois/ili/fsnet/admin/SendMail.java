f806501977c57ed06559a8e2e767abbb75009131
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-11 20:35:35 +0000

    Code for web projetcs formatted and corriged when necessary, few modifications on pom.xml
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@387 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/SendMail.java
+++ fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/SendMail.java
@@ -6,89 +6,86 @@ import javax.mail.*;
 import javax.mail.internet.*;
 
 public class SendMail {
-	
-	private static final String TIMEOUT = "1000";
-	private Properties props;
-	private Authenticator auth;
-	private String addressHost;
-	private String pwdAddressHost;
 
-	public SendMail(){}
-	
-	public SendMail(String serverSmtp,String addressHost, String pwdAddressHost, String port) {
-		// Setting the host address and smtp address
-		props = new Properties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", serverSmtp);
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", port);
-		props.put("mail.smtp.timeout", TIMEOUT);
-		this.addressHost = addressHost;
-		this.pwdAddressHost = pwdAddressHost;
-		// TODO search reason !
+    private static final String TIMEOUT = "1000";
+    private Properties props;
+    private Authenticator auth;
+    private String addressHost;
+    private String pwdAddressHost;
+
+    public SendMail() {
+    }
+
+    public SendMail(String serverSmtp, String addressHost, String pwdAddressHost, String port) {
+        // Setting the host address and smtp address
+        props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", serverSmtp);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.timeout", TIMEOUT);
+        this.addressHost = addressHost;
+        this.pwdAddressHost = pwdAddressHost;
+        // TODO search reason !
 		/*java.security.Security
-				.addProvider(new com.sun.net.ssl.internal.ssl.Provider());*/
+        .addProvider(new com.sun.net.ssl.internal.ssl.Provider());*/
 
-		auth = new AuthenticationSMTP();
+        auth = new AuthenticationSMTP();
 
-	}
-	
-	public String getAddressHost(){
-		return addressHost;
-	}
-	
-	public String getPwdAddressHost(){
-		return pwdAddressHost;
-	}
+    }
 
-	private Message createMessage(List<String> liste, String sujet,
-			String message) throws MessagingException {
-		Session session = Session.getDefaultInstance(props, auth);
+    public String getAddressHost() {
+        return addressHost;
+    }
 
-		session.setDebug(false);
+    public String getPwdAddressHost() {
+        return pwdAddressHost;
+    }
 
-		// creation message
-		Message msg = new MimeMessage(session);
+    private Message createMessage(List<String> liste, String sujet,
+            String message) throws MessagingException {
+        Session session = Session.getDefaultInstance(props, auth);
 
-		InternetAddress[] destination = new InternetAddress[liste.size()];
-		for (int i = 0; i < liste.size(); i++) {
-			destination[i] = new InternetAddress(liste.get(i));
-		}
-		msg.setRecipients(Message.RecipientType.TO, destination);
-		msg.setSubject(sujet);
-		msg.setContent(message, "text/plain");
-		return msg;
-	}
+        session.setDebug(false);
 
-	public void sendMessageWithAttachements(List<String> liste, String sujet, String message,
-			String file) throws MessagingException {
+        // creation message
+        Message msg = new MimeMessage(session);
 
-		Message msg = createMessage(liste, sujet, message);
-		msg.setFileName(file);
-		Transport.send(msg);
-	}
+        InternetAddress[] destination = new InternetAddress[liste.size()];
+        for (int i = 0; i < liste.size(); i++) {
+            destination[i] = new InternetAddress(liste.get(i));
+        }
+        msg.setRecipients(Message.RecipientType.TO, destination);
+        msg.setSubject(sujet);
+        msg.setContent(message, "text/plain");
+        return msg;
+    }
 
-	// **********************************************************************************
+    public void sendMessageWithAttachements(List<String> liste, String sujet, String message,
+            String file) throws MessagingException {
 
-	public void sendMessage(List<String> liste, String sujet, String message)
-			throws MessagingException {
+        Message msg = createMessage(liste, sujet, message);
+        msg.setFileName(file);
+        Transport.send(msg);
+    }
 
-		Message msg = createMessage(liste, sujet, message);
-		Transport.send(msg);
-	}
+    // **********************************************************************************
+    public void sendMessage(List<String> liste, String sujet, String message)
+            throws MessagingException {
 
-	// ***********************************************************************************
+        Message msg = createMessage(liste, sujet, message);
+        Transport.send(msg);
+    }
 
-	/**
-	 * a function that allows the authentication to the mail server
-	 */
-	private class AuthenticationSMTP extends javax.mail.Authenticator {
+    // ***********************************************************************************
+    /**
+     * a function that allows the authentication to the mail server
+     */
+    private class AuthenticationSMTP extends javax.mail.Authenticator {
 
-
-		public PasswordAuthentication getPasswordAuthentication() {
-			return new PasswordAuthentication(addressHost, pwdAddressHost);
-		}
-	}
-
+        public PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(addressHost, pwdAddressHost);
+        }
+    }
 }
--- fr.univartois.ili.fsnet/maquette/src/main/webapp/bas.jsp
+++ fr.univartois.ili.fsnet/maquette/src/main/webapp/bas.jsp
@@ -1,25 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+         pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<link rel="icon" type="image/png" href="images/favicon.ico" />
-<meta http-equiv="content-type"
-	content="application/xhtml+xml; charset=UTF-8" />
-<meta name="author" content="Luka Cvrk - www.solucija.com" />
-<meta name="description" content="Site Description" />
-<meta name="keywords" content="site, keywords" />
-<meta name="robots" content="index, follow" />
-<link rel="stylesheet" type="text/css" media="screen"
-	href="css/style.css" />
-<title>FSNet</title>
+    <head>
+        <link rel="icon" type="image/png" href="images/favicon.ico" />
+        <meta http-equiv="content-type"
+              content="application/xhtml+xml; charset=UTF-8" />
+        <meta name="author" content="Luka Cvrk - www.solucija.com" />
+        <meta name="description" content="Site Description" />
+        <meta name="keywords" content="site, keywords" />
+        <meta name="robots" content="index, follow" />
+        <link rel="stylesheet" type="text/css" media="screen"
+              href="css/style.css" />
+        <title>FSNet</title>
 
 
-</head>
-<body>
-<p id="ad">&nbsp;</p>
-<div class="promo" style="text-align: center">
-<div class="wrap">FSnet licence</div>
-</div>
-</body>
+    </head>
+    <body>
+        <p id="ad">&nbsp;</p>
+        <div class="promo" style="text-align: center">
+            <div class="wrap">FSnet licence</div>
+        </div>
+    </body>
 </html>
