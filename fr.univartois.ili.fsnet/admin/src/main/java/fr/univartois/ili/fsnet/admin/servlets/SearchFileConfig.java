cbce99f45f39ba272a71acfc4d533901069fa133
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-12 22:31:01 +0000

    Modified dependencies, web projects now runs on fresh Tomcat server (no need for libs)
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@398 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/servlets/SearchFileConfig.java
+++ fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/servlets/SearchFileConfig.java
@@ -13,50 +13,49 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class CreateFileConfig
  */
 public class SearchFileConfig extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	public static final String FILE_PATH = System.getProperty("user.home")
-			+ "/.fsnetadminrc";
+    private static final long serialVersionUID = 1L;
+    public static final String FILE_PATH = System.getProperty("user.home")
+            + "/.fsnetadminrc";
+    public static final String HOME = "/index.jsp?accueil=current";
+    public static final String OPTIONS = "/optionsrequired.jsp?options=current";
 
-	public static final String HOME = "/index.jsp?accueil=current";
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchFileConfig() {
+        super();
+    }
 
-	public static final String OPTIONS = "/optionsrequired.jsp?options=current";
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        String redirection = searchFile();
+        RequestDispatcher disp = getServletContext().getRequestDispatcher(
+                redirection);
+        disp.forward(request, response);
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public SearchFileConfig() {
-		super();
-	}
+    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String redirection = searchFile();
-		RequestDispatcher disp = getServletContext().getRequestDispatcher(
-				redirection);
-		disp.forward(request, response);
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-	private String searchFile() {
-		File file = new File(FILE_PATH);
-		if (!file.exists()) {
-			return OPTIONS;
-		}
-		return HOME;
-	}
-
+    private String searchFile() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return OPTIONS;
+        }
+        return HOME;
+    }
 }
--- fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/tags/InscriptionTag.java
+++ fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/tags/InscriptionTag.java
@@ -22,181 +22,170 @@ import fr.univartois.ili.fsnet.entities.Inscription;
  */
 public class InscriptionTag extends TagSupport {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private static final String FIND_ALL = "SELECT i FROM Inscription i ORDER BY i.entite.nom ASC";
+    private static final String FIND_LAST_BY_STATE = "SELECT i FROM Inscription i WHERE i.etat = ?1";
+    private static final String FIND_BY_ENTITY = "SELECT i FROM Inscription i WHERE i.entite.id = ?1 ";
+    private static final String DATABASE_NAME = "fsnetjpa";
+    private static final String WAITING = "En attente d'inscription";
+    private static final String REGISTERED = "Inscrit";
+    private static final String CONDITION_AWAITING_REGISTRATION = "ORDER BY i.entite.nom ASC";
+    private static final String CONDITION_REGISTERED = "ORDER BY i.id DESC";
+    private static final int MAX_MEMBERS = 7;
+    private static final int SIZE = 10;
+    private Iterator<Inscription> it;
+    private String var;
+    private String etat;
+    private String entite;
+    private String parametre;
+    private String filtre;
 
-	private static final String FIND_ALL = "SELECT i FROM Inscription i ORDER BY i.entite.nom ASC";
+    public void setFiltre(final String filtre) {
+        this.filtre = filtre;
+    }
 
-	private static final String FIND_LAST_BY_STATE = "SELECT i FROM Inscription i WHERE i.etat = ?1";
+    public void setParametre(final String parametre) {
+        this.parametre = parametre;
+    }
 
-	private static final String FIND_BY_ENTITY = "SELECT i FROM Inscription i WHERE i.entite.id = ?1 ";
+    public void setVar(final String var) {
+        this.var = var;
+    }
 
-	private static final String DATABASE_NAME = "fsnetjpa";
+    public void setEtat(final String etat) {
+        this.etat = etat;
+    }
 
-	private static final String WAITING = "En attente d'inscription";
+    public void setEntite(final String entite) {
+        this.entite = entite;
+    }
 
-	private static final String REGISTERED = "Inscrit";
+    @Override
+    public int doStartTag() throws JspException {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+        EntityManager em = factory.createEntityManager();
+        Query query;
+        List<Inscription> lesInscriptions;
+        String condition = null;
+        if (filtre != null) {
+            lesInscriptions = recherche();
+        } else {
+            if (etat == null) {
+                if (entite == null) {
+                    query = em.createQuery(FIND_ALL);
 
-	private static final String CONDITION_AWAITING_REGISTRATION = "ORDER BY i.entite.nom ASC";
+                } else {
+                    query = em.createQuery(FIND_BY_ENTITY);
+                    query.setParameter(1, Integer.parseInt(entite));
+                }
+                lesInscriptions = query.getResultList();
+            } else {
+                if (REGISTERED.equals(etat)) {
+                    condition = CONDITION_REGISTERED;
+                    query = em.createQuery(FIND_LAST_BY_STATE + condition);
+                    query.setParameter(1, etat);
+                    lesInscriptions = query.setFirstResult(0).setMaxResults(
+                            MAX_MEMBERS).getResultList();
+                } else {
+                    condition = CONDITION_AWAITING_REGISTRATION;
+                    query = em.createQuery(FIND_LAST_BY_STATE + condition);
+                    query.setParameter(1, etat);
+                    lesInscriptions = query.getResultList();
+                }
+            }
+        }
 
-	private static final String CONDITION_REGISTERED = "ORDER BY i.id DESC";
+        if ((lesInscriptions == null) || (lesInscriptions.isEmpty())) {
+            pageContext.setAttribute("vide", "vide");
+        } else {
+            it = lesInscriptions.iterator();
+            pageContext.setAttribute("vide", "nonVide");
+        }
+        if (it != null && it.hasNext()) {
+            updateContext(it.next());
+            return EVAL_BODY_INCLUDE;
+        }
 
-	private static final int MAX_MEMBERS = 7;
+        return SKIP_BODY;
+    }
 
-	private static final int SIZE = 10;
+    private void updateContext(Inscription inscription) {
+        pageContext.setAttribute(var, inscription);
+        String nom = inscription.getEntite().getNom();
+        String prenom = inscription.getEntite().getPrenom();
+        String email = inscription.getEntite().getEmail();
+        if (nom != null && nom.length() < SIZE) {
+            pageContext.setAttribute("svarNom", nom);
+        } else {
+            pageContext.setAttribute("svarNom", nom.substring(0, SIZE - 1)
+                    + "...");
+        }
+        if (prenom != null) {
+            if (prenom.length() < SIZE) {
+                pageContext.setAttribute("svarPrenom", prenom);
+            } else {
+                pageContext.setAttribute("svarPrenom", prenom.substring(0, SIZE - 1)
+                        + "...");
+            }
+        }
+        if (email != null) {
+            if (email.length() < SIZE) {
+                pageContext.setAttribute("svarEmail", email);
+            } else {
+                pageContext.setAttribute("svarEmail", email.substring(0, SIZE - 1)
+                        + "...");
+            }
+        }
+    }
 
-	private Iterator<Inscription> it;
-	private String var;
-	private String etat;
-	private String entite;
-	private String parametre;
-	private String filtre;
+    @Override
+    public int doAfterBody() throws JspException {
+        if (it.hasNext()) {
+            updateContext(it.next());
+            return EVAL_BODY_AGAIN;
+        }
+        return SKIP_BODY;
+    }
 
-	public void setFiltre(final String filtre) {
-		this.filtre = filtre;
-	}
+    @Override
+    public int doEndTag() throws JspException {
+        pageContext.removeAttribute(var);
+        return super.doEndTag();
+    }
 
-	public void setParametre(final String parametre) {
-		this.parametre = parametre;
-	}
+    public List<Inscription> recherche() {
+        if (filtre.equals("nom")) {
+            return searchNom(parametre);
+        }
+        if (filtre.equals("prenom")) {
+            return searchPrenom(parametre);
+        }
+        return null;
+    }
 
-	public void setVar(final String var) {
-		this.var = var;
-	}
+    public List<Inscription> searchNom(String param) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+        EntityManager em = factory.createEntityManager();
+        List<Inscription> lesES = new ArrayList<Inscription>();
+        Query query = em.createQuery("SELECT i FROM Inscription i WHERE i.entite.nom=?1");
+        query.setParameter(1, param);
 
-	public void setEtat(final String etat) {
-		this.etat = etat;
-	}
+        System.out.println("Taille pour nom " + query.getResultList().size());
+        lesES = query.getResultList();
+        return lesES;
+    }
 
-	public void setEntite(final String entite) {
-		this.entite = entite;
-	}
+    public List<Inscription> searchPrenom(String param) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+        EntityManager em = factory.createEntityManager();
+        List<Inscription> lesES = new ArrayList<Inscription>();
+        Query query = em.createQuery("SELECT i FROM Inscription i WHERE i.entite.prenom=?1");
+        query.setParameter(1, param);
+        lesES = query.getResultList();
+        return lesES;
 
-	public int doStartTag() throws JspException {
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory(DATABASE_NAME);
-		EntityManager em = factory.createEntityManager();
-		Query query;
-		List<Inscription> lesInscriptions;
-		String condition = null;
-		if (filtre != null)
-			lesInscriptions = recherche();
-		else {
-			if (etat == null) {
-				if (entite == null) {
-					query = em.createQuery(FIND_ALL);
-
-				} else {
-					query = em.createQuery(FIND_BY_ENTITY);
-					query.setParameter(1, Integer.parseInt(entite));
-				}
-				lesInscriptions = query.getResultList();
-			} else {
-				if (REGISTERED.equals(etat)) {
-					condition = CONDITION_REGISTERED;
-					query = em.createQuery(FIND_LAST_BY_STATE + condition);
-					query.setParameter(1, etat);
-					lesInscriptions = query.setFirstResult(0).setMaxResults(
-							MAX_MEMBERS).getResultList();
-				} else {
-					condition = CONDITION_AWAITING_REGISTRATION;
-					query = em.createQuery(FIND_LAST_BY_STATE + condition);
-					query.setParameter(1, etat);
-					lesInscriptions = query.getResultList();
-				}
-			}
-		}
-
-		if ((lesInscriptions == null) || (lesInscriptions.isEmpty())) {
-			pageContext.setAttribute("vide", "vide");
-		} else {
-			it = lesInscriptions.iterator();
-			pageContext.setAttribute("vide", "nonVide");
-		}
-		if (it != null && it.hasNext()) {
-			updateContext(it.next());
-			return EVAL_BODY_INCLUDE;
-		}
-
-		return SKIP_BODY;
-	}
-
-	private void updateContext(Inscription inscription) {
-		pageContext.setAttribute(var, inscription);
-		String nom = inscription.getEntite().getNom();
-		String prenom = inscription.getEntite().getPrenom();
-		String email = inscription.getEntite().getEmail();
-		if (nom.length() < SIZE) {
-			pageContext.setAttribute("svarNom", nom);
-		} else {
-			pageContext.setAttribute("svarNom", nom.substring(0, SIZE - 1)
-					+ "...");
-		}
-		if (prenom.length() < SIZE) {
-			pageContext.setAttribute("svarPrenom", prenom);
-		} else {
-			pageContext.setAttribute("svarPrenom", prenom
-					.substring(0, SIZE - 1)
-					+ "...");
-		}
-		if (email.length() < SIZE) {
-			pageContext.setAttribute("svarEmail", email);
-		} else {
-			pageContext.setAttribute("svarEmail", email.substring(0, SIZE - 1)
-					+ "...");
-		}
-	}
-
-	public int doAfterBody() throws JspException {
-		if (it.hasNext()) {
-			updateContext(it.next());
-			return EVAL_BODY_AGAIN;
-		}
-		return SKIP_BODY;
-	}
-
-	public int doEndTag() throws JspException {
-		pageContext.removeAttribute(var);
-		return super.doEndTag();
-	}
-
-	public List<Inscription> recherche() {
-		if (filtre.equals("nom")) {
-			return searchNom(parametre);
-		}
-		if (filtre.equals("prenom")) {
-			return searchPrenom(parametre);
-		}
-		return null;
-	}
-
-	public List<Inscription> searchNom(String param) {
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory(DATABASE_NAME);
-		EntityManager em = factory.createEntityManager();
-		List<Inscription> lesES = new ArrayList<Inscription>();
-		Query query = em
-				.createQuery("SELECT i FROM Inscription i WHERE i.entite.nom=?1");
-		query.setParameter(1, param);
-
-		System.out.println("Taille pour nom " + query.getResultList().size());
-		lesES = query.getResultList();
-		return lesES;
-	}
-
-	public List<Inscription> searchPrenom(String param) {
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory(DATABASE_NAME);
-		EntityManager em = factory.createEntityManager();
-		List<Inscription> lesES = new ArrayList<Inscription>();
-		Query query = em
-				.createQuery("SELECT i FROM Inscription i WHERE i.entite.prenom=?1");
-		query.setParameter(1, param);
-		lesES = query.getResultList();
-		return lesES;
-
-	}
-
+    }
 }
