4bb6f64a1db26952cdd5ff444017289a2dc75f9c
supergeoffrey <supergeoffrey@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-13 21:31:53 +0000

    "watch" code remove unused import remove auto-generate comment
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@408 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/trayWithServlets/src/main/java/fr/univartois/ili/fsnet/trayWithServlets/servlets/InformationsSender.java
+++ fr.univartois.ili.fsnet/trayWithServlets/src/main/java/fr/univartois/ili/fsnet/trayWithServlets/servlets/InformationsSender.java
@@ -21,15 +21,12 @@ public class InformationsSender extends HttpServlet {
      */
     public InformationsSender() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
 		Set<String> maSet = new HashSet<String>();
 		String info1= "java site developpez";
 		String info2= "nouvelle annoce de soiree";
@@ -46,8 +43,6 @@ public class InformationsSender extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 
 }
