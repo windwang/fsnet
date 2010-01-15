b9401a8a672f6c61b5f399ee851e717360b6e9e9
alexandre.lohez <alexandre.lohez@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-15 20:18:52 +0000

    resolving issue 13
    Remove AddInterest servlet
    modify ActionInterest action, struts-config (forwarding), web.xml (delete servlet)
    
    git-svn-id: http://fsnet.googlecode.com/svn@428 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ActionInterest.java
+++ trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ActionInterest.java
@@ -1,26 +1,53 @@
 package fr.univartois.ili.fsnet.admin.actions;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.RollbackException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.DynaActionForm;
 
 import fr.univartois.ili.fsnet.entities.Interet;
 
 public class ActionInterest extends Action{
+	private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+    private EntityManager em;
+	
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest req, HttpServletResponse arg3) throws Exception {
-        DynaActionForm dynaform = (DynaActionForm)form;
+		em = factory.createEntityManager();
+		DynaActionForm dynaform = (DynaActionForm)form;
         Interet dto = new Interet();
         BeanUtils.copyProperties(dto,dynaform);
-
-        req.getSession().setAttribute("interet",dto);        
         
-        return mapping.findForward("continue"); 
+        try {
+            em.getTransaction().begin();
+            em.persist(dto);
+            em.getTransaction().commit();
+        } catch (RollbackException ex) {
+            ActionErrors actionErrors = new ActionErrors();
+            ActionMessage msg = new ActionMessage(
+                    "Impossible de persister l'entité, cet intérêt est déjà dans la base.");
+            actionErrors.add("nomInteret", msg);
+            saveErrors(req, actionErrors);
+        }
+        
+        em.close();
+        
+        ActionForward forward = new ActionForward(
+        		mapping.findForward("continue").getPath()+
+        		"?interest=current&showHide=show&deploy=[-]&titleDeploy=R%E9duire la liste"
+        );
+        
+        return forward; 
 	}
 }
