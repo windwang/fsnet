4d38ec236bad3ef8d78a1fbf50359cdfcf40bc96
mehdi.benzaghar <mehdi.benzaghar@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-19 00:25:28 +0000

    add create method in ManageAnnounces
    git-svn-id: http://fsnet.googlecode.com/svn@478 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageAnnounces.java
+++ trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageAnnounces.java
@@ -18,43 +18,202 @@
 package fr.univartois.ili.fsnet.actions;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 import org.apache.struts.actions.MappingDispatchAction;
 
+import fr.univartois.ili.fsnet.entities.Annonce;
+import fr.univartois.ili.fsnet.entities.EntiteSociale;
+
 /**
- *
- * @author
+ * 
+ * @author BENZAGHAR MEHDI
  */
-public class ManageAnnounces extends MappingDispatchAction implements CrudAction {
+public class ManageAnnounces extends MappingDispatchAction implements
+		CrudAction {
 
-    @Override
-    public ActionForward create(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public ActionForward create(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		if (processRoles(request, response, mapping)) {
+			Annonce newAnnounce;
+			EntiteSociale entiteSociale;
+			HttpSession session;
+			String title;
+			String content;
+			String stringExpiryDate;
+			Date expiryDate;
+			Date today;
+			DateFormat simpleFormat;
+			String DATABASE_NAME = "fsnet";
+			EntityManagerFactory factory;
+			EntityManager entityManager;
+			ActionMessages errors = new ActionErrors();
+			session = request.getSession();
+			factory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+			entityManager = factory.createEntityManager();
+			entiteSociale = (EntiteSociale) session.getAttribute("entite");
+			title = request.getParameter("titleAnnouncement");
+			content = request.getParameter("contentAnnouncement");
+			stringExpiryDate = request.getParameter("expiryDateAnnouncement");
+			if (title != null && !title.isEmpty() && content != null
+					&& !content.isEmpty() && stringExpiryDate != null
+					&& !stringExpiryDate.isEmpty()) {
+				simpleFormat = new SimpleDateFormat("dd/MM/yy", Locale.FRANCE);
+				try {
+					expiryDate = (Date) simpleFormat.parse(stringExpiryDate);
+					// TODO
+					today = new Date(0);
+					if (0 < expiryDate.compareTo(today)) {
 
-    @Override
-    public ActionForward modify(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+						newAnnounce = new Annonce(title, today, content,
+								expiryDate, "Y", entiteSociale);
+						entityManager.getTransaction().begin();
+						entityManager.persist(newAnnounce);
+						entityManager.getTransaction().commit();
 
-    @Override
-    public ActionForward delete(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+					} else {
+						errors.add("message", new ActionMessage(
+								"errors.dateLessThanCurrentDate"));
+						saveErrors(request, errors);
+					}
+				} catch (ParseException e) {
+					servlet
+							.log("class:ManageAnnounces methode:create exception whene formatying date ");
+					e.printStackTrace();
+				}
 
-    @Override
-    public ActionForward search(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+			} else {
+				if (title == null || title.isEmpty())
+					errors.add("message", new ActionMessage(
+							"errors.required.title"));
+				if (stringExpiryDate == null || stringExpiryDate.isEmpty())
+					errors.add("message", new ActionMessage(
+							"errors.required.content"));
+				if (content == null || content.isEmpty())
+					errors.add("message", new ActionMessage(
+							"errors.required.expiryDate"));
+				saveErrors(request, errors);
+			}
+			// TODO
+			return mapping.findForward("");
 
-    @Override
-    public ActionForward display(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+		}
+		return mapping.findForward("");
+
+	}
+
+	@Override
+	public ActionForward modify(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public ActionForward delete(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public ActionForward search(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public ActionForward display(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @author mehdi
+	 * @param request
+	 * @return boolean true : if user is connected
+	 */
+	protected boolean isUserConnected(HttpServletRequest request) {
+		EntiteSociale es = null;
+		if (request != null) {
+			HttpSession session = request.getSession(false);
+			if (session != null) {
+				es = (EntiteSociale) session.getAttribute("es");
+			}
+
+			return es != null;
+		}
+		return false;
+	}
+
+	/**
+	 * @author mehdi
+	 * @param request
+	 * @return boolean true : if this session is valid
+	 * @throws ServletException
+	 */
+
+	protected boolean isSessionValid(HttpServletRequest request)
+			throws ServletException {
+		if (request != null) {
+			return request.isRequestedSessionIdValid();
+		}
+
+		return false;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param request
+	 * @param response
+	 * @param mapping
+	 * @return boolean true : if the session is valid and the user is connected
+	 * @throws java.io.IOException
+	 * @throws javax.servlet.ServletException
+	 */
+	protected boolean processRoles(HttpServletRequest request,
+			HttpServletResponse response, ActionMapping mapping)
+			throws java.io.IOException, javax.servlet.ServletException {
+
+		ActionMessages messages = new ActionMessages();
+		if (isSessionValid(request)) {
+			if (isUserConnected(request)) {
+				return true;
+			} else {
+				messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(
+						"error.connection.required"));
+			}
+		} else {
+			messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(
+					"error.session.expired"));
+		}
+
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+		return false;
+	}
 
 }
