d27c9a6dfd153a09ebda176e4f3684896b75c723
orel.legrand <orel.legrand@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-20 15:41:59 +0000

    added some actions on ManageContact
    with grynch62
    
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@526 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageContacts.java
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageContacts.java
@@ -17,36 +17,47 @@
 package fr.univartois.ili.fsnet.actions;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.MappingDispatchAction;
 
+import fr.univartois.ili.fsnet.auth.Authenticate;
 import fr.univartois.ili.fsnet.entities.EntiteSociale;
+import fr.univartois.ili.fsnet.entities.test.utils.TestEntityManagerProvider;
 
 /**
  * 
  * @author Grioche Legrand
  */
 public class ManageContacts extends MappingDispatchAction implements CrudAction {
-	
+
 	private static EntityManagerFactory factory = Persistence
-	.createEntityManagerFactory("fsnetjpa");
+			.createEntityManagerFactory("fsnetjpa");
+
+	public ActionForward ask(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = TestEntityManagerProvider.getInstance()
+				.getEntityManager();
+		List listEntities = em.createQuery("select entite from EntiteSociale entite").getResultList();
+		request.setAttribute("listEntities", listEntities);
+		return mapping.findForward("success");
+	}
 
 	/**
 	 * to submit a request contact to another social entity
+	 * 
 	 * @param mapping
 	 * @param form
 	 * @param request
@@ -58,11 +69,28 @@ public class ManageContacts extends MappingDispatchAction implements CrudAction 
 	public ActionForward askContact(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
+
+		EntityManager em = TestEntityManagerProvider.getInstance()
+				.getEntityManager();
+		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+				Authenticate.AUTHENTICATED_USER);
+		EntiteSociale entitySelected = (EntiteSociale) request
+				.getAttribute("entitySelected");
+		em.getTransaction().begin();
+
+		// TODO changer les listes en set sur les entites sociales pour eviter
+		// les doublons
+		user.getRequested().add(entitySelected);
+		entitySelected.getAsked().add(user);
+		em.merge(entitySelected);
+		em.merge(user);
+		em.getTransaction().commit();
+		return mapping.findForward("success");
 	}
-	
+
 	/**
 	 * To accept a contact request
+	 * 
 	 * @param mapping
 	 * @param form
 	 * @param request
@@ -74,11 +102,27 @@ public class ManageContacts extends MappingDispatchAction implements CrudAction 
 	public ActionForward accept(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
+
+		EntityManager em = TestEntityManagerProvider.getInstance()
+				.getEntityManager();
+		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+				Authenticate.AUTHENTICATED_USER);
+		EntiteSociale entityAccepted = (EntiteSociale) request
+				.getAttribute("entityAccepted");
+		em.getTransaction().begin();
+		user.getContacts().add(entityAccepted);
+		entityAccepted.getContacts().add(user);
+		user.getAsked().remove((EntiteSociale) entityAccepted);
+		entityAccepted.getRequested().remove(user);
+		em.merge(user);
+		em.merge(entityAccepted);
+		em.getTransaction().commit();
+		return mapping.findForward("success");
 	}
-	
+
 	/**
 	 * To refuse a contact request
+	 * 
 	 * @param mapping
 	 * @param form
 	 * @param request
@@ -90,11 +134,29 @@ public class ManageContacts extends MappingDispatchAction implements CrudAction 
 	public ActionForward refuse(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
+
+		EntityManager em = TestEntityManagerProvider.getInstance()
+				.getEntityManager();
+		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+				Authenticate.AUTHENTICATED_USER);
+		EntiteSociale entityRefused = (EntiteSociale) request
+				.getAttribute("entityRefused");
+
+		em.getTransaction().begin();
+		user.getRefused().add(entityRefused);
+		user.getAsked().remove(entityRefused);
+		entityRefused.getRequested().remove(user);
+		entityRefused.getRefused().add(user);
+		em.merge(user);
+		em.merge(entityRefused);
+		em.getTransaction().commit();
+
+		return mapping.findForward("success");
 	}
 
 	/**
 	 * Display the list of asked contacts
+	 * 
 	 * @param mapping
 	 * @param form
 	 * @param request
@@ -103,14 +165,23 @@ public class ManageContacts extends MappingDispatchAction implements CrudAction 
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public ActionForward displayRequest(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public ActionForward displayRequests(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+
+		EntityManager em = TestEntityManagerProvider.getInstance()
+				.getEntityManager();
+		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+				Authenticate.AUTHENTICATED_USER);
+		em.refresh(user);
+		List<EntiteSociale> listRequests = user.getRequested();
+		request.setAttribute("listRequests", listRequests);
+		return mapping.findForward("success");
 	}
-	
+
 	/**
 	 * Display the list of received demands
+	 * 
 	 * @param mapping
 	 * @param form
 	 * @param request
@@ -122,10 +193,13 @@ public class ManageContacts extends MappingDispatchAction implements CrudAction 
 	public ActionForward displayDemands(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
+		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+				Authenticate.AUTHENTICATED_USER);
+		List<EntiteSociale> listDemands = user.getAsked();
+		request.setAttribute("listDemands", listDemands);
+		return mapping.findForward("success");
 	}
-	
-	
+
 	@Override
 	public ActionForward create(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
@@ -158,19 +232,10 @@ public class ManageContacts extends MappingDispatchAction implements CrudAction 
 	public ActionForward display(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		
-		EntiteSociale es = new EntiteSociale("Proucelle", "Matthieu",
-		"prouprou@fsnet.com");
-		
-		EntiteSociale es1 = new EntiteSociale("Gry", "Orel",
-		"grygry@fsnet.com");
-		EntiteSociale es2 = new EntiteSociale("René", "Bigood",
-		"bigood@fsnet.com");
-		es.setContacts(new ArrayList<EntiteSociale>());
-		es.getContacts().add(es1);
-		es.getContacts().add(es2);
-		request.setAttribute("entitiContact", es);	
-		Logger.getAnonymousLogger().info("okkkkkkkkkkkkkkkkkkk");
+		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+				Authenticate.AUTHENTICATED_USER);
+		List<EntiteSociale> listContacts = user.getContacts();
+		request.setAttribute("listContacts", listContacts);
 		return mapping.findForward("success");
 	}
 }
