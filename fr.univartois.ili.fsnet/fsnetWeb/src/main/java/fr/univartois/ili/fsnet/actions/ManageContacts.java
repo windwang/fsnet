6c60390841d0cf3634728b4ffefeac6e412bd726
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-20 20:43:15 +0000

    Fixed use of a test util class in java sources. Build now run corectly.
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@537 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageContacts.java
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageContacts.java
@@ -35,7 +35,6 @@ import org.apache.struts.actions.MappingDispatchAction;
 
 import fr.univartois.ili.fsnet.auth.Authenticate;
 import fr.univartois.ili.fsnet.entities.EntiteSociale;
-import fr.univartois.ili.fsnet.entities.test.utils.TestEntityManagerProvider;
 
 /**
  * 
@@ -43,209 +42,210 @@ import fr.univartois.ili.fsnet.entities.test.utils.TestEntityManagerProvider;
  */
 public class ManageContacts extends MappingDispatchAction implements CrudAction {
 
-	private static EntityManagerFactory factory = Persistence
-			.createEntityManagerFactory("fsnetjpa");
+    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
 
-	public ActionForward ask(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		EntityManager em = TestEntityManagerProvider.getInstance()
-				.getEntityManager();
-		List<EntiteSociale> listEntities = em.createQuery(
-				"select entite from EntiteSociale entite").getResultList();
-		request.setAttribute("listEntities", listEntities);
-		Logger.getAnonymousLogger().severe("@@@@@@@@@@@@@" + listEntities);
-		return mapping.findForward("success");
-	}
+    public ActionForward ask(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        EntityManager em = factory.createEntityManager();
+        List<EntiteSociale> listEntities = em.createQuery(
+                "select entite from EntiteSociale entite").getResultList();
+        request.setAttribute("listEntities", listEntities);
+        Logger.getAnonymousLogger().severe("@@@@@@@@@@@@@" + listEntities);
+        em.close();
+        return mapping.findForward("success");
+    }
 
-	/**
-	 * to submit a request contact to another social entity
-	 * 
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 * @throws ServletException
-	 */
-	public ActionForward askContact(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+    /**
+     * to submit a request contact to another social entity
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     * @throws ServletException
+     */
+    public ActionForward askContact(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
 
-		EntityManager em = TestEntityManagerProvider.getInstance()
-				.getEntityManager();
-		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
-				Authenticate.AUTHENTICATED_USER);
-		EntiteSociale entitySelected = (EntiteSociale) request
-				.getAttribute("entitySelected");
-		em.getTransaction().begin();
 
-		// TODO changer les listes en set sur les entites sociales pour eviter
-		// les doublons
-		user.getRequested().add(entitySelected);
-		entitySelected.getAsked().add(user);
-		em.merge(entitySelected);
-		em.merge(user);
-		em.getTransaction().commit();
-		return mapping.findForward("success");
-	}
+        EntityManager em = factory.createEntityManager();
+        EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+                Authenticate.AUTHENTICATED_USER);
+        EntiteSociale entitySelected = (EntiteSociale) request.getAttribute("entitySelected");
+        em.getTransaction().begin();
 
-	/**
-	 * To accept a contact request
-	 * 
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 * @throws ServletException
-	 */
-	public ActionForward accept(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+        // TODO changer les listes en set sur les entites sociales pour eviter
+        // les doublons
+        user.getRequested().add(entitySelected);
+        entitySelected.getAsked().add(user);
+        em.merge(entitySelected);
+        em.merge(user);
+        em.getTransaction().commit();
+        em.close();
+        return mapping.findForward("success");
+    }
 
-		EntityManager em = TestEntityManagerProvider.getInstance()
-				.getEntityManager();
-		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
-				Authenticate.AUTHENTICATED_USER);
-		EntiteSociale entityAccepted = (EntiteSociale) request
-				.getAttribute("entityAccepted");
-		em.getTransaction().begin();
-		user.getContacts().add(entityAccepted);
-		entityAccepted.getContacts().add(user);
-		user.getAsked().remove((EntiteSociale) entityAccepted);
-		entityAccepted.getRequested().remove(user);
-		em.merge(user);
-		em.merge(entityAccepted);
-		em.getTransaction().commit();
-		return mapping.findForward("success");
-	}
+    /**
+     * To accept a contact request
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     * @throws ServletException
+     */
+    public ActionForward accept(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
 
-	/**
-	 * To refuse a contact request
-	 * 
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 * @throws ServletException
-	 */
-	public ActionForward refuse(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
 
-		EntityManager em = TestEntityManagerProvider.getInstance()
-				.getEntityManager();
-		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
-				Authenticate.AUTHENTICATED_USER);
-		EntiteSociale entityRefused = (EntiteSociale) request
-				.getAttribute("entityRefused");
+        EntityManager em = factory.createEntityManager();
+        EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+                Authenticate.AUTHENTICATED_USER);
+        EntiteSociale entityAccepted = (EntiteSociale) request.getAttribute("entityAccepted");
+        em.getTransaction().begin();
+        user.getContacts().add(entityAccepted);
+        entityAccepted.getContacts().add(user);
+        user.getAsked().remove((EntiteSociale) entityAccepted);
+        entityAccepted.getRequested().remove(user);
+        em.merge(user);
+        em.merge(entityAccepted);
+        em.getTransaction().commit();
+        em.close();
+        return mapping.findForward("success");
+    }
 
-		em.getTransaction().begin();
-		user.getRefused().add(entityRefused);
-		user.getAsked().remove(entityRefused);
-		entityRefused.getRequested().remove(user);
-		entityRefused.getRefused().add(user);
-		em.merge(user);
-		em.merge(entityRefused);
-		em.getTransaction().commit();
+    /**
+     * To refuse a contact request
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     * @throws ServletException
+     */
+    public ActionForward refuse(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
 
-		return mapping.findForward("success");
-	}
 
-	/**
-	 * Display the list of asked contacts
-	 * 
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 * @throws ServletException
-	 */
-	public ActionForward displayRequests(ActionMapping mapping,
-			ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException {
+        EntityManager em = factory.createEntityManager();
+        EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+                Authenticate.AUTHENTICATED_USER);
+        EntiteSociale entityRefused = (EntiteSociale) request.getAttribute("entityRefused");
 
-		EntityManager em = TestEntityManagerProvider.getInstance()
-				.getEntityManager();
-		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
-				Authenticate.AUTHENTICATED_USER);
-		em.refresh(user);
-		List<EntiteSociale> listRequests = user.getRequested();
-		request.setAttribute("listRequests", listRequests);
-		return mapping.findForward("success");
-	}
+        em.getTransaction().begin();
+        user.getRefused().add(entityRefused);
+        user.getAsked().remove(entityRefused);
+        entityRefused.getRequested().remove(user);
+        entityRefused.getRefused().add(user);
+        em.merge(user);
+        em.merge(entityRefused);
+        em.getTransaction().commit();
 
-	/**
-	 * Display the list of received demands
-	 * 
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 * @throws ServletException
-	 */
-	public ActionForward displayDemands(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
-				Authenticate.AUTHENTICATED_USER);
-		List<EntiteSociale> listDemands = user.getAsked();
-		request.setAttribute("listDemands", listDemands);
-		return mapping.findForward("success");
-	}
+        em.close();
+        return mapping.findForward("success");
+    }
 
-	@Override
-	public ActionForward create(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    /**
+     * Display the list of asked contacts
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     * @throws ServletException
+     */
+    public ActionForward displayRequests(ActionMapping mapping,
+            ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws IOException, ServletException {
 
-	@Override
-	public ActionForward modify(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
 
-	@Override
-	public ActionForward delete(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+        EntityManager em = factory.createEntityManager();
+        EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+                Authenticate.AUTHENTICATED_USER);
+        // Non surtout pas
+        //em.refresh(user);
+        List<EntiteSociale> listRequests = user.getRequested();
+        request.setAttribute("listRequests", listRequests);
+        return mapping.findForward("success");
+    }
 
-	@Override
-	public ActionForward search(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    /**
+     * Display the list of received demands
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     * @throws ServletException
+     */
+    public ActionForward displayDemands(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+                Authenticate.AUTHENTICATED_USER);
+        List<EntiteSociale> listDemands = user.getAsked();
+        request.setAttribute("listDemands", listDemands);
+        return mapping.findForward("success");
+    }
 
-	@Override
-	public ActionForward display(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		EntityManager em = TestEntityManagerProvider.getInstance()
-				.getEntityManager();
-		EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
-				Authenticate.AUTHENTICATED_USER);
-		user = em.find(EntiteSociale.class, user.getId());
-		List<EntiteSociale> listContacts = user.getContacts();
-		request.setAttribute("listContacts", listContacts);
-		List<EntiteSociale> listDemands = user.getAsked();
-		request.setAttribute("listDemands", listDemands);
-		List<EntiteSociale> listRequests = user.getRequested();
-		request.setAttribute("listRequests", listRequests);
-		return mapping.findForward("success");
-	}
+    @Override
+    public ActionForward create(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public ActionForward modify(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public ActionForward delete(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public ActionForward search(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public ActionForward display(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        EntityManager em = factory.createEntityManager();
+        EntiteSociale user = (EntiteSociale) request.getSession().getAttribute(
+                Authenticate.AUTHENTICATED_USER);
+        user = em.find(EntiteSociale.class, user.getId());
+        List<EntiteSociale> listContacts = user.getContacts();
+        request.setAttribute("listContacts", listContacts);
+        List<EntiteSociale> listDemands = user.getAsked();
+        request.setAttribute("listDemands", listDemands);
+        List<EntiteSociale> listRequests = user.getRequested();
+        request.setAttribute("listRequests", listRequests);
+        em.close();
+        return mapping.findForward("success");
+    }
 }
