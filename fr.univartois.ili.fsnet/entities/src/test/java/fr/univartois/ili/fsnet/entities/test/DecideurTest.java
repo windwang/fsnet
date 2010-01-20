e9546948c01f6048439daea6bbaabac1b0f356dd
mehdi.benabdessalem <mehdi.benabdessalem@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-20 16:04:35 +0000

    Cancel changes on Decideur
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@528 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/entities/src/test/java/fr/univartois/ili/fsnet/entities/test/DecideurTest.java
+++ fr.univartois.ili.fsnet/entities/src/test/java/fr/univartois/ili/fsnet/entities/test/DecideurTest.java
@@ -3,15 +3,12 @@ package fr.univartois.ili.fsnet.entities.test;
 import static org.junit.Assert.assertNotNull;
 
 import javax.persistence.EntityManager;
-import javax.persistence.RollbackException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import fr.univartois.ili.fsnet.entities.Decideur;
-import fr.univartois.ili.fsnet.entities.EntiteSociale;
-import fr.univartois.ili.fsnet.entities.Interaction;
 import fr.univartois.ili.fsnet.entities.test.utils.TestEntityManagerProvider;
 
 public class DecideurTest {
@@ -29,29 +26,12 @@ public class DecideurTest {
 
     @Test
     public void testPersist() {
-        Decideur decideur = new Decideur(null, null);
+        Decideur decideur = new Decideur();
         em.getTransaction().begin();
         em.persist(decideur);
         em.getTransaction().commit();
         int monId = decideur.getId();
         assertNotNull("id not null", monId);
     }
-    
-    @Test(expected = RollbackException.class)
-	public void testRequieredEntSociale() {
-    	Interaction i= new Interaction();
-		Decideur d = new Decideur(null,i);
-		em.getTransaction().begin();
-		em.persist(d);
-		em.getTransaction().commit();
-	}
-    
-    @Test(expected = RollbackException.class)
-	public void testRequieredInteraction() {
-    	EntiteSociale es = new EntiteSociale("tata", "titi", "titi@gmai.com");
-		Decideur d = new Decideur(es,null);
-		em.getTransaction().begin();
-		em.persist(d);
-		em.getTransaction().commit();
-	}
+  
 }
