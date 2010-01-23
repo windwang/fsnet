effd9431cf1ffae1e401142a91fa4d7208f60ea4
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-23 13:02:59 +0000

    Home page added
    git-svn-id: http://fsnet.googlecode.com/svn@603 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/auth/Authenticate.java
+++ trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/auth/Authenticate.java
@@ -28,7 +28,7 @@ public class Authenticate extends HttpServlet {
 	/**
 	 * Welcome page path when the user is authenticated
 	 */
-	private static final String WELCOME_AUTHENTICATED_PAGE = "DisplayEvents.do";
+	private static final String WELCOME_AUTHENTICATED_PAGE = "Home.do";
 
 	/**
 	 * Welcome page path when the user is NOT authenticated
