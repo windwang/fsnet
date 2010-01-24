c19b101174722709d728eae934a95e255d1de5e1
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-24 22:29:21 +0000

    Mail configuration is back.
    git-svn-id: http://fsnet.googlecode.com/svn@647 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ModifyOptions.java
+++ trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ModifyOptions.java
@@ -1,61 +1,47 @@
 package fr.univartois.ili.fsnet.admin.actions;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.DynaActionForm;
 
+import fr.univartois.ili.fsnet.admin.utils.FSNetConfiguration;
+
 public class ModifyOptions extends Action {
-
-    private static final Logger log = Logger.getLogger(ModifyOptions.class.getName());
-
-    @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
-            HttpServletRequest req, HttpServletResponse arg3) throws Exception {
-        DynaActionForm dynaform = (DynaActionForm) form;
-        try {
-            setServerOptions(
-                    (String) dynaform.get("serveursmtp"),
-                    (String) dynaform.get("hote"),
-                    (String) dynaform.get("motdepasse"),
-                    (String) dynaform.get("adressefsnet"),
-                    (Integer) dynaform.get("port"));
-        } catch (BackingStoreException e) {
-            log.log(Level.SEVERE, "Impossible d'enregistrer les preferences.");
-            ActionErrors errors = new ActionErrors();
-            errors.add("errors.optionSave", new ActionMessage("errors.optionSave"));
-
+	
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest req, HttpServletResponse arg3) throws Exception {
+		DynaActionForm dynaform = (DynaActionForm) form; 							//NOSONAR
+		FSNetConfiguration conf = FSNetConfiguration.getInstance();
+		conf.reset();
+        if ("".equals(dynaform.get("enableTLS"))) {
+        	conf.disableTLS();
+        } else {
+        	conf.enableTLS();
         }
-        return mapping.findForward("continue");
-    }
-
-    /**
-     * Set mail server options in preference file
-     * @param host
-     * @param password
-     * @param adresseFsnet
-     * @param port
-     * @throws BackingStoreException
-     */
-    public void setServerOptions(String serverSmtp, String host, String password, String adresseFsnet, Integer port)
-            throws BackingStoreException {
-
-        Preferences appPref = Preferences.userNodeForPackage(ModifyOptions.class);
-        appPref.put("serveursmtp", serverSmtp);
-        appPref.put("hote", host);
-        appPref.put("motdepasse", password);
-        appPref.put("adressefsnet", adresseFsnet);
-        appPref.putInt("port", port);
-        appPref.flush();
-    }
+        if ("".equals(dynaform.get("enableSSL"))) {
+        	conf.disableSSL();
+        } else {
+        	conf.enableSSL();
+        }
+        if ("".equals(dynaform.get("authenticate"))) {
+        	conf.disableAuthentication();
+        } else {
+        	conf.enableAuthentication((String) dynaform.get("username"), (String) dynaform.get("motdepasse"));
+        }
+        
+        conf.setFrom((String) dynaform.get("hote"));
+        conf.setSMTPHost((String) dynaform.get("serveursmtp"));
+        conf.setSMTPPort((String) dynaform.get("port"));
+        conf.setFSNetWebAddress((String) dynaform.get("adressefsnet"));
+        conf.setMailConfigured(true);
+        conf.save();
+        
+		return mapping.findForward("continue");
+	}
 }
---
+++ trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/utils/FSNetConfiguration.java
@@ -0,0 +1,179 @@
+package fr.univartois.ili.fsnet.admin.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FSNetConfiguration {
+
+	private static final String MAIL_FROM = "fsnet.mail.from";
+	private static final String ENABLE_TLS = "mail.smtp.starttls.enable";
+	private static final String SMTP_HOST = "mail.smtp.host";
+	private static final String SMTP_USER = "mail.smtp.user";
+	private static final String SMTP_PASSWORD = "mail.smtp.password";
+	private static final String FSNET_WEB_ADDRESS = "fsnet.web.address";
+	private static final String ENABLE_AUTHENTICATION = "mail.smtp.auth";
+	private static final String SMTP_PORT = "mail.smtp.port";
+	private static final String SSL_KEY = "mail.smtp.socketFactory.class";
+	private static final String SSL_ENABLED_VALUE = "javax.net.ssl.SSLSocketFactory";
+	private static final String MAIl_CONFIGURATION_DONE = "fsnet.mail.isConfigured";
+
+	private static final FSNetConfiguration instance;
+
+	public static final FSNetConfiguration getInstance() {
+		return instance;
+	}
+
+	private static String CONFIGURATION_FILE_PATH;
+
+	private Properties properties = new Properties();
+
+	static {
+		CONFIGURATION_FILE_PATH = System.getProperty("user.home");
+		CONFIGURATION_FILE_PATH += "/.FsnetConfiguration";
+		instance = new FSNetConfiguration();
+	}
+
+	private FSNetConfiguration() {
+		loadConfiguration();
+	}
+
+	public Properties getFSNetConfiguration() {
+		return properties;
+	}
+
+	public void save() {
+		File f = new File(CONFIGURATION_FILE_PATH);
+		try {
+			f.createNewFile();
+			properties.storeToXML(new FileOutputStream(f), null);
+		} catch (FileNotFoundException e) {
+			Logger.getAnonymousLogger().log(Level.SEVERE, "", e);
+		} catch (IOException e) {
+			Logger.getAnonymousLogger().log(Level.SEVERE, "", e);
+		}
+	}
+
+	private final void loadConfiguration() {
+		File conf = new File(CONFIGURATION_FILE_PATH);
+		// if no exists we create the file
+		if (!conf.exists()) {
+			try {
+				properties.storeToXML(new FileOutputStream(conf), null);
+			} catch (IOException e) {
+				Logger.getAnonymousLogger().log(Level.SEVERE, "", e);
+			}
+		} else {
+			try {
+				properties.loadFromXML(new FileInputStream(conf));
+			} catch (FileNotFoundException e) {
+				Logger.getAnonymousLogger().log(Level.SEVERE, "", e);
+			} catch (IOException e) {
+				Logger.getAnonymousLogger().log(Level.SEVERE, "", e);
+			}
+		}
+	}
+
+	public void reset() {
+		properties.clear();
+		save();
+	}
+
+	public void enableTLS() {
+		properties.setProperty(ENABLE_TLS, "true");
+	}
+
+	public void disableTLS() {
+		properties.setProperty(ENABLE_TLS, "false");
+	}
+
+	public boolean isTLSEnabled() {
+		return "true".equals(properties.getProperty(ENABLE_TLS));
+	}
+
+	public void enableSSL() {
+		properties.setProperty(SSL_KEY, SSL_ENABLED_VALUE);
+	}
+
+	public void disableSSL() {
+		properties.remove(SSL_KEY);
+	}
+
+	public boolean isSSLEnabled() {
+		return SSL_ENABLED_VALUE.equals(properties.getProperty(SSL_KEY));
+	}
+
+	public void enableAuthentication(String username, String password) {
+		properties.setProperty(ENABLE_AUTHENTICATION, "true");
+		properties.setProperty(SMTP_USER, username);
+		properties.setProperty(SMTP_PASSWORD, password);
+	}
+
+	public void disableAuthentication() {
+		properties.setProperty(ENABLE_AUTHENTICATION, "false");
+		properties.remove(SMTP_USER);
+		properties.remove(SMTP_PASSWORD);
+	}
+
+	public boolean isAuthenticationEnabled() {
+		return "true".equals(properties.getProperty(ENABLE_AUTHENTICATION));
+	}
+
+	public String getUsername() {
+		return properties.getProperty(SMTP_USER);
+	}
+
+	public String getPassword() {
+		return properties.getProperty(SMTP_PASSWORD);
+	}
+
+	public String getSMTPHost() {
+		return properties.getProperty(SMTP_HOST);
+	}
+
+	public void setSMTPHost(String SMTPHost) {
+		properties.setProperty(SMTP_HOST, SMTPHost);
+	}
+
+	public String getSMTPPort() {
+		return properties.getProperty(SMTP_PORT);
+	}
+
+	public void setSMTPPort(String SMTPPort) {
+		properties.setProperty(SMTP_PORT, SMTPPort);
+	}
+
+	public String getFrom() {
+		return properties.getProperty(MAIL_FROM);
+	}
+
+	public void setFrom(String mailAddress) {
+		properties.setProperty(MAIL_FROM, mailAddress);
+	}
+
+	public String getFSNetWebAddress() {
+		return properties.getProperty(FSNET_WEB_ADDRESS);
+	}
+
+	public void setFSNetWebAddress(String FSNetWebAddress) {
+		properties.setProperty(FSNET_WEB_ADDRESS, FSNetWebAddress);
+	}
+
+	public void setMailConfigured(boolean isConfigured) {
+		if (isConfigured) {
+			properties.setProperty(MAIl_CONFIGURATION_DONE, "true");
+		} else {
+			properties.setProperty(MAIl_CONFIGURATION_DONE, "false");
+		}
+	}
+
+	public boolean isMailConfigured() {
+		return "true".equals(properties.getProperty(MAIl_CONFIGURATION_DONE));
+	}
+
+}
