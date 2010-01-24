24fbdcaf0f77a9c64a021f6c6c870bf9b2aaecf3
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-24 23:40:50 +0000

    Added default SMTP port (25) if this field is not informed
    git-svn-id: http://fsnet.googlecode.com/svn@651 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ModifyOptions.java
+++ trunk/fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ModifyOptions.java
@@ -12,36 +12,46 @@ import org.apache.struts.action.DynaActionForm;
 import fr.univartois.ili.fsnet.admin.utils.FSNetConfiguration;
 
 public class ModifyOptions extends Action {
-	
+
+	private static final int DEFAULT_SMTP_PORT = 25;
+
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest req, HttpServletResponse arg3) throws Exception {
-		DynaActionForm dynaform = (DynaActionForm) form; 							//NOSONAR
+		DynaActionForm dynaform = (DynaActionForm) form; // NOSONAR
 		FSNetConfiguration conf = FSNetConfiguration.getInstance();
 		conf.reset();
-        if ("".equals(dynaform.get("enableTLS"))) {
-        	conf.disableTLS();
-        } else {
-        	conf.enableTLS();
-        }
-        if ("".equals(dynaform.get("enableSSL"))) {
-        	conf.disableSSL();
-        } else {
-        	conf.enableSSL();
-        }
-        if ("".equals(dynaform.get("authenticate"))) {
-        	conf.disableAuthentication();
-        } else {
-        	conf.enableAuthentication((String) dynaform.get("username"), (String) dynaform.get("motdepasse"));
-        }
-        
-        conf.setFrom((String) dynaform.get("hote"));
-        conf.setSMTPHost((String) dynaform.get("serveursmtp"));
-        conf.setSMTPPort((String) dynaform.get("port"));
-        conf.setFSNetWebAddress((String) dynaform.get("adressefsnet"));
-        conf.setMailConfigured(true);
-        conf.save();
-        
+		if ("".equals(dynaform.get("enableTLS"))) {
+			conf.disableTLS();
+		} else {
+			conf.enableTLS();
+		}
+		if ("".equals(dynaform.get("enableSSL"))) {
+			conf.disableSSL();
+		} else {
+			conf.enableSSL();
+		}
+		if ("".equals(dynaform.get("authenticate"))) {
+			conf.disableAuthentication();
+		} else {
+			conf.enableAuthentication((String) dynaform.get("username"),
+					(String) dynaform.get("motdepasse"));
+		}
+
+		conf.setFrom((String) dynaform.get("hote"));
+		conf.setSMTPHost((String) dynaform.get("serveursmtp"));
+		int port;
+		try {
+			port = Integer.parseInt((String) dynaform.get("port"));
+		} catch (NumberFormatException e) {
+			port = DEFAULT_SMTP_PORT;
+		}
+		conf.setSMTPPort(port);
+
+		conf.setFSNetWebAddress((String) dynaform.get("adressefsnet"));
+		conf.setMailConfigured(true);
+		conf.save();
+
 		return mapping.findForward("continue");
 	}
 }
