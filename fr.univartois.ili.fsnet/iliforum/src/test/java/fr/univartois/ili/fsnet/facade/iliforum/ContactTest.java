240e4c8ff81bb35283ffce3690d8e0138a75ef78
orel.legrand <orel.legrand@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-26 16:54:20 +0000

    create method facade for contact begin the tests
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@711 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/iliforum/ContactTest.java
@@ -0,0 +1,46 @@
+package fr.univartois.ili.fsnet.facade.iliforum;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.univartois.ili.fsnet.entities.SocialEntity;
+import fr.univartois.ili.fsnet.entities.test.utils.TestEntityManagerProvider;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.ContactFacade;
+
+public class ContactTest {
+
+	private EntityManager em;
+
+	@Before
+	public void setUp() {
+		em = TestEntityManagerProvider.getInstance().getEntityManager();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void acceptWithoutAskingTest() {
+		ContactFacade cf = new ContactFacade(em);
+		SocialEntity se1 = new SocialEntity("toto", "titi", "toto@gmail.com");
+		SocialEntity se2 = new SocialEntity("tata", "titi", "tata@gmail.com");
+		em.getTransaction().begin();
+		em.persist(se1);
+		em.persist(se2);
+		em.getTransaction().commit();
+		cf.acceptContact(se1, se2);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void alreadyAskedTest() {
+		ContactFacade cf = new ContactFacade(em);
+		SocialEntity se1 = new SocialEntity("toto", "titi", "titi@gmail.com");
+		SocialEntity se2 = new SocialEntity("tata", "titi", "taytaygmail.com");
+		em.getTransaction().begin();
+		em.persist(se1);
+		em.persist(se2);
+		em.getTransaction().commit();
+		cf.askContact(se1, se2);
+		cf.askContact(se2, se1);
+	}
+
+}
