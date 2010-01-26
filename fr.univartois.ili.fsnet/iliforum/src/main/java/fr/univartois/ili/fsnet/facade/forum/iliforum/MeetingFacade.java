2cca0a822d3ba13555149fa04d338e91c7a79f0e
micgamers <micgamers@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-26 15:39:25 +0000

    Start of MeetingFacade in iliforum
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@705 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/MeetingFacade.java
@@ -0,0 +1,37 @@
+package fr.univartois.ili.fsnet.facade.forum.iliforum;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+
+import fr.univartois.ili.fsnet.entities.Address;
+import fr.univartois.ili.fsnet.entities.Meeting;
+import fr.univartois.ili.fsnet.entities.SocialEntity;
+
+public class MeetingFacade {
+
+	private final EntityManager em;
+
+	MeetingFacade(EntityManager em) {
+		this.em = em;
+	}
+
+	public Meeting createMeeting(SocialEntity member, String eventName,
+			String eventDescription, Date endDate, Boolean isPrivate,
+			Date startDate, String address, String city) {
+
+		Meeting event = new Meeting(member, eventName, eventDescription,
+				endDate, isPrivate, startDate, new Address(address, city));
+
+		member.getInteractions().add(event);
+		em.persist(event);
+
+		return event;
+
+	}
+	
+	public Meeting deleteMeeting(){
+		return null;
+		
+	}
+}
