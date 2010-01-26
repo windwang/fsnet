58e6b6ec9953a1387abb4c186ace637861727c12
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-26 20:24:02 +0000

    iliforum : added hub facade
    git-svn-id: http://fsnet.googlecode.com/svn@715 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/TopicFacade.java
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/TopicFacade.java
@@ -4,7 +4,6 @@ import fr.univartois.ili.fsnet.entities.Hub;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.entities.Topic;
 import java.util.List;
-import javax.management.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
@@ -28,9 +27,6 @@ public class TopicFacade {
      * @return the created and persisted Topic
      */
     public final Topic createTopic(Hub hub, SocialEntity creator, String title) {
-        if (hub == null || creator == null || title == null) {
-            throw new IllegalArgumentException();
-        }
         Topic topic = new Topic(hub, creator, title);
         em.persist(topic);
         hub.getTopics().add(topic);
@@ -57,6 +53,12 @@ public class TopicFacade {
         return em.find(Topic.class, id);
     }
 
+    /**
+     * Search Topics in the given Hub
+     * @param pattern the pattern to search
+     * @param hub the hub to search in
+     * @return a list of topics
+     */
     public List<Topic> searchTopic(String pattern, Hub hub) {
         if (pattern == null || hub == null) {
             throw new IllegalArgumentException();
@@ -64,6 +66,20 @@ public class TopicFacade {
         TypedQuery<Topic> query = em.createQuery("SELECT topic FROM Topic topic WHERE topic.title LIKE :pattern AND topic.hub = :hub ", Topic.class);
         query.setParameter("pattern", "%" + pattern + "%");
         query.setParameter("hub", hub);
+        return query.getResultList();
+    }
+
+    /**
+     * Search Topics
+     * @param pattern the pattern to search
+     * @return a list of topics
+     */
+    public List<Topic> searchTopic(String pattern) {
+        if (pattern == null) {
+            throw new IllegalArgumentException();
+        }
+        TypedQuery<Topic> query = em.createQuery("SELECT topic FROM Topic topic WHERE topic.title LIKE :pattern", Topic.class);
+        query.setParameter("pattern", "%" + pattern + "%");
         return query.getResultList();
     }
 }
