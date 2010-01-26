c3631dae0044b7a2670ab1a9dfa76b5fffaa3587
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-26 18:09:19 +0000

    iliforum : Added persistence.xml in test resources in order to run tests
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@712 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/iliforum/SocialEntityFacadeTest.java
+++ fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/iliforum/SocialEntityFacadeTest.java
@@ -11,44 +11,46 @@ import org.junit.Before;
 import org.junit.Test;
 
 import fr.univartois.ili.fsnet.entities.SocialEntity;
-import fr.univartois.ili.fsnet.entities.test.utils.TestEntityManagerProvider;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.SocialEntityFacade;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.SocialEntityFacade.SearchResult;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class SocialEntityFacadeTest {
-	private EntityManager em;
-	private SocialEntityFacade sef;
 
-	@Before
-	public void setUp() {
-		em = TestEntityManagerProvider.getInstance().getEntityManager();
-		sef = new SocialEntityFacade(em);
-	}
+    private EntityManager em;
+    private SocialEntityFacade sef;
 
-	@Test
-	public void testCreate() {
-		String name = "name";
-		String firstName = "first";
-		String email = "email@email.com";
-		SocialEntity es = sef.createSocialEntity(name, firstName, email);
-		SocialEntity esp = em.find(SocialEntity.class, es.getId());
-		assertEquals(esp.getName(), es.getName());
-		assertEquals(esp.getFirstName(), es.getFirstName());
-		assertEquals(esp.getEmail(), es.getEmail());
-	}
+    @Before
+    public void setUp() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TestPU");
+        em = emf.createEntityManager();
+        sef = new SocialEntityFacade(em);
+    }
 
-	@Test
-	public void testSearch() {
-		SocialEntity ent1 = new SocialEntity("zaza", "zaza", "zaza@gmail.com");
-		SocialEntity ent2 = new SocialEntity("zozo", "zozo", "zaza@gmail.com");
-		SocialEntity ent3 = new SocialEntity("zozi", "zozi", "zizi@gmail.com");
-		String searchText = "zo";
-		HashMap<SearchResult, List<SocialEntity>> result = sef.searchSocialEntity(searchText, ent1.getId());
-		List<SocialEntity> l = result.get("Others");
-		SocialEntity ent21 = l.get(0);
-		assertEquals(ent2.getName(), ent21.getName());
-		assertEquals(ent2.getFirstName(), ent21.getFirstName());
-		assertEquals(ent2.getEmail(), ent21.getEmail());
-	}
+    @Test
+    public void testCreate() {
+        String name = "name";
+        String firstName = "first";
+        String email = "email@email.com";
+        SocialEntity es = sef.createSocialEntity(name, firstName, email);
+        SocialEntity esp = em.find(SocialEntity.class, es.getId());
+        assertEquals(esp.getName(), es.getName());
+        assertEquals(esp.getFirstName(), es.getFirstName());
+        assertEquals(esp.getEmail(), es.getEmail());
+    }
 
+    @Test
+    public void testSearch() {
+        SocialEntity ent1 = new SocialEntity("zaza", "zaza", "zaza@gmail.com");
+        SocialEntity ent2 = new SocialEntity("zozo", "zozo", "zaza@gmail.com");
+        SocialEntity ent3 = new SocialEntity("zozi", "zozi", "zizi@gmail.com");
+        String searchText = "zo";
+        HashMap<SearchResult, List<SocialEntity>> result = sef.searchSocialEntity(searchText, ent1.getId());
+        List<SocialEntity> l = result.get("Others");
+        SocialEntity ent21 = l.get(0);
+        assertEquals(ent2.getName(), ent21.getName());
+        assertEquals(ent2.getFirstName(), ent21.getFirstName());
+        assertEquals(ent2.getEmail(), ent21.getEmail());
+    }
 }
