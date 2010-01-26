b561e8d0d53e14a8a932d3e1da9cb1afb7ecff35
mehdi.benzaghar <mehdi.benzaghar@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-26 14:30:58 +0000

    add new project admin
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@694 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fr.univartois.ili.fsnet/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ManageConfiguration.java
@@ -0,0 +1,55 @@
+package fr.univartois.ili.fsnet.admin.actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class ManageConfiguration implements CrudAction {
+
+	@Override
+	public ActionForward create(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward delete(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward display(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward modify(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward search(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
