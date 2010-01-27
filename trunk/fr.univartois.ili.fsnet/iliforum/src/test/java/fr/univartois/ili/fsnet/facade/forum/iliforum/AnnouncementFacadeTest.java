abf9d51aa086a31466af5cb7ab53dbf71daedcf8
micgamers <micgamers@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-27 16:49:15 +0000

    Add AnnoucementFacade and tests
    git-svn-id: http://fsnet.googlecode.com/svn@737 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/forum/iliforum/AnnouncementFacadeTest.java
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/forum/iliforum/AnnouncementFacadeTest.java
@@ -1,5 +1,9 @@
 package fr.univartois.ili.fsnet.facade.forum.iliforum;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,10 +11,15 @@ import javax.persistence.Persistence;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.univartois.ili.fsnet.entities.Announcement;
+import fr.univartois.ili.fsnet.entities.Meeting;
+import fr.univartois.ili.fsnet.entities.SocialEntity;
+
 public class AnnouncementFacadeTest {
 
 	private EntityManager em;
 	private AnnouncementFacade af;
+	private SocialEntityFacade sef;
 
 	@Before
 	public void setUp() {
@@ -18,10 +27,29 @@ public class AnnouncementFacadeTest {
 				.createEntityManagerFactory("TestPU");
 		em = emf.createEntityManager();
 		af = new AnnouncementFacade(em);
+		sef = new SocialEntityFacade(em);
+	}
+
+	@Test
+	public void testCreate() {
+		SocialEntity member = sef.createSocialEntity("zaza", "zaza",
+				"zaza@gmail.com");
+		String eventName = "eventName";
+		String eventDescription = "eventDescription";
+		Date endDate = new Date();
+		Boolean isPrivate = false;
+
+		Announcement ann = af.createAnnouncement(member, eventName,
+				eventDescription, endDate, isPrivate);
+		Announcement annp = em.find(Announcement.class, ann.getId());
+		assertEquals(annp.getCreator(), ann.getCreator());
+		assertEquals(annp.getTitle(), ann.getTitle());
+		assertEquals(annp.getContent(), ann.getContent());
+		assertEquals(annp.getEndDate(), ann.getEndDate());
 	}
 	
 	@Test
-	public void testCreate() {
+	public void testSearch() {
 		
 	}
 }
