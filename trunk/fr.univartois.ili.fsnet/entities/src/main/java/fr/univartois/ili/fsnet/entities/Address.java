369e23cff6c5730f3159dab388069ab98855fad4
mehdi.benabdessalem <mehdi.benabdessalem@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-27 15:27:44 +0000

    Added method equals
    git-svn-id: http://fsnet.googlecode.com/svn@731 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/entities/src/main/java/fr/univartois/ili/fsnet/entities/Address.java
+++ trunk/fr.univartois.ili.fsnet/entities/src/main/java/fr/univartois/ili/fsnet/entities/Address.java
@@ -59,4 +59,42 @@ public class Address implements Serializable {
     public void setCity(String city) {
         this.city = city;
     }
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		return true;
+	}
+    
 }
