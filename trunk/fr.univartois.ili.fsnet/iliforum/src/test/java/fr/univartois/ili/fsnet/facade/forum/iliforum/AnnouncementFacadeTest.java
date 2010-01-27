68e356e4cd6155e41357832181d708b60f66fc35
micgamers <micgamers@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-27 21:25:12 +0000

    Add Tests create,delete and search in AnnouncementFacadeTest
    git-svn-id: http://fsnet.googlecode.com/svn@753 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/forum/iliforum/AnnouncementFacadeTest.java
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/forum/iliforum/AnnouncementFacadeTest.java
@@ -1,8 +1,10 @@
 package fr.univartois.ili.fsnet.facade.forum.iliforum;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -37,16 +39,63 @@ public class AnnouncementFacadeTest {
         Date endDate = new Date();
         Boolean isPrivate = false;
 
-        Announcement ann = af.createAnnouncement(member, eventName,
-                eventDescription, endDate, isPrivate);
-        Announcement annp = em.find(Announcement.class, ann.getId());
-        assertEquals(annp.getCreator(), ann.getCreator());
-        assertEquals(annp.getTitle(), ann.getTitle());
-        assertEquals(annp.getContent(), ann.getContent());
-        assertEquals(annp.getEndDate(), ann.getEndDate());
-    }
+		Announcement ann = af.createAnnouncement(member, eventName,
+				eventDescription, endDate, isPrivate);
+		Announcement annp = em.find(Announcement.class, ann.getId());
+		assertEquals(annp.getCreator(), ann.getCreator());
+		assertEquals(annp.getTitle(), ann.getTitle());
+		assertEquals(annp.getContent(), ann.getContent());
+		assertEquals(annp.getEndDate(), ann.getEndDate());
+	}
 
-    @Test
-    public void testSearch() {
-    }
+	@Test
+	public void testSearch() {
+		Date end = new Date();
+		em.getTransaction().begin();
+		SocialEntity member3 = sef.createSocialEntity("zaza", "zaza",
+				"zaza1@gmail.com");
+		Announcement ann1 = af.createAnnouncement(member3, "tata", "tete", end,
+				false);
+
+		SocialEntity member2 = sef.createSocialEntity("zaza", "zaza",
+				"zaza2@gmail.com");
+		Announcement ann2 = af.createAnnouncement(member2, "titi", "toto", end,
+				false);
+
+		SocialEntity member = sef.createSocialEntity("zaza", "zaza",
+				"zaza3@gmail.com");
+		Announcement ann3 = af.createAnnouncement(member, "tutu", "tyty", end,
+				false);
+
+		em.getTransaction().commit();
+		String searchText = "titi";
+		List<Announcement> results = af.searchAnnouncement(searchText);
+		Announcement aRes = results.get(0);
+		assertEquals(ann2.getTitle(), aRes.getTitle());
+		assertEquals(ann2.getContent(), aRes.getContent());
+	}
+	
+	@Test
+	public void testDelete() {
+		Date end = new Date();
+		em.getTransaction().begin();
+		SocialEntity member = sef.createSocialEntity("zaza", "zaza",
+				"zaza4@gmail.com");
+		Announcement ann1 = af.createAnnouncement(member, "tata", "tete", end, false);
+		SocialEntity member2 = sef.createSocialEntity("zaza", "zaza",
+				"zaza5@gmail.com");
+		Announcement ann2 = af.createAnnouncement(member2, "titi", "toto", end, false);
+		SocialEntity member3 = sef.createSocialEntity("zaza", "zaza",
+				"zaza6@gmail.com");
+		Announcement ann3 = af.createAnnouncement(member3, "tutu", "tyty", end, false);
+
+		em.getTransaction().commit();
+
+		em.getTransaction().begin();
+
+		af.deleteAnnouncement(ann2.getId());
+		em.getTransaction().commit();
+		assertNull(em.find(Announcement.class, ann2.getId()));
+
+	}
 }
