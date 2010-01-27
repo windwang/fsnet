b8f11dd58612ba094dc89610416187516b5ff222
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-27 21:05:47 +0000

    iliforum : Adding javadoc and formating
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@751 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/ProfileFacade.java
+++ fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/ProfileFacade.java
@@ -9,11 +9,6 @@ import javax.persistence.EntityManager;
 import fr.univartois.ili.fsnet.entities.Address;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 
-
-
-
-
-
 /**
  * 
  * profile facade
@@ -22,57 +17,56 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
  */
 public class ProfileFacade {
 
-	private EntityManager em;
-	private static Logger loger = Logger.getAnonymousLogger();
-	
-	/**
-	 * create a facade profile
-	 * @param em entity manager to use
-	 */
-	public ProfileFacade(EntityManager em){
-		this.em=em;
-	}
-	
-	/**
-	 * change the password from a user
-	 * @param socialEntity user
-	 * @param old old password
-	 * @param newPassword new password
-	 * @return true if success
-	 */
-	public synchronized boolean changePassword(SocialEntity socialEntity,String old,String newPassword){
-		if(!old.equals(socialEntity.getPassword())){
-			loger.log(Level.SEVERE,"Formular validation error");
-			return false;
-		}
-		socialEntity.setPassword(newPassword);
+    private EntityManager em;
+    private static Logger loger = Logger.getAnonymousLogger();
+
+    /**
+     * create a facade profile
+     * @param em entity manager to use
+     */
+    public ProfileFacade(EntityManager em) {
+        this.em = em;
+    }
+
+    /**
+     * change the password from a user
+     * @param socialEntity user
+     * @param old old password
+     * @param newPassword new password
+     * @return true if success
+     */
+    public synchronized boolean changePassword(SocialEntity socialEntity, String old, String newPassword) {
+        if (!old.equals(socialEntity.getPassword())) {
+            loger.log(Level.SEVERE, "Formular validation error");
+            return false;
+        }
+        socialEntity.setPassword(newPassword);
         em.merge(socialEntity);
         return true;
-	}
-	
-	/**
-	 * change an social Entity profile 
-	 * @param socialEntity the social entity
-	 * @param name his name
-	 * @param firstName his firstnaem
-	 * @param address his adress
-	 * @param dateOfBirth his birth day
-	 * @param sexe his sex
-	 * @param job his job
-	 * @param mail his mail
-	 * @param phone his phone
-	 */
-	public synchronized void editProfile(SocialEntity socialEntity ,String name , String firstName ,Address address ,Date dateOfBirth ,
-			String sexe, String job , String mail , String phone){
-		socialEntity.setName(name);
-		socialEntity.setPrenom(firstName);
-		socialEntity.setAddress(address);
-		socialEntity.setBirthDate(dateOfBirth);
-		socialEntity.setSex(sexe);
-		socialEntity.setProfession(job);
-		socialEntity.setEmail(mail);
-		socialEntity.setPhone(phone);
-		em.merge(socialEntity);
-	}
-	
+    }
+
+    /**
+     * change an social Entity profile
+     * @param socialEntity the social entity
+     * @param name his name
+     * @param firstName his firstnaem
+     * @param address his adress
+     * @param dateOfBirth his birth day
+     * @param sexe his sex
+     * @param job his job
+     * @param mail his mail
+     * @param phone his phone
+     */
+    public synchronized void editProfile(SocialEntity socialEntity, String name, String firstName, Address address, Date dateOfBirth,
+            String sexe, String job, String mail, String phone) {
+        socialEntity.setName(name);
+        socialEntity.setPrenom(firstName);
+        socialEntity.setAddress(address);
+        socialEntity.setBirthDate(dateOfBirth);
+        socialEntity.setSex(sexe);
+        socialEntity.setProfession(job);
+        socialEntity.setEmail(mail);
+        socialEntity.setPhone(phone);
+        em.merge(socialEntity);
+    }
 }
