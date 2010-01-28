a5bc4c18f29b7db2e66324fe62cac5beaae0ab70
mehdi.benabdessalem <mehdi.benabdessalem@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-28 14:18:58 +0000

    Added SocialEntityFacade and InterestFacade Tests
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@764 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/forum/iliforum/SocialEntityFacadeTest.java
+++ fr.univartois.ili.fsnet/iliforum/src/test/java/fr/univartois/ili/fsnet/facade/forum/iliforum/SocialEntityFacadeTest.java
@@ -1,5 +1,9 @@
 package fr.univartois.ili.fsnet.facade.forum.iliforum;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,41 +11,98 @@ import javax.persistence.Persistence;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.univartois.ili.fsnet.entities.Interest;
+import fr.univartois.ili.fsnet.entities.SocialEntity;
+
 public class SocialEntityFacadeTest {
 
-    private EntityManager em;
-    private SocialEntityFacade sef;
+	private EntityManager em;
+	private SocialEntityFacade sef;
 
-    @Before
-    public void setUp() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TestPU");
-        em = emf.createEntityManager();
-        sef = new SocialEntityFacade(em);
-    }
+	@Before
+	public void setUp() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("TestPU");
+		em = emf.createEntityManager();
+		sef = new SocialEntityFacade(em);
+	}
 
-    @Test
-    public void testCreate() {
-//        String name = "name";
-//        String firstName = "first";
-//        String email = "email@email.com";
-//        SocialEntity es = sef.createSocialEntity(name, firstName, email);
-//        SocialEntity esp = em.find(SocialEntity.class, es.getId());
-//        assertEquals(esp.getName(), es.getName());
-//        assertEquals(esp.getFirstName(), es.getFirstName());
-//        assertEquals(esp.getEmail(), es.getEmail());
-    }
+	@Test
+	public void testCreate() {
+		String name = "name";
+		String firstName = "first";
+		String email = "email@email.com";
+		em.getTransaction().begin();
+		SocialEntity es = sef.createSocialEntity(name, firstName, email);
+		SocialEntity esp = em.find(SocialEntity.class, es.getId());
+		em.getTransaction().commit();
+		assertEquals(es, esp);
+	}
 
-    @Test
-    public void testSearch() {
-//        SocialEntity ent1 = new SocialEntity("zaza", "zaza", "zaza@gmail.com");
-//        SocialEntity ent2 = new SocialEntity("zozo", "zozo", "zaza@gmail.com");
-//        SocialEntity ent3 = new SocialEntity("zozi", "zozi", "zizi@gmail.com");
-//        String searchText = "zo";
-//        HashMap<SearchResult, List<SocialEntity>> result = sef.searchSocialEntity(searchText, ent1.getId());
-//        List<SocialEntity> l = result.get(SearchResult.Contacts);
-//        SocialEntity ent21 = l.get(0);
-//        assertEquals(ent2.getName(), ent21.getName());
-//        assertEquals(ent2.getFirstName(), ent21.getFirstName());
-//        assertEquals(ent2.getEmail(), ent21.getEmail());
-    }
+	@Test
+	public void testGetSocialEntity() {
+		String name = "aaaaa";
+		String firstName = "bbbbbb";
+		String email = "ab@email.com";
+		em.getTransaction().begin();
+		SocialEntity es = sef.createSocialEntity(name, firstName, email);
+		SocialEntity esp = sef.getSocialEntity(es.getId());
+		em.getTransaction().commit();
+		assertEquals(es, esp);
+	}
+
+	@Test
+	public void testDeleteSocialEntity(){
+		String name = "cc";
+		String firstName = "dd";
+		String email = "cd@email.com";
+		em.getTransaction().begin();
+		SocialEntity es = sef.createSocialEntity(name, firstName, email);
+		sef.deleteSocialEntity(es.getId());
+		em.getTransaction().commit();
+		assertNull(em.find(SocialEntity.class, es.getId()));
+	}
+
+	@Test
+	public void testSearchSocialEntity(){
+		//TODO test the SerachSocialEntity method
+	}
+
+	@Test
+	public void testSearchSocialEntitys(){
+		//TODO test the SerachSocialEntitys method
+	}
+
+	@Test
+	public void testAddInterest(){
+		InterestFacade interestFacade = new InterestFacade(em);
+		String interestName = "interest";
+		String name = "ss";
+		String firstName = "zz";
+		String email = "sz@email.com";
+		em.getTransaction().begin();
+		SocialEntity es = sef.createSocialEntity(name, firstName, email);
+		Interest interest = interestFacade.createInterest(interestName);
+		sef.addInterest(interest.getId(), es.getId());
+		em.getTransaction().commit();
+		assertEquals(1, es.getInterests().size());
+		assertTrue(es.getInterests().contains(interest));
+	}
+
+	@Test
+	public void testRemoveInterest(){
+		InterestFacade interestFacade = new InterestFacade(em);
+		String interestName = "fun";
+		String name = "gg";
+		String firstName = "kk";
+		String email = "gk@email.com";
+		em.getTransaction().begin();
+		SocialEntity es = sef.createSocialEntity(name, firstName, email);
+		Interest interest = interestFacade.createInterest(interestName);
+		sef.addInterest(interest.getId(), es.getId());
+		sef.removeInterest(interest.getId(), es.getId());
+		em.getTransaction().commit();
+		assertEquals(0, es.getInterests().size());
+		assertTrue(!es.getInterests().contains(interest));
+	}
+
 }
