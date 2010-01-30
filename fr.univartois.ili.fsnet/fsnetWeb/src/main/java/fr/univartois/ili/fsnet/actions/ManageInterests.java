b45915c6124fe222e98b15943b7350e9745f5e95
alexandre.lohez <alexandre.lohez@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-01-30 16:17:05 +0000

    Add new feature : paginated results for interests research
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@795 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageInterests.java
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageInterests.java
@@ -27,165 +27,214 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.InterestFacade;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.SocialEntityFacade;
 
-
 /**
  * Execute CRUD Actions (and more) for the entity interet
  * 
  * @author Alexandre Lohez <alexandre.lohez at gmail.com>
  */
 public class ManageInterests extends MappingDispatchAction implements
-        CrudAction {
+		CrudAction {
 
-    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
-    private static final Logger logger = Logger.getAnonymousLogger();
+	private static EntityManagerFactory factory = Persistence
+			.createEntityManagerFactory("fsnetjpa");
+	private static final Logger logger = Logger.getAnonymousLogger();
 
-    @Override
-    public ActionForward create(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        EntityManager em = factory.createEntityManager();
-        DynaActionForm dynaForm = (DynaActionForm) form; //NOSONAR
-        InterestFacade facade = new InterestFacade(em);
-        String interestName = (String) dynaForm.get("createdInterestName");
+	private static final int NB_RESULTS_ON_DEMAND = 6;
+	private static final int NB_RESULTS_RETURNED = NB_RESULTS_ON_DEMAND -1;
+	
+	@Override
+	public ActionForward create(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
+		DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
+		InterestFacade facade = new InterestFacade(em);
+		String interestName = (String) dynaForm.get("createdInterestName");
 
-        logger.info("new interest: " + interestName);
+		logger.info("new interest: " + interestName);
 
-        try {
-            em.getTransaction().begin();
-            facade.createInterest(interestName);
-            em.getTransaction().commit();
-        } catch (RollbackException ex) {
-            ActionErrors actionErrors = new ActionErrors();
-            ActionMessage msg = new ActionMessage("interest.alreadyExists");
-            actionErrors.add("createdInterestName", msg);
-            saveErrors(request, actionErrors);
-        }
+		try {
+			em.getTransaction().begin();
+			facade.createInterest(interestName);
+			em.getTransaction().commit();
+		} catch (RollbackException ex) {
+			ActionErrors actionErrors = new ActionErrors();
+			ActionMessage msg = new ActionMessage("interest.alreadyExists");
+			actionErrors.add("createdInterestName", msg);
+			saveErrors(request, actionErrors);
+		}
 
-        em.close();
+		em.close();
 
-        return mapping.findForward("success");
-    }
+		return mapping.findForward("success");
+	}
 
-    public ActionForward add(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        EntityManager em = factory.createEntityManager();
-        DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-        int interestId = Integer.valueOf((String) dynaForm.get("addedInterestId"));
+	public ActionForward add(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
+		DynaActionForm dynaForm = (DynaActionForm) form;// NOSONAR
+		int interestId = Integer.valueOf((String) dynaForm
+				.get("addedInterestId"));
 
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        SocialEntityFacade facade = new SocialEntityFacade(em);
-        logger.info("add interest: id=" + interestId + " for user: "
-                + user.getName() + " " + user.getFirstName() + " " + user.getId());
-       
-        em.getTransaction().begin();
-        facade.addInterest(interestId, user.getId());
-        em.getTransaction().commit();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		SocialEntityFacade facade = new SocialEntityFacade(em);
+		logger.info("add interest: id=" + interestId + " for user: "
+				+ user.getName() + " " + user.getFirstName() + " "
+				+ user.getId());
 
-        em.close();
+		em.getTransaction().begin();
+		facade.addInterest(interestId, user.getId());
+		em.getTransaction().commit();
 
-        return mapping.findForward("success");
-    }
+		em.close();
 
-    public ActionForward remove(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        EntityManager em = factory.createEntityManager();
+		return mapping.findForward("success");
+	}
 
-        int interestId;
+	public ActionForward remove(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
 
-        if (request.getParameterMap().containsKey("removedInterestId")) {
-            interestId = Integer.valueOf(request.getParameter("removedInterestId"));
+		int interestId;
 
-        } else {
-            DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-            interestId = Integer.valueOf((String) dynaForm.get("removedInterestId"));
-        }
+		if (request.getParameterMap().containsKey("removedInterestId")) {
+			interestId = Integer.valueOf(request
+					.getParameter("removedInterestId"));
 
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        SocialEntityFacade facade = new SocialEntityFacade(em);
-        logger.info("remove interest: id=" + interestId + " for user: "
-                + user.getName() + " " + user.getFirstName() + " " + user.getId());
+		} else {
+			DynaActionForm dynaForm = (DynaActionForm) form;// NOSONAR
+			interestId = Integer.valueOf((String) dynaForm
+					.get("removedInterestId"));
+		}
 
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		SocialEntityFacade facade = new SocialEntityFacade(em);
+		logger.info("remove interest: id=" + interestId + " for user: "
+				+ user.getName() + " " + user.getFirstName() + " "
+				+ user.getId());
 
-        em.getTransaction().begin();
-        facade.removeInterest(interestId, user.getId());
-        em.getTransaction().commit();
+		em.getTransaction().begin();
+		facade.removeInterest(interestId, user.getId());
+		em.getTransaction().commit();
 
+		em.close();
 
-        em.close();
+		return mapping.findForward("success");
+	}
 
-        return mapping.findForward("success");
-    }
+	@Override
+	public ActionForward modify(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		return null;
+	}
 
-    @Override
-    public ActionForward modify(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        return null;
-    }
+	@Override
+	public ActionForward delete(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		return null;
+	}
 
-    @Override
-    public ActionForward delete(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-       return null;
-    }
+	@Override
+	public ActionForward search(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
+		DynaActionForm dynaForm = (DynaActionForm) form;// NOSONAR
+		String interestName = "";
+		InterestFacade facade = new InterestFacade(em);
+		
+		if (dynaForm.get("searchInterestName") != null) {
+			interestName = (String) dynaForm.get("searchInterestName");
+		}
+		
+		logger.info("search interest: " + interestName);
+		
+		List<Interest> result = facade.searchInterest(interestName, 1, NB_RESULTS_ON_DEMAND);
+		em.close();
+		
+		if (result.size() == NB_RESULTS_ON_DEMAND) {
+			result.remove(result.size()-1);
+			request.setAttribute("hasnext", result);
+		}
+		request.setAttribute("interestResult", result);
+		request.setAttribute("currentPage", 0);
+		request.setAttribute("currentSearch", interestName);
 
-    @Override
-    public ActionForward search(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        EntityManager em = factory.createEntityManager();
-        DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-        String interestName = "";
+		return mapping.findForward("success");
+	}
 
-        if (dynaForm.get("searchInterestName") != null) {
-            interestName = (String) dynaForm.get("searchInterestName");
-        }
-        InterestFacade facade = new InterestFacade(em);
-        logger.info("search interest: " + interestName);
+	public ActionForward advancedSearch(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
+		String interestName = "";
 
-        List<Interest> result = facade.searchInterest(interestName);
-        em.close();
+		if (request.getParameterMap().containsKey("search")) {
+			interestName = request.getParameter("search");
+		}
 
-        request.setAttribute("interestResult", result);
+		InterestFacade facade = new InterestFacade(em);
+		logger.info("advanced search interest: " + interestName);
+						
+		int page = 0;
+		
+		if (request.getParameterMap().containsKey("page")) {
+			page = Integer.valueOf(request.getParameter("page"));
+			if (page < 0) {
+				page = 0;
+			}
+		}
+		
+		List<Interest> result = facade.searchInterest(interestName, page * NB_RESULTS_RETURNED + 1, NB_RESULTS_ON_DEMAND);
+		em.close();
 
-        return mapping.findForward("success");
-    }
+		if (result.size() == NB_RESULTS_ON_DEMAND) {
+			result.remove(result.size()-1);
+			request.setAttribute("hasnext", result);
+		}
+		request.setAttribute("interestResult", result);
+		request.setAttribute("currentPage", page);
+		request.setAttribute("currentSearch", interestName);
 
-    @Override
-    public ActionForward display(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        EntityManager em = factory.createEntityManager();
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        InterestFacade facade = new InterestFacade(em);
-        logger.info("Displaying interests");
+		return mapping.findForward("success");
+	}
 
-        // TODO requete provisoire
+	@Override
+	public ActionForward display(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		InterestFacade facade = new InterestFacade(em);
+		logger.info("Displaying interests");
 
-        List<Interest> listAllInterests = facade.getInterests();
+		// TODO requete provisoire
 
-        List<Interest> finalList = new ArrayList<Interest>();
-        boolean dirtyIsOK;
-        for (Interest interest : listAllInterests) {
-            dirtyIsOK = true;
-            for (Interest interestEntity : user.getInterests()) {
-                if (interestEntity.getId() == interest.getId()) {
-                    dirtyIsOK = false;
-                }
-            }
-            if (dirtyIsOK) {
-                finalList.add(interest);
-            }
-        }
+		List<Interest> listAllInterests = facade.getInterests();
+		em.close();
 
-        request.setAttribute("user", user);
-        request.setAttribute("allInterests", listAllInterests);
-        request.setAttribute("listInterests", finalList);
+		List<Interest> finalList = new ArrayList<Interest>();
+		boolean dirtyIsOK;
+		for (Interest interest : listAllInterests) {
+			dirtyIsOK = true;
+			for (Interest interestEntity : user.getInterests()) {
+				if (interestEntity.getId() == interest.getId()) {
+					dirtyIsOK = false;
+				}
+			}
+			if (dirtyIsOK) {
+				finalList.add(interest);
+			}
+		}
 
-        em.close();
-        return mapping.findForward("success");
-    }
+		request.setAttribute("user", user);
+		request.setAttribute("allInterests", listAllInterests);
+		request.setAttribute("listInterests", finalList);
+
+		return mapping.findForward("success");
+	}
 }
--- fr.univartois.ili.fsnet/fsnetWeb/src/main/webapp/content/interests/ResultInterest.jsp
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/webapp/content/interests/ResultInterest.jsp
@@ -3,6 +3,8 @@
 
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@taglib uri="http://struts.apache.org/tags-bean" prefix="bean"%>
+<%@taglib uri="http://struts.apache.org/tags-logic" prefix="logic"%>
+<%@taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
 
 <c:choose>
     <c:when test="${not empty requestScope.interestResult}">
@@ -11,6 +13,24 @@
                 <li>${interest.name}</li>
             </c:forEach>
         </ul>
+        <logic:present name="currentSearch" scope="request">
+	        <logic:present name="currentPage" scope="request">
+				<c:if test="${requestScope.currentPage > 0}">
+					<html:link styleClass="button" action="/AdvancedSearchInterest">
+						<html:param name="page" value="${requestScope.currentPage-1}"/>
+					 	<html:param name="search" value="${requestScope.currentSearch}"/>
+						<bean:message key="interests.12"/>
+					</html:link>
+				</c:if>
+				<logic:present name="hasnext" scope="request">
+					<html:link styleClass="button" action="/AdvancedSearchInterest">
+						<html:param name="page" value="${requestScope.currentPage+1}"/>
+					 	<html:param name="search" value="${requestScope.currentSearch}"/>
+						<bean:message key="interests.13"/>
+					</html:link>
+				</logic:present>
+			</logic:present>
+		</logic:present>
     </c:when>
     <c:otherwise>
         <bean:message key="interests.10"/>
