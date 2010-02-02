f54260099fe33383307d0694d5eb2b6ed27083f9
micgamers <micgamers@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-02 10:30:28 +0000

    Correction TODO NPE
    git-svn-id: http://fsnet.googlecode.com/svn@810 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/AnnouncementFacade.java
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/AnnouncementFacade.java
@@ -13,79 +13,92 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
  */
 public class AnnouncementFacade {
 
-    
-    private final EntityManager em;
+	private final EntityManager em;
 
-    /**
-     * Contructor
-     * @param em
-     */
-    public AnnouncementFacade(EntityManager em) {
-        this.em = em;
-    }
+	/**
+	 * Contructor
+	 * 
+	 * @param em
+	 */
+	public AnnouncementFacade(EntityManager em) {
+		this.em = em;
+	}
 
-    /**
-     * Create a New Announcement
-     * @param member
-     * @param annName
-     * @param annDescription
-     * @param endDate
-     * @param isPrivate
-     * @return the new Announcement
-     */
-    public final Announcement createAnnouncement(SocialEntity member, String annName,
-            String annDescription, Date endDate, Boolean isPrivate) {
-        Announcement announce = new Announcement(member, annName,
-                annDescription, endDate, isPrivate);
+	/**
+	 * Create a New Announcement
+	 * 
+	 * @param member
+	 * @param annName
+	 * @param annDescription
+	 * @param endDate
+	 * @param isPrivate
+	 * @return the new Announcement
+	 */
+	public final Announcement createAnnouncement(SocialEntity member,
+			String annName, String annDescription, Date endDate,
+			Boolean isPrivate) {
+		Announcement announce = new Announcement(member, annName,
+				annDescription, endDate, isPrivate);
 
-        em.persist(announce);
+		em.persist(announce);
 
-        return announce;
-    }
+		return announce;
+	}
 
-    
-    /**
-     * Modify the Announcement
-     * @param idAnnounce
-     * @param annName
-     * @param annDescription
-     * @param endDate
-     * @return the Announcement modify
-     */
-    public final Announcement modifyAnnouncement(int idAnnounce, String annName,
-            String annDescription, Date endDate) {
-        //TODO NPE
-        Announcement announce = em.find(Announcement.class, idAnnounce);
-        announce.setTitle(annName);
-        announce.setContent(annDescription);
-        announce.setEndDate(endDate);
-        em.merge(announce);
-        return announce;
-    }
+	/**
+	 * Modify the Announcement
+	 * 
+	 * @param idAnnounce
+	 * @param annName
+	 * @param annDescription
+	 * @param endDate
+	 * @return the Announcement modify
+	 */
+	public final Announcement modifyAnnouncement(int idAnnounce,
+			String annName, String annDescription, Date endDate) {
+		if (annName == null || annDescription == null || endDate == null) {
+			throw new IllegalArgumentException();
+		}
+		Announcement announce = em.find(Announcement.class, idAnnounce);
+		if (announce == null) {
+			throw new IllegalArgumentException();
+		}
+		announce.setTitle(annName);
+		announce.setContent(annDescription);
+		announce.setEndDate(endDate);
+		em.merge(announce);
+		return announce;
+	}
 
-    /**
-     * Delete the Announcement with the id of the Announcement AnnouncementId
-     * 
-     * @param idAnnounce
-     */
-    public final void deleteAnnouncement(int idAnnounce) {
-        Announcement announce = em.find(Announcement.class, idAnnounce);
-        em.remove(announce);
-        em.flush();
-    }
+	/**
+	 * Delete the Announcement with the id of the Announcement AnnouncementId
+	 * 
+	 * @param idAnnounce
+	 */
+	public final void deleteAnnouncement(int idAnnounce) {
+		Announcement announce = em.find(Announcement.class, idAnnounce);
+		em.remove(announce);
+		em.flush();
+	}
 
-    /**
-     * Search the Announcement with the param textSearchAnnounce
-     * @param textSearchAnnounce
-     * @return a list of Announcement
-     */
-    public final List<Announcement> searchAnnouncement(String textSearchAnnounce) {
-        //TODO NPE + LISIBLE
-        List<Announcement> listAnnounces;
-        listAnnounces = em.createQuery(
-                "SELECT a FROM Announcement a WHERE  TYPE(a) IN(Announcement) AND ( a.title LIKE :textSearchAnnounce OR a.content LIKE :textSearchAnnounce) ", Announcement.class).setParameter("textSearchAnnounce",
-                "%" + textSearchAnnounce + "%").getResultList();
-        return listAnnounces;
+	/**
+	 * Search the Announcement with the param textSearchAnnounce
+	 * 
+	 * @param textSearchAnnounce
+	 * @return a list of Announcement
+	 */
+	public final List<Announcement> searchAnnouncement(String textSearchAnnounce) {
+		if (textSearchAnnounce == null) {
+			throw new IllegalArgumentException();
+		}
+		List<Announcement> listAnnounces;
+		listAnnounces = em
+				.createQuery(
+						"SELECT a FROM Announcement a WHERE  TYPE(a) IN(Announcement) AND "
+								+ "(a.title LIKE :textSearchAnnounce OR a.content LIKE :textSearchAnnounce) ",
+						Announcement.class).setParameter("textSearchAnnounce",
+						"%" + textSearchAnnounce + "%").getResultList();
+		return listAnnounces;
 
-    }
+	}
 }
--- trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/MeetingFacade.java
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/MeetingFacade.java
@@ -15,80 +15,85 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
  */
 public class MeetingFacade {
 
-    private final EntityManager em;
+	private final EntityManager em;
 
-    /**
-     * Constructor
-     * @param em
-     */
-    public MeetingFacade(EntityManager em) {
-        this.em = em;
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param em
+	 */
+	public MeetingFacade(EntityManager em) {
+		this.em = em;
+	}
 
-    /**
-     * Create a New Meeting
-     *
-     * @param member
-     * @param eventName
-     * @param eventDescription
-     * @param endDate
-     * @param isPrivate
-     * @param startDate
-     * @param address
-     * @param city
-     * @return the new meeting
-     */
-    public final Meeting createMeeting(SocialEntity member, String eventName,
-            String eventDescription, Date endDate, Boolean isPrivate,
-            Date startDate, String address, String city) {
+	/**
+	 * Create a New Meeting
+	 * 
+	 * @param member
+	 * @param eventName
+	 * @param eventDescription
+	 * @param endDate
+	 * @param isPrivate
+	 * @param startDate
+	 * @param address
+	 * @param city
+	 * @return the new meeting
+	 */
+	public final Meeting createMeeting(SocialEntity member, String eventName,
+			String eventDescription, Date endDate, Boolean isPrivate,
+			Date startDate, String address, String city) {
+		if (member == null || eventName == null || eventDescription == null
+				|| endDate == null || isPrivate == null || startDate == null
+				|| address == null || city == null) {
+			throw new IllegalArgumentException();
+		}
+		Meeting event = new Meeting(member, eventName, eventDescription,
+				endDate, isPrivate, startDate, new Address(address, city));
 
-        Meeting event = new Meeting(member, eventName, eventDescription,
-                endDate, isPrivate, startDate, new Address(address, city));
+		member.getInteractions().add(event);
+		em.persist(event);
+		return event;
+	}
 
-        member.getInteractions().add(event);
-        em.persist(event);
-        return event;
-    }
+	/**
+	 * 
+	 * @param meetingId
+	 * @return
+	 */
+	public final Meeting getMeeting(int meetingId) {
+		return em.find(Meeting.class, meetingId);
+	}
 
-    /**
-     *
-     * @param meetingId
-     * @return
-     */
-    public final Meeting getMeeting(int meetingId) {
-        return em.find(Meeting.class, meetingId);
-    }
+	/**
+	 * Delete the Meeting with the id of the meeting meetingId
+	 * 
+	 * @param meetingId
+	 */
+	public final void deleteMeeting(int meetingId) {
+		Meeting meet = getMeeting(meetingId);
+		if (meet != null) {
+			em.remove(meet);
+			em.flush();
+		}
+	}
 
-    /**
-     * Delete the Meeting with the id of the meeting meetingId
-     *
-     * @param meetingId
-     */
-    public final void deleteMeeting(int meetingId) {
-        Meeting meet = getMeeting(meetingId);
-        if (meet != null) {
-            em.remove(meet);
-            em.flush();
-        }
-    }
-
-    /**
-     * Search the meeting with the param searchStr
-     *
-     * @param searchStr
-     * @return
-     */
-    public final List<Meeting> searchMeeting(String searchStr) {
-        //TODO NPE + remove useless variables
-        String searchString = searchStr;
-
-        List<Meeting> results;
-        final TypedQuery<Meeting> query;
-        query = em.createQuery("SELECT e FROM Meeting e "
-                + "WHERE e.title LIKE :searchString "
-                + "OR e.content LIKE :searchString ", Meeting.class);
-        query.setParameter("searchString", "%" + searchString + "%");
-        results = query.getResultList();
-        return results;
-    }
+	/**
+	 * Search the meeting with the param searchStr
+	 * 
+	 * @param searchStr
+	 * @return
+	 */
+	public final List<Meeting> searchMeeting(String searchStr) {
+		if (searchStr == null) {
+			throw new IllegalArgumentException();
+		}
+		List<Meeting> results;
+		final TypedQuery<Meeting> query;
+		query = em.createQuery("SELECT e FROM Meeting e "
+				+ "WHERE e.title LIKE :searchString "
+				+ "OR e.content LIKE :searchString ", Meeting.class);
+		query.setParameter("searchString", "%" + searchStr + "%");
+		results = query.getResultList();
+		return results;
+	}
 }
