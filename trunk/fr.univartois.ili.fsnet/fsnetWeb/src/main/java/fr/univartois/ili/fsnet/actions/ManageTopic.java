b2427d8ae5b8d0bbf7200cea9d0d99f4db34a0f8
mehdi.benabdessalem <mehdi.benabdessalem@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-03 16:03:24 +0000

    Updated action ManageTopic to use TopicFacade
    git-svn-id: http://fsnet.googlecode.com/svn@830 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageTopic.java
+++ trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageTopic.java
@@ -6,8 +6,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +21,9 @@ import fr.univartois.ili.fsnet.entities.Hub;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.entities.Topic;
 import fr.univartois.ili.fsnet.entities.TopicMessage;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.HubFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.TopicFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.TopicMessageFacade;
 
 /**
  * 
@@ -31,22 +32,25 @@ import fr.univartois.ili.fsnet.entities.TopicMessage;
 public class ManageTopic extends MappingDispatchAction implements CrudAction {
 
 	private static EntityManagerFactory factory = Persistence
-			.createEntityManagerFactory("fsnetjpa");
+	.createEntityManagerFactory("fsnetjpa");
 
 	@Override
 	public ActionForward create(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	throws IOException, ServletException {
 		EntityManager em = factory.createEntityManager();
 		DynaActionForm dynaForm = (DynaActionForm) form; 					// NOSONAR
 		String topicSujet = (String) dynaForm.get("topicSubject");				// NOSONAR
 		String messageDescription = (String) dynaForm.get("messageDescription");
 		int hubId = Integer.valueOf(Integer.parseInt(dynaForm
 				.getString("hubId")));
-		Hub hub = em.find(Hub.class, hubId);
+		HubFacade hubFacade = new HubFacade(em);
+		Hub hub = hubFacade.getHub(hubId);
 		SocialEntity socialEntity = UserUtils.getAuthenticatedUser(request, em);
-		Topic topic = new Topic(hub, socialEntity, topicSujet);
-        TopicMessage message = new TopicMessage(messageDescription, socialEntity, topic);
+		TopicFacade topicFacade = new TopicFacade(em);
+		Topic topic = topicFacade.createTopic(hub, socialEntity, topicSujet);
+		TopicMessageFacade topicMessageFacade = new TopicMessageFacade(em);
+		TopicMessage message = topicMessageFacade.createTopicMessage(messageDescription, socialEntity, topic);
 		em.getTransaction().begin();
 		hub.getTopics().add(topic);
 		topic.getMessages().add(message);
@@ -58,29 +62,25 @@ public class ManageTopic extends MappingDispatchAction implements CrudAction {
 	@Override
 	public ActionForward modify(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	throws IOException, ServletException {
 		return null;
 	}
 
 	@Override
 	public ActionForward delete(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	throws IOException, ServletException {
 		EntityManager em = factory.createEntityManager();
 		int hubId = Integer.valueOf(Integer.parseInt(request
 				.getParameter("hubId")));
-		Hub hub = em.find(Hub.class, hubId);
+		HubFacade hubFacade = new HubFacade(em);
+		Hub hub = hubFacade.getHub(hubId);
 		if (request.getParameterMap().containsKey("topicId")) {
 			int topicId = Integer.valueOf(request.getParameter("topicId"));
-			Topic topic = em.find(Topic.class, topicId);
-			em.getTransaction().begin();
-			Query query = em
-					.createQuery("DELETE FROM Topic topic WHERE topic.id = :topicId");
-			query.setParameter("topicId", topicId);
-			query.executeUpdate();
+			TopicFacade topicFacade = new TopicFacade(em);
+			topicFacade.deleteTopic(topicId);
+			Topic topic = topicFacade.getTopic(topicId);
 			hub.getTopics().remove(topic);
-			em.getTransaction().commit();
-
 		}
 		em.close();
 		return mapping.findForward("success");
@@ -89,22 +89,16 @@ public class ManageTopic extends MappingDispatchAction implements CrudAction {
 	@Override
 	public ActionForward search(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	throws IOException, ServletException {
 		EntityManager em = factory.createEntityManager();
 		DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
 		String topicSujet = (String) dynaForm.get("topicSujetSearch");
 		int hubId = Integer.parseInt((String) dynaForm.get("hubId"));
-		Hub hub = em.find(Hub.class, hubId);
-		if (topicSujet != null) {
-			TypedQuery<Topic> query = em
-					.createQuery(
-							"SELECT topic FROM Topic topic WHERE topic.title LIKE :sujetRea AND topic.hub = :hub ",
-							Topic.class);
-			query.setParameter("sujetRea", "%" + topicSujet + "%");
-			query.setParameter("hub", hub);
-			List<Topic> result = query.getResultList();
-			request.setAttribute("resRearchTopics", result);
-		}
+		HubFacade hubFacade = new HubFacade(em);
+		Hub hub = hubFacade.getHub(hubId);
+		TopicFacade topicFacade = new TopicFacade(em);
+		List<Topic> result = topicFacade.searchTopic(topicSujet, hub);
+		request.setAttribute("resRearchTopics", result);
 		em.close();
 		return mapping.findForward("success");
 	}
@@ -112,14 +106,12 @@ public class ManageTopic extends MappingDispatchAction implements CrudAction {
 	@Override
 	public ActionForward display(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	throws IOException, ServletException {
 		EntityManager em = factory.createEntityManager();
 		if (request.getParameterMap().containsKey("topicId")) {
 			int topicId = Integer.valueOf(request.getParameter("topicId"));
-			Query query = em
-					.createQuery("SELECT topic FROM Topic topic WHERE topic.id = :topicId ");
-			query.setParameter("topicId", topicId);
-			Topic result = (Topic) query.getSingleResult();
+			TopicFacade topicFacade = new TopicFacade(em);
+			Topic result = topicFacade.getTopic(topicId);
 			request.setAttribute("topic", result);
 		}
 		em.close();
