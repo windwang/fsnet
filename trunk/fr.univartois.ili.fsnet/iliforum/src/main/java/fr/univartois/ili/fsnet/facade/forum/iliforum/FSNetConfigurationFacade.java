423d2339066180fc903d58e08d7ae08f5e132b39
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-03 22:06:16 +0000

    Changed  entity name from Configuration to Property Updated references in code
    git-svn-id: http://fsnet.googlecode.com/svn@842 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/FSNetConfigurationFacade.java
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/FSNetConfigurationFacade.java
@@ -2,7 +2,7 @@ package fr.univartois.ili.fsnet.facade.forum.iliforum;
 
 import javax.persistence.EntityManager;
 
-import fr.univartois.ili.fsnet.entities.Configuration;
+import fr.univartois.ili.fsnet.entities.Property;
 
 /**
  * 
@@ -19,16 +19,16 @@ public class FSNetConfigurationFacade {
 	}
 
 	public int getSMTPPort() {
-		return Integer.parseInt(em.find(Configuration.class,
-				Configuration.FsnetProperty.SMTP_PORT).getValue());
+		return Integer.parseInt(em.find(Property.class,
+				Property.FsnetProperty.SMTP_PORT).getValue());
 	}
 
 	public void setSMTPPort(int smtpPort) {
-		Configuration smtpPortConf = em.find(Configuration.class,
-				Configuration.FsnetProperty.SMTP_PORT);
+		Property smtpPortConf = em.find(Property.class,
+				Property.FsnetProperty.SMTP_PORT);
 		if (smtpPortConf == null) {
-			smtpPortConf = new Configuration();
-			smtpPortConf.setKey(Configuration.FsnetProperty.SMTP_PORT);
+			smtpPortConf = new Property();
+			smtpPortConf.setKey(Property.FsnetProperty.SMTP_PORT);
 			smtpPortConf.setValue(String.valueOf(smtpPort));
 			em.persist(smtpPortConf);
 		} else {
@@ -39,16 +39,16 @@ public class FSNetConfigurationFacade {
 	}
 
 	public int getSMTPHost() {
-		return Integer.parseInt(em.find(Configuration.class,
-				Configuration.FsnetProperty.SMTP_HOST).getValue());
+		return Integer.parseInt(em.find(Property.class,
+				Property.FsnetProperty.SMTP_HOST).getValue());
 	}
 
 	public void setSMTPHost(int smtpHost) {
-		Configuration smtpHostConf = em.find(Configuration.class,
-				Configuration.FsnetProperty.SMTP_HOST);
+		Property smtpHostConf = em.find(Property.class,
+				Property.FsnetProperty.SMTP_HOST);
 		if(smtpHostConf == null) {
-			smtpHostConf = new Configuration();
-			smtpHostConf.setKey(Configuration.FsnetProperty.SMTP_HOST);
+			smtpHostConf = new Property();
+			smtpHostConf.setKey(Property.FsnetProperty.SMTP_HOST);
 			smtpHostConf.setValue(String.valueOf(smtpHost));
 			em.persist(smtpHostConf);
 		} else {
