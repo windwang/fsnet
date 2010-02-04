f77b08a81e4550235db05396662f9594b4001bcf
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-04 00:50:07 +0000

    FSNETweb : few changes in tiny mce, you will be pleased, trust me ;)
    git-svn-id: http://fsnet.googlecode.com/svn@851 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/webapp/js/mceTextArea.js
+++ trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/webapp/js/mceTextArea.js
@@ -1,6 +1,13 @@
 tinyMCE.init({
 	// General options
 	mode : "textareas",
-	theme : "simple",
-        content_css : "css/fsnet-custom.css"
+	theme : "advanced",
+        content_css : "css/fsnet-custom.css",
+        plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager",
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect,forecolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left"
+
 });
