acad4430ab0bc08a4a410027d9b9b832177dde1b
mehdi.benabdessalem <mehdi.benabdessalem@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-04 14:05:18 +0000

    Update ManageContacts to use ContactsFacade and correction in Contacts
    git-svn-id: http://fsnet.googlecode.com/svn@855 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageContacts.java
+++ trunk/fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageContacts.java
@@ -13,12 +13,13 @@ import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.DynaActionForm;
 import org.apache.struts.actions.MappingDispatchAction;
 
 import fr.univartois.ili.fsnet.actions.utils.UserUtils;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.ContactFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.SocialEntityFacade;
 
 /**
  * 
@@ -26,187 +27,166 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
  */
 public class ManageContacts extends MappingDispatchAction implements CrudAction {
 
-    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
+	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
 
 
-    /**
-     * Submit a request contact to another social entity
-     *
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return
-     * @throws IOException
-     * @throws ServletException
-     */
-    public ActionForward askContact(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-        EntityManager em = factory.createEntityManager();
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        final String idString = (String) dynaForm.get("entitySelected");
-        int entitySelected = Integer.parseInt(idString);
+	/**
+	 * Submit a request contact to another social entity
+	 *
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public ActionForward askContact(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		final String idString = (String) dynaForm.get("entitySelected");
+		int entitySelected = Integer.parseInt(idString);
+		
+		ActionErrors errors = new ActionErrors();
+		// TODO changer les listes en set sur les entites sociales pour eviter
+		// les doublons
+		
+		SocialEntityFacade socialEntityFacade = new SocialEntityFacade(em); 
+		SocialEntity entity = socialEntityFacade.getSocialEntity(entitySelected);
+		ContactFacade contactFacade = new ContactFacade(em);
+		contactFacade.askContact(user, entity);
 
-        ActionErrors errors = new ActionErrors();
-        if (entitySelected != user.getId()) {
-            em.getTransaction().begin();
+		em.getTransaction().commit();
+		em.close();
+		return mapping.findForward("success");
+	}
 
-            // TODO changer les listes en set sur les entites sociales pour eviter
-            // les doublons
-            SocialEntity entity = em.find(SocialEntity.class, entitySelected);
+	/**
+	 * To accept a contact request
+	 *
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public ActionForward accept(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		final String idString = (String) dynaForm.get("entityAccepted");
+		int id = Integer.parseInt(idString);
+		SocialEntityFacade socialEntityFacade = new SocialEntityFacade(em); 
+		SocialEntity entityAccepted = socialEntityFacade.getSocialEntity(id);
+		ContactFacade contactFacade = new ContactFacade(em);
+		contactFacade.acceptContact(user, entityAccepted);
+		em.getTransaction().commit();
+		em.close();
+		return mapping.findForward("success");
+	}
 
-            if (notInRelation(user, entity)) {
-                user.getRequested().add(entity);
-                entity.getAsked().add(user);
-                em.getTransaction().commit();
-            } else {
-                errors.add("entitySelected", new ActionMessage("contacts.error.alreadyAsked"));
-            }
-        } else {
-            errors.add("entitySelected", new ActionMessage("contacts.error.askself"));
-        }
-        em.close();
-        return mapping.findForward("success");
-    }
+	/**
+	 * To refuse a contact request
+	 *
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public ActionForward refuse(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
 
-    /**
-     * To accept a contact request
-     *
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return
-     * @throws IOException
-     * @throws ServletException
-     */
-    public ActionForward accept(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-        EntityManager em = factory.createEntityManager();
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        final String idString = (String) dynaForm.get("entityAccepted");
-        int id = Integer.parseInt(idString);
-        SocialEntity entityAccepted = em.find(SocialEntity.class, id);
-        //TODO check that in a method when --facading--
-        if (user.getAsked().contains(entityAccepted)
-                && entityAccepted.getRequested().contains(user)
-                && !user.getContacts().contains(entityAccepted)
-                && !entityAccepted.getContacts().contains(user)) {
-            em.getTransaction().begin();
-            user.getContacts().add(entityAccepted);
-            entityAccepted.getContacts().add(user);
-            user.getAsked().remove(entityAccepted);
-            entityAccepted.getRequested().remove(user);
-            em.getTransaction().commit();
-        } else {		//NOSONAR
-            //TODO 1. create a non consistant database error message SEVER !
-        }
-        em.close();
-        return mapping.findForward("success");
-    }
+		DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		final String idString = (String) dynaForm.get("entityRefused");
+		int id = Integer.parseInt(idString);
+		SocialEntityFacade socialEntityFacade = new SocialEntityFacade(em); 
+		SocialEntity entityRefused = socialEntityFacade.getSocialEntity(id);
+		ContactFacade contactFacade = new ContactFacade(em);
+		contactFacade.refuseContact(user, entityRefused);
+		em.getTransaction().commit();
+		em.close();
+		return mapping.findForward("success");
+	}
 
-    /**
-     * To refuse a contact request
-     *
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return
-     * @throws IOException
-     * @throws ServletException
-     */
-    public ActionForward refuse(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+	@Override
+	public ActionForward create(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-        DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-        EntityManager em = factory.createEntityManager();
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        final String idString = (String) dynaForm.get("entityRefused");
-        int id = Integer.parseInt(idString);
-        SocialEntity entityRefused = em.find(SocialEntity.class, id);
-        if (user.getAsked().contains(entityRefused)
-                && entityRefused.getRequested().contains(user)
-                && !user.getContacts().contains(entityRefused)
-                && !entityRefused.getContacts().contains(user)) {
-        em.getTransaction().begin();
-        user.getAsked().remove(entityRefused);
-        entityRefused.getRequested().remove(user);
-        user.getRefused().add(entityRefused);
-        em.merge(user);
-        em.merge(entityRefused);
-        em.getTransaction().commit();
-        }
-        em.close();
-        return mapping.findForward("success");
-    }
+	@Override
+	public ActionForward modify(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public ActionForward create(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public ActionForward delete(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		final String idString = (String) dynaForm.get("entityDeleted");
+		int id = Integer.parseInt(idString);
+		SocialEntityFacade socialEntityFacade = new SocialEntityFacade(em); 
+		SocialEntity removedEntity = socialEntityFacade.getSocialEntity(id);
+		ContactFacade contactFacade = new ContactFacade(em);
+		contactFacade.removeContact(user, removedEntity);        
+		em.getTransaction().commit();
+		em.close();
+		return mapping.findForward("success");
+	}
 
-    @Override
-    public ActionForward modify(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public ActionForward search(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public ActionForward delete(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-        EntityManager em = factory.createEntityManager();
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        final String idString = (String) dynaForm.get("entityDeleted");
-        int id = Integer.parseInt(idString);
-        SocialEntity entityDeleted = em.find(SocialEntity.class, id);
-        em.getTransaction().begin();
-        user.getContacts().remove(entityDeleted);
-        entityDeleted.getContacts().remove(user);
-        em.getTransaction().commit();
-        em.close();
-        return mapping.findForward("success");
-    }
+	@Override
+	public ActionForward display(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
 
-    @Override
-    public ActionForward search(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+		EntityManager em = factory.createEntityManager();
+		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+	
+		user.getContacts();
+		user.getAsked();
+		user.getRequested();
+		request.setAttribute("theUser", user);
+		em.close();
+		return mapping.findForward("success");
+	}
 
-    @Override
-    public ActionForward display(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-
-        EntityManager em = factory.createEntityManager();
-        SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
-        user.getContacts();
-        user.getAsked();
-        user.getRequested();
-        request.setAttribute("theUser", user);
-        em.close();
-        return mapping.findForward("success");
-    }
-
-    // TODO move in facade
-    private boolean notInRelation(SocialEntity user, SocialEntity entity) {
-        return !(user.getAsked().contains(entity)
-                || user.getRequested().contains(entity)
-                || user.getContacts().contains(entity)
-                || entity.getAsked().contains(user)
-                || entity.getRequested().contains(user)
-                || entity.getContacts().contains(user));
-    }
+	// TODO move in facade
+	private boolean notInRelation(SocialEntity user, SocialEntity entity) {
+		return !(user.getAsked().contains(entity)
+				|| user.getRequested().contains(entity)
+				|| user.getContacts().contains(entity)
+				|| entity.getAsked().contains(user)
+				|| entity.getRequested().contains(user)
+				|| entity.getContacts().contains(user));
+	}
 }
