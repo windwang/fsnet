0c17f787bfca5e9f0b2393f694ab9d75d33defd1
orel.legrand <orel.legrand@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-05 15:29:35 +0000

    you can add interests when you create a topic
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@884 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageTopic.java
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageTopic.java
@@ -19,11 +19,15 @@ import org.apache.struts.actions.MappingDispatchAction;
 
 import fr.univartois.ili.fsnet.actions.utils.UserUtils;
 import fr.univartois.ili.fsnet.entities.Hub;
+import fr.univartois.ili.fsnet.entities.Interest;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.entities.Topic;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.HubFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.InteractionFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.InterestFacade;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.TopicFacade;
 import fr.univartois.ili.fsnet.facade.forum.iliforum.TopicMessageFacade;
+import java.util.ArrayList;
 
 /**
  * 
@@ -31,92 +35,100 @@ import fr.univartois.ili.fsnet.facade.forum.iliforum.TopicMessageFacade;
  */
 public class ManageTopic extends MappingDispatchAction implements CrudAction {
 
-	private static EntityManagerFactory factory = Persistence
-	.createEntityManagerFactory("fsnetjpa");
+    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("fsnetjpa");
 
-	@Override
-	public ActionForward create(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException {
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		DynaActionForm dynaForm = (DynaActionForm) form; 					// NOSONAR
-		String topicSujet = (String) dynaForm.get("topicSubject");				// NOSONAR
-		String messageDescription = (String) dynaForm.get("messageDescription");
-		int hubId = Integer.valueOf(Integer.parseInt(dynaForm
-				.getString("hubId")));
-		HubFacade hubFacade = new HubFacade(em);
-		Hub hub = hubFacade.getHub(hubId);
-		SocialEntity socialEntity = UserUtils.getAuthenticatedUser(request, em);
-		TopicFacade topicFacade = new TopicFacade(em);
-		Topic topic = topicFacade.createTopic(hub, socialEntity, topicSujet);
-		TopicMessageFacade topicMessageFacade = new TopicMessageFacade(em);
-		topicMessageFacade.createTopicMessage(messageDescription, socialEntity, topic);
-		em.getTransaction().commit();
-		em.close();
-		return mapping.findForward("success");
-	}
+    @Override
+    public ActionForward create(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        DynaActionForm dynaForm = (DynaActionForm) form; 					// NOSONAR
+        String topicSujet = (String) dynaForm.get("topicSubject");				// NOSONAR
+        String messageDescription = (String) dynaForm.get("messageDescription");
+        int hubId = Integer.valueOf(Integer.parseInt(dynaForm.getString("hubId")));
+        HubFacade hubFacade = new HubFacade(em);
+        Hub hub = hubFacade.getHub(hubId);
+        SocialEntity socialEntity = UserUtils.getAuthenticatedUser(request, em);
+        TopicFacade topicFacade = new TopicFacade(em);
+        Topic topic = topicFacade.createTopic(hub, socialEntity, topicSujet);
 
-	@Override
-	public ActionForward modify(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException {
-		return null;
-	}
+        String InterestsIds[] = (String[]) dynaForm.get("selectedInterests");
+        InterestFacade fac = new InterestFacade(em);
+        List<Interest> interests = new ArrayList<Interest>();
+        int currentId;
+        for (currentId = 0; currentId < InterestsIds.length; currentId++) {
+            interests.add(fac.getInterest(Integer.valueOf(InterestsIds[currentId])));
+        }
+        InteractionFacade ifacade = new InteractionFacade(em);
+        ifacade.addInterests(topic, interests);
 
-	@Override
-	public ActionForward delete(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException {
-		//TODO hubId not necessary
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		int hubId = Integer.valueOf(Integer.parseInt(request
-				.getParameter("hubId")));
-		HubFacade hubFacade = new HubFacade(em);
-		Hub hub = hubFacade.getHub(hubId);
-		if (request.getParameterMap().containsKey("topicId")) {
-			int topicId = Integer.valueOf(request.getParameter("topicId"));
-			TopicFacade topicFacade = new TopicFacade(em);
-			Topic topic = topicFacade.getTopic(topicId);
-			hub.getTopics().remove(topic);
-			topicFacade.deleteTopic(topicId);
-		}
-		em.getTransaction().commit();
-		em.close();
-		return mapping.findForward("success");
-	}
+        TopicMessageFacade topicMessageFacade = new TopicMessageFacade(em);
+        topicMessageFacade.createTopicMessage(messageDescription, socialEntity, topic);
+        em.getTransaction().commit();
+        em.close();
+        return mapping.findForward("success");
+    }
 
-	@Override
-	public ActionForward search(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException {
-		EntityManager em = factory.createEntityManager();
-		DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
-		String topicSujet = (String) dynaForm.get("topicSujetSearch");
-		int hubId = Integer.parseInt((String) dynaForm.get("hubId"));
-		HubFacade hubFacade = new HubFacade(em);
-		Hub hub = hubFacade.getHub(hubId);
-		TopicFacade topicFacade = new TopicFacade(em);
-		List<Topic> result = topicFacade.searchTopic(topicSujet, hub);
-		request.setAttribute("resRearchTopics", result);
-		em.close();
-		return mapping.findForward("success");
-	}
+    @Override
+    public ActionForward modify(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        return null;
+    }
 
-	@Override
-	public ActionForward display(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException {
-		EntityManager em = factory.createEntityManager();
-		if (request.getParameterMap().containsKey("topicId")) {
-			int topicId = Integer.valueOf(request.getParameter("topicId"));
-			TopicFacade topicFacade = new TopicFacade(em);
-			Topic result = topicFacade.getTopic(topicId);
-			Logger.getAnonymousLogger().info("#############  topic messages = "+ result.getMessages().size());
-			request.setAttribute("topic", result);
-		}
-		em.close();
-		return mapping.findForward("success");
-	}
+    @Override
+    public ActionForward delete(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        //TODO hubId not necessary
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        int hubId = Integer.valueOf(Integer.parseInt(request.getParameter("hubId")));
+        HubFacade hubFacade = new HubFacade(em);
+        Hub hub = hubFacade.getHub(hubId);
+        if (request.getParameterMap().containsKey("topicId")) {
+            int topicId = Integer.valueOf(request.getParameter("topicId"));
+            TopicFacade topicFacade = new TopicFacade(em);
+            Topic topic = topicFacade.getTopic(topicId);
+            hub.getTopics().remove(topic);
+            topicFacade.deleteTopic(topicId);
+        }
+        em.getTransaction().commit();
+        em.close();
+        return mapping.findForward("success");
+    }
+
+    @Override
+    public ActionForward search(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        EntityManager em = factory.createEntityManager();
+        DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
+        String topicSujet = (String) dynaForm.get("topicSujetSearch");
+        int hubId = Integer.parseInt((String) dynaForm.get("hubId"));
+        HubFacade hubFacade = new HubFacade(em);
+        Hub hub = hubFacade.getHub(hubId);
+        TopicFacade topicFacade = new TopicFacade(em);
+        List<Topic> result = topicFacade.searchTopic(topicSujet, hub);
+        request.setAttribute("resRearchTopics", result);
+        em.close();
+        return mapping.findForward("success");
+    }
+
+    @Override
+    public ActionForward display(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        EntityManager em = factory.createEntityManager();
+        if (request.getParameterMap().containsKey("topicId")) {
+            int topicId = Integer.valueOf(request.getParameter("topicId"));
+            TopicFacade topicFacade = new TopicFacade(em);
+            Topic result = topicFacade.getTopic(topicId);
+            Logger.getAnonymousLogger().info("#############  topic messages = " + result.getMessages().size());
+            request.setAttribute("topic", result);
+        }
+        em.close();
+        return mapping.findForward("success");
+    }
 }
--- fr.univartois.ili.fsnet/fsnetWeb/src/main/webapp/content/topics/CreateTopic.jsp
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/webapp/content/topics/CreateTopic.jsp
@@ -9,6 +9,7 @@
 <%@ taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
 <%@ taglib uri="http://struts.apache.org/tags-logic" prefix="logic"%>
 <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <script type="text/javascript" src="js/tiny_mce/tiny_mce.js"></script>
 <script type="text/javascript" src="js/mceTextArea.js"></script>
@@ -36,6 +37,7 @@
                     </div>
                 </logic:messagesPresent>
             </td>
+            <jsp:include page="/InterestCheckBoxes.do"/>
         </tr>
         <tr>
             <td>
