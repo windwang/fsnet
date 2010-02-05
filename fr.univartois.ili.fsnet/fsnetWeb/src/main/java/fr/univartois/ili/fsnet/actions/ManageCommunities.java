75c5cfe6fb896b9919aa8559bb0b854a34a841d6
aud4rue <aud4rue@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-05 10:58:13 +0000

    Communities : create, search and delete done
    with cerelia12
    
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@869 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageCommunities.java
@@ -0,0 +1,113 @@
+package fr.univartois.ili.fsnet.actions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+import org.apache.struts.actions.MappingDispatchAction;
+
+import fr.univartois.ili.fsnet.actions.utils.UserUtils;
+import fr.univartois.ili.fsnet.entities.Community;
+import fr.univartois.ili.fsnet.entities.Hub;
+import fr.univartois.ili.fsnet.entities.SocialEntity;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.CommunityFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.HubFacade;
+
+/**
+ * Execute CRUD Actions (and more) for the entity community
+ * 
+ * @author Audrey Ruellan et Cerelia Besnainou
+ */
+public class ManageCommunities extends MappingDispatchAction implements CrudAction {
+
+	private static EntityManagerFactory factory = Persistence
+			.createEntityManagerFactory("fsnetjpa");
+	private static final Logger logger = Logger.getAnonymousLogger();
+
+	@Override
+	public ActionForward create(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
+		String name = (String) dynaForm.get("name");
+		
+		logger.info("#### New Community : " + name);
+		EntityManager em = factory.createEntityManager();
+		CommunityFacade communityFacade = new CommunityFacade(em);
+		SocialEntity creator = UserUtils.getAuthenticatedUser(request, em);
+		em.getTransaction().begin();
+		communityFacade.createCommunity(creator, name);
+		em.getTransaction().commit();
+		em.close();
+		
+		return mapping.findForward("success");
+	}
+	
+	@Override
+	public ActionForward delete(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String communityId = request.getParameter("communityId");
+
+		logger.info("delete community: " + communityId);
+		
+		EntityManager em = factory.createEntityManager();
+		CommunityFacade communityFacade = new CommunityFacade(em);
+		em.getTransaction().begin();
+		communityFacade.deleteCommunity(Integer.parseInt(communityId));
+		em.getTransaction().commit();
+		em.close();
+		
+		return mapping.findForward("success");
+	
+	}
+
+	@Override
+	public ActionForward display(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet");
+	}
+
+	@Override
+	public ActionForward modify(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not supported yet");
+	}
+
+	@Override
+	public ActionForward search(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		EntityManager em = factory.createEntityManager();
+		List<Community> result = null;
+		String searchText = "";
+		CommunityFacade communityFacade = new CommunityFacade(em);
+		if (form != null) {
+			DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
+			searchText = (String) dynaForm.get("searchText");
+			
+		}
+		em.getTransaction().begin();
+		result = communityFacade.searchCommunity(searchText);
+		em.getTransaction().commit();
+		em.close();
+		
+
+		request.setAttribute("communitiesResult", result);
+		return mapping.findForward("success");
+	}
+}
