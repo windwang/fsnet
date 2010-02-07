73de6d3caa5cd2143f367424d588ef68d90a28a1
mehdi.benabdessalem <mehdi.benabdessalem@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-07 10:16:07 +0000

    Update ManageMembers to use Facade
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@905 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageMembers.java
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageMembers.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +18,9 @@ import org.apache.struts.actions.MappingDispatchAction;
 
 import fr.univartois.ili.fsnet.actions.utils.UserUtils;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.SocialEntityFacade;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.SocialEntityFacade.SearchResult;
+import java.util.HashMap;
 
 public class ManageMembers extends MappingDispatchAction {
 
@@ -34,76 +36,35 @@ public class ManageMembers extends MappingDispatchAction {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
+    @SuppressWarnings("element-type-mismatch")
 	public ActionForward search(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 	throws IOException, ServletException {
 		EntityManager em = factory.createEntityManager();
-		TypedQuery<SocialEntity> query = null;
-		TypedQuery<SocialEntity> queryContacts = null;
-		TypedQuery<SocialEntity> queryRequested = null;
-		TypedQuery<SocialEntity> queryAsked = null;
+                em.getTransaction().begin();
 		List<SocialEntity> resultOthers = null;
 		List<SocialEntity> resultContacts = null;
 		List<SocialEntity> resultRequested = null;
 		List<SocialEntity> resultAsked = null;
+
 		SocialEntity member = UserUtils.getAuthenticatedUser(request, em);
-
-		member = em.find(SocialEntity.class, member.getId());
-
-		if (form != null) {
-			DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
-			String searchText = (String) dynaForm.get("searchText");
-
-			query = em.createQuery(
-					"SELECT es FROM SocialEntity es WHERE (es.name LIKE :searchText"
-					+ " OR es.firstName LIKE :searchText OR es.email LIKE :searchText) AND es.id <> :id", SocialEntity.class);
-			query.setParameter("searchText", "%" + searchText + "%");
-			query.setParameter("id", member.getId());
-
-			queryContacts = em.createQuery(
-					"SELECT e FROM SocialEntity e JOIN e.contacts c WHERE c.id = :id AND (e.name LIKE :searchText"
-					+ " OR e.firstName LIKE :searchText OR e.email LIKE :searchText)", SocialEntity.class);
-			queryContacts.setParameter("searchText", "%" + searchText + "%");
-			queryContacts.setParameter("id", member.getId());
-
-			queryRequested = em.createQuery(
-					"SELECT e FROM SocialEntity e JOIN e.asked r WHERE r.id = :id AND (e.name LIKE :searchText"
-					+ " OR e.firstName LIKE :searchText OR e.email LIKE :searchText)", SocialEntity.class);
-			queryRequested.setParameter("searchText", "%" + searchText + "%");
-			queryRequested.setParameter("id", member.getId());
-
-			queryAsked = em.createQuery(
-					"SELECT e FROM SocialEntity e JOIN e.requested r WHERE r.id = :id AND (e.name LIKE :searchText"
-					+ " OR e.firstName LIKE :searchText OR e.email LIKE :searchText)", SocialEntity.class);
-			queryAsked.setParameter("searchText", "%" + searchText + "%");
-			queryAsked.setParameter("id", member.getId());
-
-			resultContacts = queryContacts.getResultList();
-			resultRequested = queryRequested.getResultList();
-			resultAsked = queryAsked.getResultList();
-
-		} else {
-			query = em.createQuery("SELECT es FROM SocialEntity es WHERE es.id <> :id", SocialEntity.class);
-			query.setParameter("id", member.getId());
-
-			resultContacts = member.getContacts();
-			resultRequested = member.getRequested();
-			resultAsked = member.getAsked();
-		}
-
-		resultOthers = query.getResultList();
-
-		resultOthers.removeAll(resultContacts);
-		resultOthers.removeAll(resultAsked);
-		resultOthers.removeAll(resultRequested);
-
+	
+                DynaActionForm dynaForm = (DynaActionForm) form;//NOSONAR
+		String searchText = (String) dynaForm.get("searchText");
+           
+                SocialEntityFacade sef = new SocialEntityFacade(em);
+                HashMap<SearchResult, List<SocialEntity>> results = sef.searchSocialEntity(searchText, member);	
+                resultContacts = results.get(SearchResult.Contacts);
+		resultRequested = results.get(SearchResult.Requested);
+		resultAsked = results.get(SearchResult.Asked);
+		resultOthers = results.get(SearchResult.Others);
+                em.getTransaction().commit();
 		em.close();
 
 		request.setAttribute("membersResult", resultOthers);
 		request.setAttribute("membersContactsResult", resultContacts);
 		request.setAttribute("membersRequestedResult", resultRequested);
 		request.setAttribute("membersAskedResult", resultAsked);
-
 
 		return mapping.findForward("success");
 	}
