5eda3e07abf556ae376af81ed6557fb5cff2876b
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-08 20:08:44 +0000

    Added documentation in the Paginator class
    git-svn-id: http://fsnet.googlecode.com/svn@925 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/commons/src/main/java/fr/univartois/ili/fsnet/commons/pagination/Paginator.java
+++ trunk/fr.univartois.ili.fsnet/commons/src/main/java/fr/univartois/ili/fsnet/commons/pagination/Paginator.java
@@ -7,26 +7,68 @@ import javax.servlet.http.HttpServletRequest;
 
 public class Paginator<T> {
 
+	/**
+	 * The name of the parameter in the request to give the requested page id
+	 */
 	public static final String PAGE_ID = "pageId";
 
+	/**
+	 * The name of the parameter in the request to set the number of results
+	 */
 	public static final String NUM_RESULT_PER_PAGES = "numResults";
 
+	/**
+	 * The default number of results if The user or developer has not set this
+	 * parameter
+	 */
 	public static final int DEFAULT_NUM_RESULT_PER_PAGE = 15;
 
+	/**
+	 * The requested page id
+	 */
 	private int requestedPage = 0;
 
+	/**
+	 * The number of results per page (set to NUM_RESULT_PER_PAGES by default)
+	 */
 	private int numResultsPerPage;
 
+	/**
+	 * The number of pages in this query
+	 */
 	private int numPages;
 
+	/**
+	 * true if this query admit a next page
+	 */
 	private boolean hasNextPage;
 
+	/**
+	 * true if this query admit a previous page
+	 */
 	private boolean hasPreviousPage;
 
+	/**
+	 * All results from query
+	 */
 	private List<T> allResults;
 
+	/**
+	 * The splited results
+	 */
 	private List<T> results;
 
+	/**
+	 * This constructor allow developper to set the number of results, AND this
+	 * parameter is higher priority to HTTP parameter
+	 * 
+	 * @param results
+	 *            All results from a query from example
+	 * @param requestThe
+	 *            HTTPRequest that contains the proper parameters
+	 * @param numResultsPerPage
+	 *            the number of results per page
+	 */
 	public Paginator(List<T> results, HttpServletRequest request,
 			int numResultsPerPage) {
 		this.allResults = results;
@@ -35,6 +77,14 @@ public class Paginator<T> {
 		init();
 	}
 
+	/**
+	 * Default constructor that allow user to set the number of result by
+	 * including the proper parameter in his request
+	 * 
+	 * @param results
+	 * @param request
+	 * @throws IllegalArgumentException
+	 */
 	public Paginator(List<T> results, HttpServletRequest request)
 			throws IllegalArgumentException {
 		this.allResults = results;
@@ -42,6 +92,9 @@ public class Paginator<T> {
 		init();
 	}
 
+	/**
+	 * Init the properties about this paginator
+	 */
 	private void init() {
 		numPages = (int) Math.ceil(allResults.size()
 				/ ((double) numResultsPerPage));
@@ -59,6 +112,11 @@ public class Paginator<T> {
 		}
 	}
 
+	/*
+	 * Parse the parameters from the request
+	 * @param request
+	 * @throws IllegalArgumentException
+	 */
 	private void parseRequest(HttpServletRequest request)
 			throws IllegalArgumentException {
 		if (request.getParameterMap().containsKey(PAGE_ID)) {
@@ -91,6 +149,10 @@ public class Paginator<T> {
 		return result;
 	}
 
+	/**
+	 * Return the splited result list
+	 * @return
+	 */
 	public List<T> getResultList() {
 		return results;
 	}
