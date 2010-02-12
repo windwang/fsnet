489959ad24e2176a9ce777dd362f3192b8dad1d3
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-12 12:41:48 +0000

    Enhanced MockDB
    git-svn-id: http://fsnet.googlecode.com/svn@975 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/mockDB/src/main/java/fr/univartois/ili/fsnet/fakeDB/PopulateDB.java
+++ trunk/fr.univartois.ili.fsnet/mockDB/src/main/java/fr/univartois/ili/fsnet/fakeDB/PopulateDB.java
@@ -1,8 +1,6 @@
 package fr.univartois.ili.fsnet.fakeDB;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,38 +10,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.univartois.ili.fsnet.fakeDB.mocks.MathieuBonifaceMock;
-
 public class PopulateDB extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final Logger logger = Logger.getAnonymousLogger();
-
 	private static final EntityManagerFactory factory = Persistence
 			.createEntityManagerFactory("fsnetjpa");
-
-	private void loadMock(Class<? extends Mock> clazz) {
-		EntityManager em = factory.createEntityManager();
-		try {
-			Mock mock;
-			mock = clazz.newInstance();
-			mock.populate(em);
-		} catch (SecurityException e) {
-			logger.log(Level.SEVERE, "", e);
-		} catch (InstantiationException e) {
-			logger.log(Level.SEVERE, "", e);
-		} catch (IllegalAccessException e) {
-			logger.log(Level.SEVERE, "", e);
-		} finally {
-			em.close();
-		}
-	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		loadMock(MathieuBonifaceMock.class);
+		EntityManager em = factory.createEntityManager();
+		Instancier instancier = new Instancier(em);
+		instancier.start();
 		resp.sendRedirect("index.jsp");
 	}
 
