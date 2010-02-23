c0ceaf18ba9325fb992c65459f84274f3c039748
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-23 18:39:31 +0000

    Added structure to manage unauthorized operations exceptions
    git-svn-id: http://fsnet.googlecode.com/svn@1143 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ trunk/fr.univartois.ili.fsnet/iliforum/src/main/java/fr/univartois/ili/fsnet/facade/forum/iliforum/security/UnauthorizedOperationException.java
@@ -0,0 +1,14 @@
+package fr.univartois.ili.fsnet.facade.forum.iliforum.security;
+
+public class UnauthorizedOperationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * The unique constructor of this class.
+	 * @param key the resource bundle key that represents the message to show to an user
+	 */
+	public UnauthorizedOperationException(String key) {
+	}
+
+}
