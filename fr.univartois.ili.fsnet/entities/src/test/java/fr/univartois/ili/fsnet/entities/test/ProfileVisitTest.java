3b40f9cc43ece09faaa625e5e0e91e48b1098b88
supergeoffrey <supergeoffrey@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-24 19:18:43 +0000

    add persistance test in visite profile test
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@1158 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/entities/src/test/java/fr/univartois/ili/fsnet/entities/test/ProfileVisitTest.java
+++ fr.univartois.ili.fsnet/entities/src/test/java/fr/univartois/ili/fsnet/entities/test/ProfileVisitTest.java
@@ -3,14 +3,22 @@ package fr.univartois.ili.fsnet.entities.test;
 import static org.junit.Assert.*;
 
 import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import fr.univartois.ili.fsnet.entities.ProfileVisite;
+import fr.univartois.ili.fsnet.entities.ProfileVisitePK;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 
 public class ProfileVisitTest {
-
+	
+	private EntityManager em;
 	
 	private SocialEntity bibi;
 	
@@ -21,17 +29,24 @@ public class ProfileVisitTest {
 	private ProfileVisite pv;
 	
 	@Before 
-	public void createSocialEntity(){
+	public void setUp(){
+        EntityManagerFactory fact = Persistence.createEntityManagerFactory("TestPU");
+        em = fact.createEntityManager();
 		bibi = new SocialEntity("bibi","bibi","bibi@bibi.fr");
 		bobo = new SocialEntity("bobo","bobo","bobo@bobo.fr");
 		toto = new SocialEntity("toto","toto","toto@bobo.fr");
-		pv = new ProfileVisite(bobo,bibi);
+		pv = new ProfileVisite(bobo, bibi);
 		try {
 			Thread.sleep(2);
 		} catch (InterruptedException e) {
 			fail();
 		}
 		
+	}
+	
+	@After
+	public void tearDown(){
+		em.close();
 	}
 	
 	@Test
@@ -77,6 +92,23 @@ public class ProfileVisitTest {
 			fail();
 		}
 		assertTrue(d.before(pv.getLastVisite()));
+	}
+	
+	@Test 
+	public void persistanceTest(){
+		em.getTransaction().begin();
+        em.persist(bibi);
+        em.persist(bobo);
+        em.persist(toto);
+		em.getTransaction().commit();
+		em.getTransaction().begin();
+		em.persist(pv);
+		em.getTransaction().commit();
+		em.getTransaction().begin();
+		ProfileVisite pv2 = em.find(ProfileVisite.class
+				, new ProfileVisitePK(bibi.getId(),bobo.getId()));
+		em.getTransaction().commit();
+		assertEquals(pv, pv2);
 	}
 	
 }
