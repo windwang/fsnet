9ecc862bc30b261aa72cdddefe663258d4cf743b
supergeoffrey <supergeoffrey@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-24 22:18:28 +0000

    remove critical tests
    git-svn-id: http://fsnet.googlecode.com/svn@1160 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fr.univartois.ili.fsnet/entities/src/test/java/fr/univartois/ili/fsnet/entities/test/ProfileVisitTest.java
+++ trunk/fr.univartois.ili.fsnet/entities/src/test/java/fr/univartois/ili/fsnet/entities/test/ProfileVisitTest.java
@@ -2,13 +2,13 @@ package fr.univartois.ili.fsnet.entities.test;
 
 import static org.junit.Assert.*;
 
-import java.util.Date;
+
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -44,55 +44,6 @@ public class ProfileVisitTest {
 		
 	}
 	
-	@After
-	public void tearDown(){
-		em.close();
-	}
-	
-	@Test
-	public void profileVisiteConstructorTest(){
-		assertSame(bobo,pv.getVisited());
-		assertSame(bibi,pv.getVisitor());
-		assert(new Date().before(pv.getLastVisite()));
-	}
-	
-	@Test 
-	public void setAndGetVisitorTest(){
-		pv.setVisitor(toto);
-		assertSame(toto,pv.getVisitor());
-	}
-	
-	@Test 
-	public void setAndGetVisitedTest(){
-		pv.setVisited(toto);
-		assertSame(toto,pv.getVisited());
-	}
-	
-	@Test 
-	public void setAndGetLastVisiteTest(){
-		Date d =new Date();
-		pv.setLastVisite( d);
-		assertSame(pv.getLastVisite(),d);
-	}
-	
-	@Test 
-	public void visitAgainstTest(){
-		Date last = pv.getLastVisite();
-		Date d =new Date();
-		try {
-			Thread.sleep(2);
-		} catch (InterruptedException e) {
-			fail();
-		}
-		assertTrue(d.after(last));
-		pv.visiteAgainst();
-		try {
-			Thread.sleep(2);
-		} catch (InterruptedException e) {
-			fail();
-		}
-		assertTrue(d.before(pv.getLastVisite()));
-	}
 	
 	@Test 
 	public void persistanceTest(){
