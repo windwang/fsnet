a0a0c4bb883114f78dc1091c9252e50b62259324
alexandre.lohez <alexandre.lohez@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-02-25 19:55:03 +0000

    first step for home page informations
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@1178 07c314b0-eee8-11dd-b478-27b5dcd53177

--- fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/Home.java
+++ fr.univartois.ili.fsnet/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/Home.java
@@ -3,7 +3,9 @@ package fr.univartois.ili.fsnet.actions;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -17,27 +19,42 @@ import org.apache.struts.actions.MappingDispatchAction;
 
 import fr.univartois.ili.fsnet.actions.utils.UserUtils;
 import fr.univartois.ili.fsnet.commons.utils.PersistenceProvider;
+import fr.univartois.ili.fsnet.entities.Interaction;
 import fr.univartois.ili.fsnet.entities.PrivateMessage;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
+import fr.univartois.ili.fsnet.facade.forum.iliforum.InteractionFacade;
 
 public class Home extends MappingDispatchAction {
 
 	public ActionForward doDashboard(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		EntityManager em = PersistenceProvider.createEntityManager();
 		SocialEntity authenticatedUser = UserUtils.getAuthenticatedUser(
 				request, em);
+		InteractionFacade facade = new InteractionFacade(em);
+
 		if (form == null) {
 			List<PrivateMessage> userMessages = new ArrayList<PrivateMessage>(
 					authenticatedUser.getReceivedPrivateMessages());
 			Collections.reverse(userMessages);
 			request.setAttribute("messages", userMessages);
+
+			
+			// TODO possible de faire variable.class dans JSP ? trouver un moyen  de trier par avance dans le cas contraire
+			HashMap<String, List<Interaction>> resultMap = facade
+					.getLastInteractions(authenticatedUser);
+
+			for (Map.Entry<String, List<Interaction>> interactionEntry : resultMap
+					.entrySet()) {
+				request.setAttribute(interactionEntry.getKey(),
+						interactionEntry.getValue());
+			}
 		} else {
 			// TODO
 		}
 		em.close();
 		return mapping.findForward("success");
 	}
-	
+
 }
