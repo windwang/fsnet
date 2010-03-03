08d6730ff18b10d26307c937b15a7feda30ea5e3
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-03-03 23:44:27 +0000

    [maven-release-plugin]  copy for tag 0.4.0
    git-svn-id: http://fsnet.googlecode.com/svn@1282 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ tags/0.4.0/admin/src/main/webapp/content/communities/CreateCommunity.jsp
@@ -0,0 +1,63 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+
+<%@taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
+<%@taglib uri="http://struts.apache.org/tags-bean" prefix="bean"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+
+
+<h3>
+  <bean:message key="communities.create"/>
+</h3>
+
+<html:form action="/CreateCommunity">
+	
+    <table id="CreateCommunity">
+        <tr>
+            <td>
+                <label for="name">
+                    <bean:message key="communities.name"/>
+                </label>
+            </td>
+            <td>
+                <html:text property="name" styleId="name" errorStyleClass="error"/>
+            </td>
+        </tr>
+        <tr class="errorMessage">
+            <td colspan="2">
+                <html:errors property="name"/>
+                <html:errors property="createdCommunityName"/>
+            </td>
+        </tr>
+        <tr>
+       		<td>
+                <label for="creator">
+                    <bean:message key="communities.creator"/>
+                </label>
+            </td>
+            <td>
+				<html:select property="socialEntityId" styleClass="select">
+					<html:option value=""><bean:message key="communities.listMember"/></html:option>
+					<c:forEach var="socialEntity" items="${allMembers}">
+						<html:option value="${socialEntity.id}">${socialEntity.name} ${socialEntity.firstName}</html:option>
+					</c:forEach>
+				</html:select>
+            </td>
+         </tr>
+         <tr class="errorMessage">
+            <td colspan="2">
+                <html:errors property="socialEntityId"/>
+            </td>
+        </tr>
+       
+        <tr>        	
+            <td colspan="2">
+                <html:submit styleClass="button">
+                   <bean:message key="communities.validate"/>
+                </html:submit>
+            </td>
+        </tr>
+    </table>
+</html:form>
+
---
+++ tags/0.4.0/entities/src/main/java/fr/univartois/ili/fsnet/entities/Interest.java
@@ -0,0 +1,212 @@
+package fr.univartois.ili.fsnet.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.OrderColumn;
+import javax.persistence.PostRemove;
+
+/**
+ * 
+ * The class Interest.
+ * 
+ */
+@Entity
+public class Interest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * The identifier.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    
+    /**
+     * The list of social entities that are affected by this interest.
+     */
+    @ManyToMany(mappedBy = "interests")
+    @JoinColumn(nullable = false)
+    private Set<SocialEntity> entities = new HashSet<SocialEntity>();
+    
+    @OneToMany(mappedBy="parentInterest")
+    private Set<Interest> childrenInterests;
+    
+    @ManyToOne
+    private Interest parentInterest;
+    
+    /**
+     * The interest name.
+     */
+    @Column(nullable = false, unique = true)
+    @OrderColumn
+    private String name;
+
+    /**
+     * Constructor of the class Interest.
+     */
+    public Interest() {
+    }
+
+    /**
+     * Constructor of the class Interest.
+     *
+     * @param entities
+     * @param name
+     */
+    public Interest(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.entities = new HashSet<SocialEntity>();
+    }
+    
+    /**
+     * Constructor of the class Interest.
+     *
+     * @param entities
+     * @param name
+     * @param parentInterest
+     * 
+     * @author Alexandre Lohez <alexandre.lohez at gmail.com>
+     */
+    public Interest(String name, Interest parentInterest) {
+        if (name == null || parentInterest == null) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.entities = new HashSet<SocialEntity>();
+        this.parentInterest = parentInterest;
+    }
+
+    /**
+     *
+     * @return the identifier.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Gives an identifier to the interest.
+     *
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return the list of social entities that are affected by this interest.
+     */
+    public Set<SocialEntity> getEntities() {
+        return entities;
+    }
+
+    /**
+     * Gives the list of social entities that are affected by this interest.
+     *
+     * @param entities
+     */
+    public void setEntities(Set<SocialEntity> entities) {
+        this.entities = entities;
+    }
+
+    /**
+     *
+     * @return the interest name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gives a name to the interest.
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
+	/**
+	 * @return the children's interests of this interests
+	 */
+	public Set<Interest> getChildrenInterests() {
+		return childrenInterests;
+	}
+
+	/**
+	 * @param childrenInterests the children's interests to set
+	 */
+	public void setChildrenInterests(Set<Interest> childrenInterests) {
+		this.childrenInterests = childrenInterests;
+	}
+
+	/**
+	 * @return the parent's interest of this interest
+	 */
+	public Interest getParentInterest() {
+		return parentInterest;
+	}
+
+	/**
+	 * @param parentInterest the parent's interest to set
+	 */
+	public void setParentInterest(Interest parentInterest) {
+		this.parentInterest = parentInterest;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Interest other = (Interest) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	@PostRemove
+	public void onRemove() {
+		Logger.getAnonymousLogger().log(Level.INFO, "Interest.onInterestRemove("+getId()+")");
+		for (SocialEntity entity : entities) {
+			entity.getInterests().remove(this);
+		}
+	}
+    
+}
