a91c3e38903b12bb42ed6f0315c42788f0ddf291
proucelle.matthieu <proucelle.matthieu@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-03-03 16:37:33 +0000

    facade : added proper SE remove.
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@1273 07c314b0-eee8-11dd-b478-27b5dcd53177

--- facade/src/main/java/fr/univartois/ili/fsnet/facade/HubFacade.java
+++ facade/src/main/java/fr/univartois/ili/fsnet/facade/HubFacade.java
@@ -14,44 +14,59 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
  */
 public class HubFacade {
 
-	private final EntityManager em;
+    private final EntityManager em;
 
-	public HubFacade(EntityManager em) {
-		this.em = em;
-	}
+    public HubFacade(EntityManager em) {
+        this.em = em;
+    }
 
-	/**
-	 * Create and persist a new Hub
-	 * 
-	 * @param community
-	 *            the related Community
-	 * @param creator
-	 *            the Hub creator
-	 * @param name
-	 *            Hub name
-	 * @return the new Hub
-	 */
-	public final Hub createHub(Community community, SocialEntity creator,
-			String name) {
-                //TODO actually no gestion for commmunity
-		if (creator == null || name == null) {
-			throw new IllegalArgumentException();
-		}
-		Hub hub = new Hub(community, creator, name);
-		community.getHubs().add(hub);
-		em.persist(hub);
-		return hub;
-	}
+    /**
+     * Create and persist a new Hub
+     *
+     * @param community
+     *            the related Community
+     * @param creator
+     *            the Hub creator
+     * @param name
+     *            Hub name
+     * @return the new Hub
+     */
+    public final Hub createHub(Community community, SocialEntity creator,
+            String name) {
+        //TODO actually no gestion for commmunity
+        if (creator == null || name == null) {
+            throw new IllegalArgumentException();
+        }
+        Hub hub = new Hub(community, creator, name);
+        community.getHubs().add(hub);
+        em.persist(hub);
+        return hub;
+    }
 
+    /**
+     *
+     * @param id
+     * @return the Hub identified by id
+     */
+    public final Hub getHub(int id) {
+        return em.find(Hub.class, id);
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return the Hub identified by id
-	 */
-	public final Hub getHub(int id) {
-		return em.find(Hub.class, id);
-	}
+    /**
+     * Search a Hub
+     *
+     * @param pattern
+     * @return a list of pattern matching hub
+     */
+    public final List<Hub> searchHub(String pattern) {
+        if (pattern == null) {
+            throw new IllegalArgumentException();
+        }
+        List<Hub> hubs = em.createQuery(
+                "SELECT hub FROM Hub hub WHERE hub.title LIKE :hubName ",
+                Hub.class).setParameter("hubName", "%" + pattern + "%").getResultList();
+        return hubs;
+    }
 	
 	/**
 	 * 
@@ -69,39 +84,20 @@ public class HubFacade {
 		return hub;
 	}
 
-	/**
-	 * Search a Hub
-	 * 
-	 * @param pattern
-	 * @return a list of pattern matching hub
-	 */
-	public final List<Hub> searchHub(String pattern) {
-		if (pattern == null) {
-			throw new IllegalArgumentException();
-		}
-		List<Hub> hubs = em.createQuery(
-				"SELECT hub FROM Hub hub WHERE hub.title LIKE :hubName ",
-				Hub.class).setParameter("hubName", "%" + pattern + "%")
-				.getResultList();
-		return hubs;
-	}
-
-	/**
-	 * Search a Hub in a Community
-	 * 
-	 * @param pattern
-	 * @param community
-	 * @return a list of pattern matching hub
-	 */
-	public final List<Hub> searchHub(String pattern, Community community) {
-		if (pattern == null || community == null) {
-			throw new IllegalArgumentException();
-		}
-		List<Hub> hubs = em
-				.createQuery(
-						"SELECT hub FROM Hub hub WHERE hub.title LIKE :hubName AND hub.community = :com",
-						Hub.class).setParameter("hubName", "%" + pattern + "%")
-				.setParameter("com", community).getResultList();
-		return hubs;
-	}
+    /**
+     * Search a Hub in a Community
+     *
+     * @param pattern
+     * @param community
+     * @return a list of pattern matching hub
+     */
+    public final List<Hub> searchHub(String pattern, Community community) {
+        if (pattern == null || community == null) {
+            throw new IllegalArgumentException();
+        }
+        List<Hub> hubs = em.createQuery(
+                "SELECT hub FROM Hub hub WHERE hub.title LIKE :hubName AND hub.community = :com",
+                Hub.class).setParameter("hubName", "%" + pattern + "%").setParameter("com", community).getResultList();
+        return hubs;
+    }
 }
