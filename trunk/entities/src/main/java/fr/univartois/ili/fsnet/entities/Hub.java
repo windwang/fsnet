c534ea56a28aed7467a0422da9166466fe6147ca
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-03-12 02:43:23 +0000

    Removed unused imports in entities java sources
    git-svn-id: http://fsnet.googlecode.com/svn@1328 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/entities/src/main/java/fr/univartois/ili/fsnet/entities/Hub.java
+++ trunk/entities/src/main/java/fr/univartois/ili/fsnet/entities/Hub.java
@@ -2,8 +2,6 @@ package fr.univartois.ili.fsnet.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
