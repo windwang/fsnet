2ee1e1a31ee56a778e0728a85183a5c4827a7bf1
Grynch62 <Grynch62@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-03-17 14:01:36 +0000

    actionMouseListener adding that highlights the message panel
    git-svn-id: http://fsnet.googlecode.com/svn@1433 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/trayDesktop/src/main/java/fr/univartois/ili/fsnet/trayDesktop/views/NotificationFrame.java
+++ trunk/trayDesktop/src/main/java/fr/univartois/ili/fsnet/trayDesktop/views/NotificationFrame.java
@@ -1,8 +1,8 @@
 package fr.univartois.ili.fsnet.trayDesktop.views;
 
+import java.awt.Color;
 import java.awt.Desktop;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -19,29 +19,29 @@ import javax.swing.BoxLayout;
 
 public class NotificationFrame {
 
-    private JFrame frame;
-    private Point point;
-    private Point position;
+	private JFrame frame;
+	private Point point;
+	private Point position;
 
-    public NotificationFrame(Point position) {
-        frame = new JFrame("Fsnet");
-        this.position = position;
-        init(position);
-    }
+	public NotificationFrame(Point position) {
+		frame = new JFrame("Fsnet");
+		this.position = position;
+		init(position);
+	}
 
-    public JFrame getFrame() {
-        return frame;
-    }
+	public JFrame getFrame() {
+		return frame;
+	}
 
-    public void addPanelMessage(int nbMessage) {
-        System.out.println("new panel with " + nbMessage + " messages");
-        NotificationPanel panel = new NotificationPanel(nbMessage,
+	public void addPanelMessage(int nbMessage) {
+        final NotificationPanel panel = new NotificationPanel(nbMessage,
                 "nouveaux messages");
         this.frame.add(panel.getPanel());
         this.frame.pack();
         initPosition(position);
         this.frame.setLocation(point.x, point.y);
-        panel.getLabel().addMouseListener(new MouseAdapter() {
+        final Color normalColor = panel.getPanel().getBackground();
+        panel.getLabel().addMouseListener(new MouseAdapter() {    
 
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -54,58 +54,81 @@ public class NotificationFrame {
                     Logger.getLogger(FSNetTray.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 frame.dispose();
+            } 
+            
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            	// TODO Auto-generated method stub
+            	panel.getPanel().setBackground(new Color(0xc6c2bf));            	
             }
-        });        
+            
+            @Override
+            public void mouseExited(MouseEvent e) {
+            	// TODO Auto-generated method stub
+            	panel.getPanel().setBackground(normalColor);  
+            }
+            
+        });  
+        
+    
+        
+        
+        
     }
 
-    public void addPanelContact(int nbContact) {
-        NotificationPanel panel = new NotificationPanel(nbContact,
-                "nouvelles demandes de contacts");
-        this.frame.add(panel.getPanel());
-        this.frame.pack();
-        initPosition(position);
-        this.frame.setLocation(point.x, point.y);
-        	  panel.getLabel().addMouseListener(new MouseAdapter() {
+	public void addPanelContact(int nbContact) {
+		NotificationPanel panel = new NotificationPanel(nbContact,
+				"nouvelles demandes de contacts");
+		this.frame.add(panel.getPanel());
+		this.frame.pack();
+		initPosition(position);
+		this.frame.setLocation(point.x, point.y);
+		panel.getLabel().addMouseListener(new MouseAdapter() {
 
-                  @Override
-                  public void mouseClicked(MouseEvent e) {
-                      try {
-                          Desktop.getDesktop().browse(new URI(Options.getFsnetUrl()+"/Contacts.do"));
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					Desktop.getDesktop().browse(
+							new URI(Options.getFsnetUrl() + "/Contacts.do"));
 
-                      } catch (URISyntaxException ex) {
-                          Logger.getLogger(FSNetTray.class.getName()).log(Level.SEVERE, null, ex);
-                      } catch (IOException ex) {
-                          Logger.getLogger(FSNetTray.class.getName()).log(Level.SEVERE, null, ex);
-                      }
-                      frame.dispose();
-                  }
-              }); 	
-    }
+				} catch (URISyntaxException ex) {
+					Logger.getLogger(FSNetTray.class.getName()).log(
+							Level.SEVERE, null, ex);
+				} catch (IOException ex) {
+					Logger.getLogger(FSNetTray.class.getName()).log(
+							Level.SEVERE, null, ex);
+				}
+				frame.dispose();
+			}
+		});
+	}
 
-    public void init(Point position) {
-        this.frame.setUndecorated(true);
-        BoxLayout flow = new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS);
-        this.frame.setVisible(true);
-        this.frame.setLayout(flow);
-        this.frame.pack();
-        initPosition(position);
-        this.frame.setLocation(point.x, point.y);
-    }
+	public void init(Point position) {
+		this.frame.setUndecorated(true);
+		BoxLayout flow = new BoxLayout(frame.getContentPane(),
+				BoxLayout.PAGE_AXIS);
+		this.frame.setVisible(true);
+		this.frame.setLayout(flow);
+		this.frame.pack();
+		initPosition(position);
+		this.frame.setLocation(point.x, point.y);
+	}
 
-    public void initPosition(Point position) {
-        point = new Point();
-        Dimension tailleEcran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        int screenheight = (int) tailleEcran.getHeight();
-        int screenwidht = (int) tailleEcran.getWidth();
-        int framewidht = this.frame.getWidth();
-        int frameheight = this.frame.getHeight();
-        point.x = position.x;
-        point.y = position.y;
-        if ((framewidht + position.x) > screenwidht) {
-            point.x = position.x - (framewidht + position.x - screenwidht);
-        }
-        if ((frameheight + position.y) > screenheight) {
-            point.y = position.y - (frameheight + position.y - screenheight);
-        }
-    }
+	public void initPosition(Point position) {
+		point = new Point();
+		Dimension tailleEcran = java.awt.Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		int screenheight = (int) tailleEcran.getHeight();
+		int screenwidht = (int) tailleEcran.getWidth();
+		int framewidht = this.frame.getWidth();
+		int frameheight = this.frame.getHeight();
+		point.x = position.x;
+		point.y = position.y;
+		if ((framewidht + position.x) > screenwidht) {
+			point.x = position.x - (framewidht + position.x - screenwidht);
+		}
+		if ((frameheight + position.y) > screenheight) {
+			point.y = position.y - (frameheight + position.y - screenheight);
+		}
+	}
 }
