8048d5b97274d73e68d6e47e41474b0a450f15ec
Grynch62 <Grynch62@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-03-17 15:07:44 +0000

    i18n added to the notification frame and adjusting the size of the frame notification
    git-svn-id: http://fsnet.googlecode.com/svn@1449 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/trayDesktop/src/main/java/fr/univartois/ili/fsnet/trayDesktop/views/NotificationPanel.java
+++ trunk/trayDesktop/src/main/java/fr/univartois/ili/fsnet/trayDesktop/views/NotificationPanel.java
@@ -2,15 +2,19 @@ package fr.univartois.ili.fsnet.trayDesktop.views;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.util.ResourceBundle;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import fr.univartois.ili.fsnet.trayDesktop.TrayLauncher;
 
 public class NotificationPanel {
 
     private JPanel panel;
     private JLabel label;
     private BorderLayout border;
+    private final ResourceBundle trayi18n = TrayLauncher.getBundle();
 
     public NotificationPanel(int nb, String message) {
         this.panel = new JPanel();
@@ -22,7 +26,7 @@ public class NotificationPanel {
     }
 
     public void init(int nb, String message) {
-        this.label = new JLabel("Vous avez " + nb + " " + message);
+        this.label = new JLabel(" "+trayi18n.getString("YOU")+" "+ nb + " " + message);
         border = new BorderLayout();
         panel.setLayout(border);
         panel.setMinimumSize(new Dimension(0,30));
