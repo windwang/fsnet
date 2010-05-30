c0425eb5c36d8f3e43d59cfa6891f7f008208873
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-05-30 22:19:02 +0000

    Fixed : bad class in test suite
    git-svn-id: http://fsnet.googlecode.com/svn@1497 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/facade/src/test/java/fr/univartois/ili/fsnet/facade/test/AllTests.java
+++ trunk/facade/src/test/java/fr/univartois/ili/fsnet/facade/test/AllTests.java
@@ -1,13 +1,7 @@
 package fr.univartois.ili.fsnet.facade.test;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
-import fr.univartois.ili.fsnet.entities.ProfileVisite;
 
 /**
  *
@@ -16,34 +10,17 @@ import fr.univartois.ili.fsnet.entities.ProfileVisite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     AnnouncementFacadeTest.class,
-    SocialEntityFacadeTest.class,
+    CommunityFacadeTest.class,
     ContactFacadeTest.class,
-    PrivateMessageFacadeTest.class,
-    MeetingFacadeTest.class,
-    InterestFacadeTest.class,
-    InteractionFacadeTest.class,
     HubFacadeTest.class,
+    InteractionFacadeTest.class,
+    InterestFacadeTest.class,
+    MeetingFacadeTest.class,
+    PrivateMessageFacadeTest.class,
+    ProfileFacadeTest.class,
+    ProfileVisiteFacadeTest.class,
+    SocialEntityFacadeTest.class,
     TopicMessageFacadeTest.class,
     TopicFacadeTest.class,
-    CommunityFacadeTest.class,
-    ProfileFacadeTest.class,
-    ProfileVisite.class
 })
-public class AllTests {
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-}
+public class AllTests {}
