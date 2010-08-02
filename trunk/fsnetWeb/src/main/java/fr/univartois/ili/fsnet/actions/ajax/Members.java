f28b6bf230d4c4604e6ca69a52218d8ddf5ef510
supergeoffrey <supergeoffrey@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-08-02 11:10:03 +0000

    Delegate output xml generated code from Action to jsp for autocomplete recipients
    git-svn-id: http://fsnet.googlecode.com/svn@1514 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ajax/Members.java
+++ trunk/fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ajax/Members.java
@@ -1,6 +1,6 @@
 package fr.univartois.ili.fsnet.actions.ajax;
 
-import java.io.Writer;
+
 import java.util.Set;
 
 import javax.persistence.EntityManager;
@@ -17,6 +17,7 @@ import fr.univartois.ili.fsnet.commons.utils.PersistenceProvider;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.facade.SocialEntityFacade;
 
+
 /**
  *
  * @author Matthieu Proucelle <matthieu.proucelle at gmail.com>
@@ -25,8 +26,8 @@ public class Members extends Action {
 	
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (form != null) {
-
+    	ActionForward res = null;
+    	if (form != null) {
             DynaActionForm theform = (DynaActionForm) form; // NOSONAR
             String actualText = (String) theform.get("searchText");
             int index = actualText.lastIndexOf(",");
@@ -36,23 +37,10 @@ public class Members extends Action {
             SocialEntityFacade sef = new SocialEntityFacade(em);
             Set<SocialEntity> listSE = sef.searchSocialEntity(searchText);
             em.close();
-            Writer out = response.getWriter();
-            response.setContentType("text/xml");
-
-            out.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-            out.append("<root>");
-            for (SocialEntity se : listSE) {
-                out.append("<item id='");
-                out.append(""+se.getId());
-                out.append("' label='"); 
-                out.append(completeUser);
-                out.append(se.getEmail());
-                out.append( "'/>");
-            }
-            out.append("</root>");
-            out.flush();
-            out.close();
+            request.setAttribute("matchesSocialEntity", listSE);
+            request.setAttribute("completeUsers", completeUser);
+            res = mapping.findForward("success");
         }
-        return null;
+        return res;
     }
 }
