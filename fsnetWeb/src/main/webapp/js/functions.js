6dd8069b6abdd1115c5440a2cce340bb1f7681b2
mat.boniface <mat.boniface@07c314b0-eee8-11dd-b478-27b5dcd53177>
2010-11-03 17:44:30 +0000

    Refactoring of favorite fragment jsp
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@1523 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ fsnetWeb/src/main/webapp/js/functions.js
@@ -0,0 +1,50 @@
+function getElementsByClass(tag, class) {
+	var elements = document.getElementsByTagName(tag);
+	var results = new Array();
+	for ( var i = 0; i < elements.length; i++) {
+		if (elements[i].className == class) {
+			results[results.length] = elements[i];
+		}
+	}
+	return results;
+}
+
+function getXhr() {
+	var xhr;
+	if (window.XMLHttpRequest)
+		xhr = new XMLHttpRequest();
+	else if (window.ActiveXObject) {
+		try {
+			xhr = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	} else
+		xhr = null;
+	return xhr;
+}
+
+function switchFavorite(id) {
+	var xhr = getXhr();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			isFavorite[id] = !isFavorite[id];
+			var favoriteImages = getElementsByClass('img', 'favorite' + id);
+			for ( var i = 0; i < favoriteImages.length; i++) {
+				favoriteImages[i].src = "images/"
+						+ (isFavorite[id] ? "favorite.png" : "non-favorite.png");
+			}
+		}
+	}
+	if (isFavorite[id]) {
+		xhr.open("POST", 'RemoveFavorite.do', true);
+		xhr.setRequestHeader('Content-Type',
+				'application/x-www-form-urlencoded');
+		xhr.send("interactionId=" + id);
+	} else {
+		xhr.open("POST", 'AddFavorite.do', true);
+		xhr.setRequestHeader('Content-Type',
+				'application/x-www-form-urlencoded');
+		xhr.send("interactionId=" + id);
+	}
+}
