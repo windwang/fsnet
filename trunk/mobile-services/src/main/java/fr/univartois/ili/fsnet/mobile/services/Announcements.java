97468919f21011b58aaf9178e2764828af84aee3
wvermersch@gmail.com <wvermersch@gmail.com@07c314b0-eee8-11dd-b478-27b5dcd53177>
2011-02-25 16:14:20 +0000

    New web services/resources implemented with jax rs and new classes for the android application, some changes will be done, to pass the delay in parameter of the web services/resources and implemented the remnants web services/resources
    git-svn-id: http://fsnet.googlecode.com/svn@1716 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ trunk/mobile-services/src/main/java/fr/univartois/ili/fsnet/mobile/services/Announcements.java
@@ -0,0 +1,67 @@
+package fr.univartois.ili.fsnet.mobile.services;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+
+import fr.univartois.ili.fsnet.entities.Announcement;
+import fr.univartois.ili.fsnet.facade.SocialEntityFacade;
+import fr.univartois.ili.fsnet.mobile.services.model.AuthInfo;
+import fr.univartois.ili.fsnet.mobile.services.model.RestAnnouncement;
+
+@Resource
+@Path("/announcements")
+public class Announcements {
+
+	private EntityManagerFactory factory;
+	private EntityManager em;
+
+	public Announcements() {
+		factory = Persistence.createEntityManagerFactory("fsnetjpa");
+		em = factory.createEntityManager();
+	}
+
+	/**
+	 * Return the unread personal messages
+	 * 
+	 * @param login
+	 * @param password
+	 * @return
+	 */
+	@POST
+	@Path("/new")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public GenericEntity<List<RestAnnouncement>> getNewAnnouncements(AuthInfo authInfo) {
+		Logger.getAnonymousLogger().info(authInfo.toString());
+		int decalage = 10;
+		Date d = new Date();
+		Date e = new Date(d.getTime()- decalage*60000);
+		List<RestAnnouncement> announcements = new ArrayList<RestAnnouncement>();
+		SocialEntityFacade sef = new SocialEntityFacade(em);
+		if (sef.isMember(authInfo.getLogin(), authInfo.getPassword())) {
+			TypedQuery<Announcement> announceQuery = em
+					.createQuery(
+							"SELECT m FROM Announcement m where type(m) in (Announcement) and m.creationDate>=?1",
+							Announcement.class);
+			announceQuery.setParameter(1, e);
+			for (Announcement announce : announceQuery.getResultList()) {
+				announcements.add(new RestAnnouncement(announce));
+			}
+		}
+		return new GenericEntity<List<RestAnnouncement>>(announcements){};
+	}
+}
--- trunk/mobile-services/src/test/java/fr/univartois/ili/fsnet/mobile/services/InfoTest.java
+++ trunk/mobile-services/src/test/java/fr/univartois/ili/fsnet/mobile/services/InfoTest.java
@@ -1,4 +1,5 @@
 package fr.univartois.ili.fsnet.mobile.services;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.core.MediaType;
@@ -15,6 +16,7 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 import fr.univartois.ili.fsnet.mobile.services.model.AuthInfo;
+import fr.univartois.ili.fsnet.mobile.services.model.RestAnnouncement;
 import fr.univartois.ili.fsnet.mobile.services.model.RestPrivateMessage;
 
 
@@ -22,16 +24,16 @@ public class InfoTest {
 	
 	private static final String ENDPOINT = "http://localhost:8080/webservice/jaxrs";
 	
-	private static final String LOGIN = "mat.boniface@gmail.com";
+	private static final String LOGIN = "wvermersch@gmail.com";
 	
-	private static final String PASSWORD = "password";
+	private static final String PASSWORD = "blabla";
 
 	@Test
 	public void empty() {
 		
 	}
 	
-	//@Test
+	@Test
 	public void newMessages() {
 		ClientConfig cc = new DefaultClientConfig();
 		cc.getClasses().add(JacksonJsonProvider.class);
@@ -43,7 +45,45 @@ public class InfoTest {
 				.entity(new AuthInfo(LOGIN, PASSWORD), MediaType.APPLICATION_JSON)//
 				.type(MediaType.APPLICATION_JSON)//
 				.post(new GenericType<List<RestPrivateMessage>>(){});
+		
 		Assert.assertNotNull(messages);
 	}
 
+	@Test
+	public void newMeetings() {
+		ClientConfig cc = new DefaultClientConfig();
+		cc.getClasses().add(JacksonJsonProvider.class);
+		Client client = Client.create(cc);
+		
+		WebResource webResource = client.resource(ENDPOINT + "/meetings/new");
+		
+		List<RestAnnouncement> meetings = webResource.accept(MediaType.APPLICATION_JSON) //
+				.entity(new AuthInfo(LOGIN, PASSWORD), MediaType.APPLICATION_JSON)//
+				.type(MediaType.APPLICATION_JSON)//
+				.post(new GenericType<List<RestAnnouncement>>(){});
+		System.out.println("Nouveaux meetings :"+meetings.size());
+		for(RestAnnouncement meeting : meetings){
+			System.out.println("Titre meeting :"+meeting.getTitle() +"créé par "+meeting.getFrom());
+		}
+		Assert.assertNotNull(meetings);
+	}
+	
+	@Test
+	public void newAnnouncements() {
+		ClientConfig cc = new DefaultClientConfig();
+		cc.getClasses().add(JacksonJsonProvider.class);
+		Client client = Client.create(cc);
+		
+		WebResource webResource = client.resource(ENDPOINT + "/announcements/new");
+		
+		List<RestAnnouncement> meetings = webResource.accept(MediaType.APPLICATION_JSON) //
+				.entity(new AuthInfo(LOGIN, PASSWORD), MediaType.APPLICATION_JSON)//
+				.type(MediaType.APPLICATION_JSON)//
+				.post(new GenericType<List<RestAnnouncement>>(){});
+		System.out.println("Nouvelles annonces:"+meetings.size());
+		for(RestAnnouncement meeting : meetings){
+			System.out.println("Titre annonce :"+meeting.getTitle() +"créé par "+meeting.getFrom());
+		}
+		Assert.assertNotNull(meetings);
+	}
 }
