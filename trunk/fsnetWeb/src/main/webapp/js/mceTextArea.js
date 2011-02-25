ac1b846bd650ceb64cd938ce4c0449b9a024399b
charlotte.pora.code <charlotte.pora.code@07c314b0-eee8-11dd-b478-27b5dcd53177>
2011-02-25 09:48:11 +0000

    Fix for issue 248 : accents in private messages
    git-svn-id: http://fsnet.googlecode.com/svn@1715 07c314b0-eee8-11dd-b478-27b5dcd53177

--- trunk/fsnetWeb/src/main/webapp/js/mceTextArea.js
+++ trunk/fsnetWeb/src/main/webapp/js/mceTextArea.js
@@ -8,6 +8,6 @@ tinyMCE.init({
         theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
 
         theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left"
-
+        theme_advanced_toolbar_align : "left",
+        entity_encoding : "raw"
 });
