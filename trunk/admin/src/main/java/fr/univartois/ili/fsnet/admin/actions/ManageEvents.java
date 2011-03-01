e1a5c1cc9eace8b05989a52de98c26d148bfdccf
bouragbamohamed <bouragbamohamed@07c314b0-eee8-11dd-b478-27b5dcd53177>
2011-03-01 09:59:52 +0000

    Allow the administrator to delete an events
    git-svn-id: http://fsnet.googlecode.com/svn@1727 07c314b0-eee8-11dd-b478-27b5dcd53177

---
+++ trunk/admin/src/main/java/fr/univartois/ili/fsnet/admin/actions/ManageEvents.java
@@ -0,0 +1,154 @@
+package fr.univartois.ili.fsnet.admin.actions;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.RollbackException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionRedirect;
+import org.apache.struts.action.DynaActionForm;
+import org.apache.struts.actions.MappingDispatchAction;
+
+import fr.univartois.ili.fsnet.commons.pagination.Paginator;
+import fr.univartois.ili.fsnet.commons.utils.DateUtils;
+import fr.univartois.ili.fsnet.commons.utils.PersistenceProvider;
+import fr.univartois.ili.fsnet.entities.Interest;
+import fr.univartois.ili.fsnet.entities.Meeting;
+import fr.univartois.ili.fsnet.entities.SocialEntity;
+import fr.univartois.ili.fsnet.facade.InteractionFacade;
+import fr.univartois.ili.fsnet.facade.InteractionRoleFacade;
+import fr.univartois.ili.fsnet.facade.InterestFacade;
+import fr.univartois.ili.fsnet.facade.MeetingFacade;
+
+/**
+ * Execute CRUD Actions for the entity Event
+ * 
+ * @author Mohamed Bouragba
+ */
+public class ManageEvents extends MappingDispatchAction implements CrudAction {
+	private static EntityManagerFactory factory = Persistence
+			.createEntityManagerFactory("fsnetjpa");
+	private static final Logger logger = Logger.getAnonymousLogger();
+
+	@Override
+	public ActionForward create(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		return null;
+	}
+
+	@Override
+	public ActionForward modify(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO code pour la modification
+
+		return null;
+	}
+
+	@Override
+	public ActionForward delete(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
+		String eventId = (String) dynaForm.get("eventId");
+		EntityManager em = factory.createEntityManager();
+		// SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
+		InteractionFacade interactionFacade = new InteractionFacade(em);
+		MeetingFacade meetingFacade = new MeetingFacade(em);
+
+		Meeting meeting = meetingFacade.getMeeting(Integer.parseInt(eventId));
+		if (meeting != null) {
+			em.getTransaction().begin();
+			SocialEntity user = meeting.getCreator();
+			Set<SocialEntity> followingEntitys = meeting.getFollowingEntitys();
+			for (SocialEntity se : followingEntitys) {
+				se.getFavoriteInteractions().remove(meeting);
+			}
+			InteractionRoleFacade interactionRoleFacade = new InteractionRoleFacade(
+					em);
+			interactionRoleFacade.unsubscribeAll(meeting);
+			interactionFacade.deleteInteraction(user, meeting);
+			try {
+				em.getTransaction().commit();
+			} catch (RollbackException e) {
+				servlet.log("no commit", e);
+			}
+			em.close();
+		}
+
+		return mapping.findForward("success");
+	}
+
+	@Override
+	public ActionForward search(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		DynaActionForm searchForm = (DynaActionForm) form; // NOSONAR
+		String searchString = (String) searchForm.get("searchString");
+
+		EntityManager em = PersistenceProvider.createEntityManager();
+		em.getTransaction().begin();
+		MeetingFacade meetingFacade = new MeetingFacade(em);
+		List<Meeting> results = meetingFacade.searchMeeting(searchString);
+		em.getTransaction().commit();
+		em.close();
+
+		Paginator<Meeting> paginator = new Paginator<Meeting>(results, request,
+				"eventsLists");
+
+		request.setAttribute("eventsListPaginator", paginator);
+		return mapping.findForward("success");
+	}
+
+	@Override
+	public ActionForward display(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		DynaActionForm dynaForm = (DynaActionForm) form; // NOSONAR
+		String eventId = (String) dynaForm.get("eventId");
+
+		EntityManager em = PersistenceProvider.createEntityManager();
+		em.getTransaction().begin();
+
+		// SocialEntity member = UserUtils.getAuthenticatedUser(request, em);
+
+		MeetingFacade meetingFacade = new MeetingFacade(em);
+		Meeting event = meetingFacade.getMeeting(Integer.parseInt(eventId));
+		SocialEntity member = event.getCreator();
+
+		InteractionRoleFacade interactionRoleFacade = new InteractionRoleFacade(
+				em);
+		// boolean isSubscriber = interactionRoleFacade.isSubsriber(member,
+		// event);
+		Set<SocialEntity> subscribers = interactionRoleFacade
+				.getSubscribers(event);
+
+		em.getTransaction().commit();
+		em.close();
+
+		// TODO find a solution to paginate a Set
+
+		request.setAttribute("subscribers", subscribers);
+		request.setAttribute("subscriber", false);
+		request.setAttribute("event", event);
+		return mapping.findForward("success");
+	}
+}
---
+++ trunk/admin/src/main/webapp/content/events/DisplayEvents.jsp
@@ -0,0 +1,64 @@
+<%--
+	author : Bouragba Mohamed
+	source : Matthieu Proucelle
+ --%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
+<%@taglib uri="http://struts.apache.org/tags-logic" prefix="logic"%>
+<%@taglib uri="http://struts.apache.org/tags-bean" prefix="bean"%>
+<%@taglib uri="../../WEB-INF/ili.tld" prefix="ili"%>
+
+
+<h3>
+    <bean:message key="events.8"/>
+</h3>
+
+<html:form action="/Events" method="GET">
+    <div id="SearchEvent">
+        <html:text property="searchString" />
+        <html:submit styleClass="button" >
+            <bean:message key="events.11"/>
+        </html:submit>
+    </div>
+</html:form>
+
+<h3>
+    <bean:message key="events.9"/> :
+</h3>
+
+<c:choose>
+
+	<c:when test="${empty requestScope.eventsListPaginator.resultList}">
+	    <bean:message key="search.noResults"/>
+	</c:when>
+	
+	<c:otherwise>
+		<table  class="inLineTable">
+		    <c:forEach var="event" items="${requestScope.eventsListPaginator.resultList}">
+		        <tr>
+		            <th>
+		                <html:link action="/DisplayEvent">
+		                    ${event.title}
+		                    <html:param name="eventId" value="${event.id}"/>
+		                </html:link>
+		            </th>
+		            <td class="left">
+		                <bean:message key="events.10"/>
+		                <bean:write name="event" property="startDate" format="dd/MM/yyyy"/>,
+		                <bean:message key="events.16"/>
+						<html:link action="/DisplayMember">
+	                    	<html:param name="idMember" value="${event.creator.id}"/>
+	                    	${event.creator.firstName} ${event.creator.name}
+	              </html:link>
+		            </td>
+		        </tr>
+		    </c:forEach>
+		</table>
+		<c:set var="paginatorInstance" value="${requestScope.eventsListPaginator}" scope="request"/>
+		<c:set var="paginatorAction" value="/Events" scope="request"/>
+		<c:set var="paginatorTile" value="eventsLists" scope="request"/>
+		<c:import url="/content/pagination/Pagination.jsp"/>
+	</c:otherwise>
+</c:choose>
