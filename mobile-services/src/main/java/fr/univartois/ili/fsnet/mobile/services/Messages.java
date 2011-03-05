ceeb912f087b221a7eefd194d8b1991c4a005b50
wvermersch@gmail.com <wvermersch@gmail.com@07c314b0-eee8-11dd-b478-27b5dcd53177>
2011-03-05 20:12:28 +0000

    Modification of the webservice to return a compatible object with RestTemplate getForObject method
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@1742 07c314b0-eee8-11dd-b478-27b5dcd53177

--- mobile-services/src/main/java/fr/univartois/ili/fsnet/mobile/services/Messages.java
+++ mobile-services/src/main/java/fr/univartois/ili/fsnet/mobile/services/Messages.java
@@ -10,7 +10,6 @@ import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
@@ -19,7 +18,9 @@ import javax.ws.rs.core.MediaType;
 import fr.univartois.ili.fsnet.entities.PrivateMessage;
 import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.facade.SocialEntityFacade;
+import fr.univartois.ili.fsnet.mobile.services.model.RestMessagesList;
 import fr.univartois.ili.fsnet.mobile.services.model.RestPrivateMessage;
+
 @Path("/messages")
 public class Messages {
 
@@ -39,12 +40,11 @@ public class Messages {
 	 * @return
 	 */
 	@GET
-	@Path("/unread/{delay}")
+	@Path("/unread")
 	@Produces(MediaType.APPLICATION_JSON)
-	public GenericEntity<List<RestPrivateMessage>> getNewMessages( //
-			@QueryParam("login") String login, // 
-			@QueryParam("pwd") String password, //
-			@PathParam("delay") Integer delay) {
+	public GenericEntity<RestMessagesList> getNewMessages( //
+			@QueryParam("login") String login, //
+			@QueryParam("pwd") String password) {
 		Logger.getAnonymousLogger().info(login);
 		List<RestPrivateMessage> messages = new ArrayList<RestPrivateMessage>();
 		SocialEntityFacade sef = new SocialEntityFacade(em);
@@ -57,16 +57,40 @@ public class Messages {
 			messageQuery.setParameter("member", se);
 			for (PrivateMessage message : messageQuery.getResultList()) {
 				RestPrivateMessage restPrivateMessage = new RestPrivateMessage();
-				
-				String from = message.getFrom().getName() + " " + message.getFrom().getFirstName();
+
+				String from = message.getFrom().getName() + " "
+						+ message.getFrom().getFirstName();
 				restPrivateMessage.setFrom(from);
-				restPrivateMessage.setMessageId(restPrivateMessage.getMessageId());
+				restPrivateMessage.setMessageId(restPrivateMessage
+						.getMessageId());
 				restPrivateMessage.setSubject(message.getSubject());
-				
+
 				messages.add(restPrivateMessage);
 			}
 		}
-		return new GenericEntity<List<RestPrivateMessage>>(messages){};
+		return new GenericEntity<RestMessagesList>(new RestMessagesList(
+				messages)) {
+		};
 	}
 
+	@GET
+	@Path("/count")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Integer getCountMessages( //
+			@QueryParam("login") String login, //
+			@QueryParam("pwd") String password) {
+		Logger.getAnonymousLogger().info(login);
+		SocialEntityFacade sef = new SocialEntityFacade(em);
+		if (sef.isMember(login, password)) {
+			SocialEntity se = sef.findByEmail(login);
+			TypedQuery<PrivateMessage> messageQuery = em
+					.createQuery(
+							"SELECT message FROM SocialEntity soc, IN(soc.receivedPrivateMessages) message where message.reed='false' and soc=:member",
+							PrivateMessage.class);
+			messageQuery.setParameter("member", se);
+			return messageQuery.getResultList().size();
+
+		}
+		return 0;
+	}
 }
