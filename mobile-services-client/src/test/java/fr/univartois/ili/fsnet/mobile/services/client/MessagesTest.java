813699e890ee193429391d8fa93789da9d1abf15
wvermersch@gmail.com <wvermersch@gmail.com@07c314b0-eee8-11dd-b478-27b5dcd53177>
2011-03-11 09:48:54 +0000

    Fix some bugs add the new return type of the web service, the provider is not the android messages provider (but is here for the call of the restful service with the basic traydesktop in the future)
    git-svn-id: http://fsnet.googlecode.com/svn/trunk@1824 07c314b0-eee8-11dd-b478-27b5dcd53177

--- mobile-services-client/src/test/java/fr/univartois/ili/fsnet/mobile/services/client/MessagesTest.java
+++ mobile-services-client/src/test/java/fr/univartois/ili/fsnet/mobile/services/client/MessagesTest.java
@@ -1,35 +1,38 @@
 package fr.univartois.ili.fsnet.mobile.services.client;
 
-import java.util.List;
-
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import fr.univartois.ili.fsnet.mobile.services.model.RestMessagesList;
 import fr.univartois.ili.fsnet.mobile.services.model.RestPrivateMessage;
 
 public class MessagesTest {
 
-	private static final String ENDPOINT = "http://localhost:8080/webservice/jaxrs";
+	private static final String ENDPOINT = "http://localhost:8080/webservice/jaxrs/";
 
 	private static final String LOGIN = "wvermersch@gmail.com";
 
 	private static final String PASSWORD = "blabla";
+	
 
 	@Test
 	public void empty() {
 
 	}
 
-	//@Test
+	@Test
 	public void newMessages() {
 		MessagesResourcesProvider provider = new MessagesResourcesProvider( //
 				ENDPOINT, //
 				LOGIN, //
 				PASSWORD);
 
-		List<RestPrivateMessage> messages = provider.unreadPrivateMessages(10);
+		RestMessagesList messages = provider.unreadPrivateMessages();
 
+		for(RestPrivateMessage mess : messages.getMessages()){
+			System.out.println("Message : "+mess.getSubject());
+		}
 		Assert.assertNotNull(messages);
 	}
 
