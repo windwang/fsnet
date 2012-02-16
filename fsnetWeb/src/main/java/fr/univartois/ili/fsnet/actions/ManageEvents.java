4735cc02e83f0eb529c5f287ebef1a00b43de7a0
Lahoucine AITELHAJ <lahoucine.aitelhaj@gmail.com>
2012-02-17 00:08:09 +0100

    fixing dates issues for creating events and announcements : time is now considered in current date

--- fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageEvents.java
+++ fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageEvents.java
@@ -53,7 +53,7 @@ public class ManageEvents extends MappingDispatchAction implements CrudAction {
 
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-				calendar.get(Calendar.DAY_OF_MONTH), 0, 0, -1);
+				calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY)-1, 0, 0);
 		Date today = calendar.getTime();
 
 		Date typedEventDate;
@@ -66,7 +66,7 @@ public class ManageEvents extends MappingDispatchAction implements CrudAction {
 			saveErrors(request, errors);
 			return null;
 		}
-
+		
 		if (typedEventDate.before(today)) {
 			ActionErrors errors = new ActionErrors();
 			errors.add(propertyKey, new ActionMessage(
