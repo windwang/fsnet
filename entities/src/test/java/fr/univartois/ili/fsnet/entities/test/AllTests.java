73512253d4985fa94db4cecd340cc2e6d6dc80ee
bfahd <bfahd@bfahd-HP-Compaq-8000-Elite-CMT-PC>
2013-02-08 16:17:51 +0000

    Correct the AllTest class with missing class MessageTest

--- entities/src/test/java/fr/univartois/ili/fsnet/entities/test/AllTests.java
+++ entities/src/test/java/fr/univartois/ili/fsnet/entities/test/AllTests.java
@@ -15,7 +15,7 @@ import org.junit.runners.Suite;
 
 ProfileVisitePKTest.class, CurriculumTest.class,CommunityTest.class, ConsultationChoiceTest.class, HubTest.class,
 		InteractionTest.class, InterestTest.class, MeetingTest.class, ProfileVisitTest.class,
-		SocialEntityTest.class,DegreeCvTest.class,MessageTest.class, SocialGroupTest.class,InteractionRolePKTest.class, TopicTest.class, PrivateMessageTest.class , AssociationDateDegreeCVTest.class
-		,InteractionGroupsTest.class, SocialElementTest.class})
+		SocialEntityTest.class,DegreeCvTest.class, SocialGroupTest.class,InteractionRolePKTest.class, TopicTest.class, PrivateMessageTest.class , AssociationDateDegreeCVTest.class
+		,InteractionGroupsTest.class, SocialElementTest.class, HobbiesCVTest.class, PropertyTest.class, FormationCvTest.class, EstablishmentCvTest.class, MessageTest.class})
 public class AllTests {
 }
