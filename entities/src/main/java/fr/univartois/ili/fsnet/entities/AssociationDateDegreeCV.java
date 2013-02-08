39451cbc3043e34e4304c9c76e7ea94bff5808d4
kevin <kevin@balingon-HP>
2013-02-08 13:39:14 +0100

    Test sur AssociationDateDegreeCV

--- entities/src/main/java/fr/univartois/ili/fsnet/entities/AssociationDateDegreeCV.java
+++ entities/src/main/java/fr/univartois/ili/fsnet/entities/AssociationDateDegreeCV.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
 @Entity
 public class AssociationDateDegreeCV implements Serializable {
 
@@ -19,7 +20,6 @@ public class AssociationDateDegreeCV implements Serializable {
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	@Id
 	private long id;
-
 	private Date startDate;
 	private Date endDate;
 
@@ -44,7 +44,7 @@ public class AssociationDateDegreeCV implements Serializable {
 	 * @param id
 	 *            the id to set
 	 */
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
@@ -92,14 +92,7 @@ public class AssociationDateDegreeCV implements Serializable {
 	public void setDegree(DegreeCV idDegree) {
 		this.degree = idDegree;
 	}
-
-	/**
-	 * @return the serialversionuid
-	 */
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
+	
 	/**
 	 * @return the idCurriculum
 	 */
