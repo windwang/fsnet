4321d7cc9129825c2e5c4842b83d66bef0553abe
Marc Leborgne <leborgneprojetsm2@gmail.com>
2013-03-02 16:58:06 +0100

    Migrate the ManageProfile action

--- fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageProfile.java
+++ fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageProfile.java
@@ -1,5 +1,7 @@
 package fr.univartois.ili.fsnet.actions;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,14 +23,12 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.struts.action.ActionErrors;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.String;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionRedirect;
-import org.apache.struts.actions.ActionSupport;
-import org.apache.struts.upload.FormFile;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.components.ActionMessage;
+import org.apache.struts2.dispatcher.mapper.ActionMapping;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 import fr.univartois.ili.fsnet.actions.utils.FacebookKeyManager;
 import fr.univartois.ili.fsnet.actions.utils.ImageManager;
@@ -56,7 +56,7 @@ import fr.univartois.ili.fsnet.facade.security.UnauthorizedOperationException;
  * @author Geoffrey Boulay
  * @author SAID Mohamed
  */
-public class ManageProfile extends ActionSupport implements CrudAction {
+public class ManageProfile extends ActionSupport implements CrudAction,ServletRequestAware {
 
 	private static final int MAX_PICTURE_SIZE = 500000;
 	/**
@@ -73,6 +73,22 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	private static final String IS_GROUP_RESPONSIBLE_ATTRIBUTE_NAME = "isGroupResponsible";
 	private static final String ERROR_UPDATE_ATTRIBUTE_STRING = "updateProfile.error.photo.fatal";
 
+	private HttpServletRequest request;
+	private String name;
+	private String firstName;
+	private String adress;
+	private String city;
+	private String sexe;
+	private String mail;
+	private String phone;
+	private String job;
+	private Date dateOfBirth;
+	private int id;
+	private File photo;
+	private String photoContentType;
+	private String photoUrl;
+	
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -83,54 +99,45 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
-	public final String create(
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	public final String create()
+			throws Exception {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
 	/**
-	 * @param dynaForm
 	 * @param em
 	 * @param request
 	 * @return
 	 */
-			HttpServletRequest request) {
-		ActionErrors res = new ActionErrors();
-		try {
-			Date birthday = DateUtils.formatDate(dynaForm
-					.getString(DATE_OF_BIRTH_FORM_FIELD_NAME));
-			Date actualDate = new Date();
-			if (birthday.after(actualDate)) {
-				res.add(DATE_OF_BIRTH_FORM_FIELD_NAME, new ActionMessage(
-						"date.error.invalid"));
-			}
-			Calendar cal = Calendar.getInstance();
-			cal.set(Calendar.YEAR, cal.get(Calendar.YEAR) - 100);
-			Date lastedDate = cal.getTime();
-			if (birthday.before(lastedDate)) {
-				res.add(DATE_OF_BIRTH_FORM_FIELD_NAME, new ActionMessage(
-						"date.error.invalid"));
-			}
-		} catch (ParseException e1) {
-			// DO NOTHING EMPTY DATE
+	private int verified(EntityManager em) throws ParseException {
+		int nbErreurs=0;
+		Date actualDate = new Date();
+		if (dateOfBirth.after(actualDate)) {
+			addFieldError(DATE_OF_BIRTH_FORM_FIELD_NAME, "date.error.invalid");
+			nbErreurs++;
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, cal.get(Calendar.YEAR) - 100);
+		Date lastedDate = cal.getTime();
+		if (dateOfBirth.before(lastedDate)) {
+			addFieldError(DATE_OF_BIRTH_FORM_FIELD_NAME, "date.error.invalid");
+			nbErreurs++;
 		}
 
 		if (!UserUtils.getAuthenticatedUser(request, em).getEmail()
-				.equals(dynaForm.getString(MAIL_FORM_FIELD_NAME))) {
+				.equals(mail)) {
 			SocialEntityFacade sef = new SocialEntityFacade(em);
 			em.getTransaction().begin();
-			SocialEntity se = sef.findByEmail(dynaForm
-					.getString(MAIL_FORM_FIELD_NAME));
+			SocialEntity se = sef.findByEmail(mail);
 			em.getTransaction().commit();
 			//em.close();
 
 			if (se != null) {
-				res.add(MAIL_FORM_FIELD_NAME, new ActionMessage(
-						"error.updateProfile.email.alwaysExist"));
+				addFieldError(MAIL_FORM_FIELD_NAME, "error.updateProfile.email.alwaysExist");
+				nbErreurs++;
 			}
 		}
-		return res;
+		return nbErreurs;
 	}
 
 	/*
@@ -143,51 +150,40 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
-	public final String modify(
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	public final String modify()
+			throws Exception {
 		EntityManager em = PersistenceProvider.createEntityManager();
 		SocialGroupFacade fascade = new SocialGroupFacade(em);
 
 		if (!fascade.isAuthorized(UserUtils.getAuthenticatedUser(request, em),
 				Right.MODIFY_PROFIL)) {
 			em.close();
-			return new ActionRedirect(mapping.findForward("unauthorized"));
+			return "unauthorized";
 		}
 
-		Date birthday = null;
 		addRightToRequest(request);
 
-		try {
-			birthday = DateUtils.formatDate(dynaForm
-					.getString(DATE_OF_BIRTH_FORM_FIELD_NAME));
-		} catch (ParseException e) {
-			// DO NOTHING EMPTY DATE
-		}
+		
 
-		ActionErrors actionsErrors = verified(dynaForm, em, request);
+		int actionsErrors = verified(em);
 
-		if (!actionsErrors.isEmpty()) {
-			saveErrors(request, actionsErrors);
+		if (actionsErrors>0) {
 			em.close();
-			return mapping.getInputForward();
+			return INPUT;
 		}
 
 		ProfileFacade pf = new ProfileFacade(em);
 		em.getTransaction().begin();
 		pf.editProfile(
 				UserUtils.getAuthenticatedUser(request, em),
-				dynaForm.getString("name"),
-				dynaForm.getString("firstName"),
-				new Address(dynaForm.getString("adress"), dynaForm
-						.getString("city")), birthday, dynaForm
-						.getString("sexe"), dynaForm.getString("job"), dynaForm
-						.getString(MAIL_FORM_FIELD_NAME).toLowerCase(),
-				dynaForm.getString("phone"));
+				name,
+				firstName,
+				new Address(adress, city), dateOfBirth, sexe, job, mail.toLowerCase(),
+				phone);
 		em.getTransaction().commit();
 		em.close();
 
-		return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+		return SUCCESS;
 	}
 
 	/**
@@ -208,8 +204,7 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
-	public final String delete(
-			HttpServletRequest request, HttpServletResponse response)
+	public final String delete()
 			throws IOException, ServletException {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
@@ -224,8 +219,7 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
-	public final String search(
-			HttpServletRequest request, HttpServletResponse response)
+	public final String search()
 			throws IOException, ServletException {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
@@ -239,32 +233,29 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public final String displayToModify(ActionMapping mapping,
-			ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException {
+	public final String displayToModify() throws Exception {
 		addKeyFacebookInRequest(request);
 		EntityManager em = PersistenceProvider.createEntityManager();
 		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
 		SocialGroupFacade sgf = new SocialGroupFacade(em);
 		addRightToRequest(request);
 		request.setAttribute("currentUser", user);
-		dyna.set("name", user.getName());
-		dyna.set("firstName", user.getFirstName());
+		name=user.getName();
+		firstName=user.getFirstName();
 
 		if (user.getAddress() != null) {
-			dyna.set("adress", user.getAddress().getAddress());
-			dyna.set("city", user.getAddress().getCity());
+			adress=user.getAddress().getAddress();
+			city=user.getAddress().getCity();
 		}
 
 		if (user.getBirthDate() != null) {
-			dyna.set(DATE_OF_BIRTH_FORM_FIELD_NAME,
-					formatter.format(user.getBirthDate()));
+			dateOfBirth=user.getBirthDate();
 		}
 
-		dyna.set("sexe", user.getSex());
-		dyna.set("job", user.getProfession());
-		dyna.set(MAIL_FORM_FIELD_NAME, user.getEmail());
-		dyna.set("phone", user.getPhone());
+		sexe=user.getSex();
+		job=user.getProfession();
+		mail=user.getEmail();
+		phone=user.getPhone();
 
 		if (sgf.isMasterGroup(user)) {
 			request.getSession(true).setAttribute(
@@ -284,7 +275,7 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 
 		em.close();
 
-		return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+		return SUCCESS;
 	}
 
 	/*
@@ -297,9 +288,8 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
-	public final String display(
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	public final String display()
+			throws Exception {
 		addKeyFacebookInRequest(request);
 		EntityManager em = PersistenceProvider.createEntityManager();
 
@@ -309,15 +299,11 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 		SocialEntity user = UserUtils.getAuthenticatedUser(request, em);
 		Boolean alreadyInContact = false;
 
-		int id = -1;
 		addRightToRequest(request);
 
-		try {
-			String idS = dyna.getString("id");
-			id = Integer.parseInt(idS);
-		} catch (NumberFormatException e) {
+		if(id<0)
 			id = user.getId();
-		}
+		
 
 		SocialEntity profile = sef.getSocialEntity(id);
 
@@ -389,8 +375,7 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 		SocialGroup socialGroup = profile.getGroup();
 		request.setAttribute("socialGroup", socialGroup);
 
-		LoggedUsersContainer loggedCon = (LoggedUsersContainer) getServlet()
-				.getServletContext().getAttribute("loggedUsers");
+		LoggedUsersContainer loggedCon = (LoggedUsersContainer) ServletActionContext.getServletContext().getAttribute("loggedUsers");
 		Map<Integer, String> loggeddd = loggedCon.getUsers();
 
 		if (loggeddd.containsKey(id)) {
@@ -401,17 +386,15 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 
 		em.close();
 		
-		return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+		return SUCCESS;
 	}
 
 	/**
 	 * @param request
 	 * @param key
 	 */
-	private void sendPictureError(HttpServletRequest request, String key) {
-		ActionErrors errors = new ActionErrors();
-		errors.add("photo", new ActionMessage(key));
-		saveErrors(request, errors);
+	private void sendPictureError(String key) {
+		addFieldError("photo", key);
 	}
 
 	/**
@@ -424,50 +407,48 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * @throws ServletException
 	 * @throws URISyntaxException
 	 */
-	public final String changePhoto(ActionMapping mapping,
-			ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException,
+	public final String changePhoto() throws IOException, ServletException,
 			URISyntaxException {
 		EntityManager em = PersistenceProvider.createEntityManager();
 		SocialGroupFacade fascade = new SocialGroupFacade(em);
 		
 		if (!fascade.isAuthorized(UserUtils.getAuthenticatedUser(request, em),
 				Right.MODIFY_PICTURE)) {
-			return new ActionRedirect(mapping.findForward("unauthorized"));
+			return "unauthorized";
 		}
 
-		FormFile file = (FormFile) dynaForm.get("photo");
 		InputStream inputStream = null;
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpGet httpGet = new HttpGet();
 		URI uri = null;
-		String stringUrl = (String) dynaForm.get("photoUrl");
-		dynaForm.set("photoUrl", "");
+		
+		
 		String urlType = null;
 		
-		if (stringUrl != null && !stringUrl.isEmpty()) {
+		if (photoUrl != null && !photoUrl.isEmpty()) {
 			try {
-				uri = new URI(stringUrl);
+				uri = new URI(photoUrl);
 				httpGet.setURI(uri);
 				HttpResponse httpResponse = httpClient.execute(httpGet);
 				inputStream = httpResponse.getEntity().getContent();
 				urlType = httpResponse.getEntity().getContentType().getValue();
 			} catch (Exception e) {
-				sendPictureError(request,
-						"updateProfile.error.photo.invalidlink");
-				return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+				sendPictureError("updateProfile.error.photo.invalidlink");
+				return SUCCESS;
 			}
 		} else {
 			uri = null;
 		}
 
+		photoUrl="";
+		
 		int userId = UserUtils.getAuthenticatesUserId(request);
 		addRightToRequest(request);
 		
-		if (file.getFileData().length != 0) {
+		if (photo.length() != 0) {
 			PictureType pictureType = null;
 			for (PictureType pt : PictureType.values()) {
-				if (pt.getMimeType().equals(file.getContentType())) {
+				if (pt.getMimeType().equals(photoContentType)) {
 					pictureType = pt;
 					break;
 				}
@@ -475,25 +456,24 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 			
 			if (pictureType != null) {
 
-				if (file.getFileSize() > MAX_PICTURE_SIZE) {
-					sendPictureError(request,
-							"updateProfile.error.photo.masize");
-					return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+				if (photo.length() > MAX_PICTURE_SIZE) {
+					sendPictureError("updateProfile.error.photo.masize");
+					return SUCCESS;
 				}
 
 				try {
 					ImageManager.createPicturesForUser(userId,
-							file.getInputStream(), pictureType);
-					return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+							new FileInputStream(photo), pictureType);
+					return SUCCESS;
 				} catch (FileNotFoundException e) {
-					sendPictureError(request, ERROR_UPDATE_ATTRIBUTE_STRING);
+					sendPictureError(ERROR_UPDATE_ATTRIBUTE_STRING);
 				} catch (IOException e) {
-					sendPictureError(request, ERROR_UPDATE_ATTRIBUTE_STRING);
+					sendPictureError(ERROR_UPDATE_ATTRIBUTE_STRING);
 				} catch (IllegalStateException e) {
-					sendPictureError(request, ERROR_UPDATE_ATTRIBUTE_STRING);
+					sendPictureError(ERROR_UPDATE_ATTRIBUTE_STRING);
 				}
 			} else {
-				sendPictureError(request, "updateProfile.error.photo.type");
+				sendPictureError("updateProfile.error.photo.type");
 			}
 		} else if (uri != null) {
 			PictureType pictureType = null;
@@ -507,31 +487,30 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 			if (pictureType != null) {
 
 				if (inputStream.available() > MAX_PICTURE_SIZE) {
-					sendPictureError(request,
-							"updateProfile.error.photo.masize");
-					return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+					sendPictureError("updateProfile.error.photo.masize");
+					return SUCCESS;
 				}
 
 				try {
 					ImageManager.createPicturesForUser(userId, inputStream,
 							pictureType);
 				} catch (FileNotFoundException e) {
-					sendPictureError(request, ERROR_UPDATE_ATTRIBUTE_STRING);
+					sendPictureError(ERROR_UPDATE_ATTRIBUTE_STRING);
 				} catch (IOException e) {
-					sendPictureError(request, ERROR_UPDATE_ATTRIBUTE_STRING);
+					sendPictureError(ERROR_UPDATE_ATTRIBUTE_STRING);
 				} catch (IllegalStateException e) {
-					sendPictureError(request, ERROR_UPDATE_ATTRIBUTE_STRING);
+					sendPictureError(ERROR_UPDATE_ATTRIBUTE_STRING);
 				}
 			} else {
-				sendPictureError(request, "updateProfile.error.photo.type");
+				sendPictureError("updateProfile.error.photo.type");
 			}
 		}
 
 		else {
-			sendPictureError(request, "updateProfile.error.photo.emptylink");
+			sendPictureError("updateProfile.error.photo.emptylink");
 		}
 
-		return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+		return SUCCESS;
 	}
 
 	/**
@@ -543,21 +522,19 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public final String deletePhoto(ActionMapping mapping,
-			ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException {
+	public final String deletePhoto() throws Exception {
 		Integer userId = UserUtils.getAuthenticatesUserId(request);
 		EntityManager em = PersistenceProvider.createEntityManager();
 		SocialGroupFacade fascade = new SocialGroupFacade(em);
 		
 		if (!fascade.isAuthorized(UserUtils.getAuthenticatedUser(request, em),
 				Right.MODIFY_PICTURE)) {
-			return new ActionRedirect(mapping.findForward("unauthorized"));
+			return "unauthorized";
 		}
 
 		ImageManager.removeOldUserPicture(userId);
 		addRightToRequest(request);
-		return mapping.findForward(SUCCES_ATTRIBUTE_NAME);
+		return SUCCESS;
 	}
 
 	/**
@@ -570,5 +547,10 @@ public class ManageProfile extends ActionSupport implements CrudAction {
 		request.setAttribute("rightModifyProfil", rightModifyProfil);
 		request.setAttribute("rightModifyPicture", rightModifyPicture);
 		request.setAttribute("socialEntity", socialEntity);
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request=request;
 	}
 }
