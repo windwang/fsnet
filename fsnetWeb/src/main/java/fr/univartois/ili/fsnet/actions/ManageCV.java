b76a0fe1b65a4074467040f745c9a92294b01e98
Balingon kévin <balingon@gmail.com>
2013-03-04 11:31:18 +0100

    Migration Struts 2 - ManageCV

--- fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageCV.java
+++ fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageCV.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,12 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts.action.ActionErrors;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.String;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.actions.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 import fr.univartois.ili.fsnet.actions.utils.UserUtils;
 import fr.univartois.ili.fsnet.commons.utils.PersistenceProvider;
@@ -45,23 +43,25 @@ import fr.univartois.ili.fsnet.facade.InteractionFacade;
  * @author fsnet
  * 
  */
-public class ManageCV extends ActionSupport {
+public class ManageCV extends ActionSupport implements SessionAware {
 
-	private static final String SUCCESS_ACTION_NAME = "success";
+	private static final long serialVersionUID = 3871072232416409836L;
+
+	private Map<String,Object> mysession ;
+	private String cvTitle;
+	private String cvFirstname;
+	private String cvSurname;
+	private String cvAddress;
+	private String cvCity;
+	private String cvPhone;
+	private String cvCP;
+	private String cvCountry;
+	private String cvMail;
+	private String cvBirthDay;
+	private String cvSituation;
+	private String cvSexe;
+
 	private static final String UNAUTHORIZED_ACTION_NAME = "unauthorized";
-
-	private static final String CV_TITLE_FIELD_FORM_NAME = "cvTitle";
-	private static final String CV_FIRSTNAME_FIELD_FORM_NAME = "cvFirstname";
-	private static final String CV_SURNAME_FIELD_FORM_NAME = "cvSurname";
-	private static final String CV_ADDRESS_FIELD_FORM_NAME = "cvAddress";
-	private static final String CV_CITY_FIELD_FORM_NAME = "cvCity";
-	private static final String CV_PHONE_FIELD_FORM_NAME = "cvPhone";
-	private static final String CV_CP_FIELD_FORM_NAME = "cvCP";
-	private static final String CV_COUNTRY_FIELD_FORM_NAME = "cvCountry";
-	private static final String CV_MAIL_FIELD_FORM_NAME = "cvMail";
-	private static final String CV_BIRTHDAY_FIELD_FORM_NAME = "cvBirthDay";
-	private static final String CV_SITUATION_FIELD_FORM_NAME = "cvSituation";
-	private static final String CV_SEX_FIELD_FORM_NAME = "cvSexe";
 
 	private static final String CV_LANG_NAME_FIELD_FORM_NAME = "cvLangName";
 	private static final String CV_LANG_LEVEL_FIELD_FORM_NAME = "cvLangLevel";
@@ -118,10 +118,9 @@ public class ManageCV extends ActionSupport {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public String cree(
-			HttpServletRequest request, HttpServletResponse response)
+	public String cree(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		return mapping.findForward(SUCCESS_ACTION_NAME);
+		return SUCCESS;
 	}
 
 	/**
@@ -133,129 +132,93 @@ public class ManageCV extends ActionSupport {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public String displayProfile(
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		HttpSession mysession = request.getSession();
-		String cvTitle = (String) dynaForm.get(CV_TITLE_FIELD_FORM_NAME);
-		String cvNom = (String) dynaForm.get(CV_FIRSTNAME_FIELD_FORM_NAME);
-		String cvPrenom = (String) dynaForm.get(CV_SURNAME_FIELD_FORM_NAME);
-		String cvAdresse = (String) dynaForm.get(CV_ADDRESS_FIELD_FORM_NAME);
-		String cvVille = (String) dynaForm.get(CV_CITY_FIELD_FORM_NAME);
-		String cvPortable = (String) dynaForm.get(CV_PHONE_FIELD_FORM_NAME);
-		String cvCp = (String) dynaForm.get(CV_CP_FIELD_FORM_NAME);
-		String cvPays = (String) dynaForm.get(CV_COUNTRY_FIELD_FORM_NAME);
-		String cvContact = (String) dynaForm.get(CV_MAIL_FIELD_FORM_NAME);
-		String birthDay = (String) dynaForm.get(CV_BIRTHDAY_FIELD_FORM_NAME);
-		mysession.setAttribute("action", true);
-		mysession.setAttribute(CV_TITLE_FIELD_FORM_NAME, cvTitle);
-		mysession.setAttribute(CV_FIRSTNAME_FIELD_FORM_NAME, cvNom);
-		mysession.setAttribute(CV_SURNAME_FIELD_FORM_NAME, cvPrenom);
-		mysession.setAttribute(CV_ADDRESS_FIELD_FORM_NAME, cvAdresse);
-		mysession.setAttribute(CV_CITY_FIELD_FORM_NAME, cvVille);
-		mysession.setAttribute(CV_PHONE_FIELD_FORM_NAME, cvPortable);
-		mysession.setAttribute(CV_CP_FIELD_FORM_NAME, cvCp);
-		mysession.setAttribute(CV_COUNTRY_FIELD_FORM_NAME, cvPays);
-		mysession.setAttribute(CV_MAIL_FIELD_FORM_NAME, cvContact);
-		mysession.setAttribute(CV_BIRTHDAY_FIELD_FORM_NAME, birthDay);
-		mysession.setAttribute(CV_SITUATION_FIELD_FORM_NAME,
-				request.getParameter("situation"));
-		mysession.setAttribute(CV_SEX_FIELD_FORM_NAME,
-				request.getParameter("sexe"));
+	public String displayProfile(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+//		HttpSession mysession = request.getSession();
 
-		ActionErrors errors = new ActionErrors();
+		mysession.put("action", true);
+		mysession.put("cvTitle", cvTitle);
+		mysession.put("cvFirstname", cvFirstname);
+		mysession.put("cvSurname", cvSurname);
+		mysession.put("cvAddress", cvAddress);
+		mysession.put("cvCity", cvCity);
+		mysession.put("cvPhone", cvPhone);
+		mysession.put("cvCP", cvCP);
+		mysession.put("cvCountry", cvCountry);
+		mysession.put("cvMail", cvMail);
+		mysession.put("cvBirthDay", cvBirthDay);
+		mysession.put("cvSituation",request.getParameter("situation"));
+		mysession.put("cvSexe",request.getParameter("sexe"));
+
 		int erreur = 0;
 
 		if ("".equals(cvTitle)) {
-			errors.add(CV_TITLE_FIELD_FORM_NAME, new ActionMessage(
-					"error.titre"));
-			saveErrors(request, errors);
+			addFieldError("cvTitle","error.titre");
 			erreur = 1;
 		}
 
-		if ("".equals(cvPortable)) {
-			errors.add(CV_PHONE_FIELD_FORM_NAME, new ActionMessage(
-					"error.portable"));
-			saveErrors(request, errors);
+		if ("".equals(cvPhone)) {
+			addFieldError("cvPhone","error.portable");
 			erreur = 1;
 		}
 
-		if ("".equals(cvNom)) {
-			errors.add(CV_FIRSTNAME_FIELD_FORM_NAME, new ActionMessage(
-					"error.nom"));
-			saveErrors(request, errors);
+		if ("".equals(cvSurname)) {
+			addFieldError("cvSurname","error.nom");
 			erreur = 1;
 		}
 
-		if ("".equals(cvPrenom)) {
-			errors.add(CV_SURNAME_FIELD_FORM_NAME, new ActionMessage(
-					"error.prenom"));
-			saveErrors(request, errors);
+		if ("".equals(cvFirstname)) {
+			addFieldError("cvFirstname","error.prenom");
 			erreur = 1;
 		}
 
-		if ("".equals(cvAdresse)) {
-			errors.add(CV_ADDRESS_FIELD_FORM_NAME, new ActionMessage(
-					"error.adresse"));
-			saveErrors(request, errors);
+		if ("".equals(cvAddress)) {
+			addFieldError("cvAddress","error.adresse");
 			erreur = 1;
 		}
 
-		if ("".equals(cvVille)) {
-			errors.add(CV_CITY_FIELD_FORM_NAME,
-					new ActionMessage("error.ville"));
-			saveErrors(request, errors);
+		if ("".equals(cvCountry)) {
+			addFieldError("cvCountry","error.ville");
 			erreur = 1;
 		}
-		if ("".equals(birthDay)) {
-			errors.add(CV_BIRTHDAY_FIELD_FORM_NAME, new ActionMessage(
-					"error.birthDay"));
-			saveErrors(request, errors);
+		if ("".equals(cvBirthDay)) {
+			addFieldError("cvBirthDay","error.birthDay");
 			erreur = 1;
 		}
 
-		if ("".equals(cvPays)) {
-			errors.add(CV_COUNTRY_FIELD_FORM_NAME, new ActionMessage(
-					"error.pays"));
-			saveErrors(request, errors);
+		if ("".equals(cvCountry)) {
+			addFieldError("cvCountry","error.pays");
 			erreur = 1;
 		}
 
-		if ("".equals(cvContact)) {
-			errors.add(CV_MAIL_FIELD_FORM_NAME, new ActionMessage(
-					"error.contact"));
-			saveErrors(request, errors);
+		if ("".equals(cvMail)) {
+			addFieldError("cvMail","error.contact");
 			erreur = 1;
 		}
 
-		if ("".equals(cvCp)) {
-			errors.add(CV_CP_FIELD_FORM_NAME, new ActionMessage("error.cp"));
-			saveErrors(request, errors);
+		if ("".equals(cvCP)) {
+			addFieldError("cvCP","error.cp");
 			erreur = 1;
 		} else {
 			try {
-				Integer.parseInt(cvCp);
+				Integer.parseInt(cvCP);
 			} catch (Exception e) {
-				errors.add(CV_CP_FIELD_FORM_NAME, new ActionMessage(
-						"error.cpInt"));
-				saveErrors(request, errors);
+				addFieldError("cvCP","error.cpInt");
 				erreur = 1;
 			}
 		}
 
 		try {
-			toDBDateFormat(birthDay);
+			toDBDateFormat(cvBirthDay);
 		} catch (Exception e) {
-			errors.add(CV_BIRTHDAY_FIELD_FORM_NAME, new ActionMessage(
-					"error.birthDayValue"));
-			saveErrors(request, errors);
+			addFieldError("cvBirthDay","error.birthDayValue");
 			erreur = 1;
 		}
 
 		if (erreur == 1) {
-			return mapping.findForward(UNAUTHORIZED_ACTION_NAME);
+			return UNAUTHORIZED_ACTION_NAME;
 		} else {
-			return mapping.findForward(SUCCESS_ACTION_NAME);
+			return SUCCESS;
 		}
 	}
 
@@ -267,9 +230,8 @@ public class ManageCV extends ActionSupport {
 	 * @return
 	 * @throws ParseException
 	 */
-	public String displayExp(
-			HttpServletRequest request, HttpServletResponse response)
-			throws ParseException {
+	public String displayExp(HttpServletRequest request,
+			HttpServletResponse response) throws ParseException {
 		request.getSession().removeAttribute("action");
 		try {
 			int nbExp = Integer.parseInt(request.getParameter("nbExp"));
@@ -288,29 +250,29 @@ public class ManageCV extends ActionSupport {
 			MemberCV member = new MemberCV();
 			try {
 				member.setBirthDate((Date) toDBDateFormat((String) mysession
-						.getAttribute(CV_BIRTHDAY_FIELD_FORM_NAME)));
+						.getAttribute(cvBirthDay)));
 			} catch (Exception e) {
 
 			}
 
 			member.setFirstName((String) mysession
-					.getAttribute(CV_FIRSTNAME_FIELD_FORM_NAME));
+					.getAttribute(cvFirstname));
 			member.setSurname((String) mysession
-					.getAttribute(CV_SURNAME_FIELD_FORM_NAME));
+					.getAttribute(cvSurname));
 			member.setNumberPhone((String) mysession
-					.getAttribute(CV_PHONE_FIELD_FORM_NAME));
+					.getAttribute(cvPhone));
 			member.setMail((String) mysession
-					.getAttribute(CV_MAIL_FIELD_FORM_NAME));
+					.getAttribute(cvMail));
 			member.setTown((String) mysession
-					.getAttribute(CV_COUNTRY_FIELD_FORM_NAME));
+					.getAttribute(cvCountry));
 			member.setAdress((String) mysession
-					.getAttribute(CV_ADDRESS_FIELD_FORM_NAME));
+					.getAttribute(cvAddress));
 			member.setPostCode(Integer.parseInt((String) mysession
-					.getAttribute(CV_CP_FIELD_FORM_NAME)));
+					.getAttribute(cvCP)));
 			member.setSituationFamilly((String) mysession
-					.getAttribute(CV_SITUATION_FIELD_FORM_NAME));
+					.getAttribute(cvSituation));
 			member.setSex((String) mysession
-					.getAttribute(CV_SEX_FIELD_FORM_NAME));
+					.getAttribute(cvSexe));
 
 			// Insert langs
 			int lang = 0;
@@ -329,7 +291,7 @@ public class ManageCV extends ActionSupport {
 			em.persist(member);
 			curriculum.setMember(member);
 			curriculum.setTitleCv((String) mysession
-					.getAttribute(CV_TITLE_FIELD_FORM_NAME));
+					.getAttribute(cvTitle));
 
 			// Insert professional experiences
 			int i = 0;
@@ -457,7 +419,7 @@ public class ManageCV extends ActionSupport {
 					dateDegreeCV.setStartDate(etudBeginDate);
 					dateDegreeCV.setEndDate(etudEndDate);
 				} catch (Exception e) {
-					Logger.getAnonymousLogger().log(Level.WARNING,"", e);
+					Logger.getAnonymousLogger().log(Level.WARNING, "", e);
 				}
 				degree.setStudiesLevel(cvEtude);
 				degree.setDomain(cvEtudeDom);
@@ -501,9 +463,9 @@ public class ManageCV extends ActionSupport {
 
 			//
 
-			return mapping.findForward(SUCCESS_ACTION_NAME);
+			return SUCCESS;
 		} catch (NumberFormatException e) {
-			return mapping.findForward(SUCCESS_ACTION_NAME);
+			return SUCCESS;
 		}
 	}
 
@@ -543,9 +505,8 @@ public class ManageCV extends ActionSupport {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public String displayCv(
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	public String displayCv(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
 
 		EntityManager em = PersistenceProvider.createEntityManager();
 		addRightToRequest(request);
@@ -559,7 +520,7 @@ public class ManageCV extends ActionSupport {
 
 		request.setAttribute("CVsList", results);
 
-		return mapping.findForward(SUCCESS_ACTION_NAME);
+		return SUCCESS;
 	}
 
 	/**
@@ -571,9 +532,8 @@ public class ManageCV extends ActionSupport {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public String delete(
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+	public String delete(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
 		EntityManager em = PersistenceProvider.createEntityManager();
 
 		try {
@@ -584,11 +544,17 @@ public class ManageCV extends ActionSupport {
 			em.remove(curriculum);
 			em.getTransaction().commit();
 		} catch (NumberFormatException e) {
-			return mapping.getInputForward();
+			return INPUT;
 		} finally {
 			em.close();
 		}
 
-		return mapping.getInputForward();
+		return INPUT;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> mysession) {
+		this.mysession = mysession; 
+		
 	}
 }
