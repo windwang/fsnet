46a06cf268f490413729e74b3eccc0dca48df003
fahdfprime@gmail.com <fahdfprime@gmail.com>
2013-03-08 09:59:39 +0000

    Members the form error still need correction

--- fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ajax/Members.java
+++ fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ajax/Members.java
@@ -1,6 +1,5 @@
 package fr.univartois.ili.fsnet.actions.ajax;
 
-
 import java.util.List;
 import java.util.Set;
 
@@ -8,11 +7,9 @@ import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.DynaActionForm;
+import org.apache.struts2.dispatcher.mapper.ActionMapping;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 import fr.univartois.ili.fsnet.actions.utils.UserUtils;
 import fr.univartois.ili.fsnet.commons.utils.PersistenceProvider;
@@ -20,34 +17,53 @@ import fr.univartois.ili.fsnet.entities.SocialEntity;
 import fr.univartois.ili.fsnet.facade.SocialEntityFacade;
 import fr.univartois.ili.fsnet.facade.SocialGroupFacade;
 
-
 /**
- *
+ * 
  * @author Matthieu Proucelle <matthieu.proucelle at gmail.com>
  */
-public class Members extends Action {
-	
-    @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	ActionForward res = null;
-    	if (form != null) {
-            DynaActionForm theform = (DynaActionForm) form; // NOSONAR
-            String actualText = (String) theform.get("searchText");
-            int index = actualText.lastIndexOf(',');
-            String completeUser = (index==-1) ? ("") : (actualText.substring(0, index+1));
-            String searchText = (index==-1) ? (actualText) : (actualText.substring(index+1));
-            EntityManager em = PersistenceProvider.createEntityManager();
-            SocialEntityFacade sef = new SocialEntityFacade(em);
-            SocialGroupFacade sgf = new SocialGroupFacade(em);
-            Set<SocialEntity> listSE = sef.searchSocialEntity(searchText);
-            SocialEntity member = UserUtils.getAuthenticatedUser(request, em);
-            List<SocialEntity> membersWithCurrentMemberCanInteract = sgf.getPersonsWithWhoMemberCanInteract(member);
-            em.close();
-            listSE.retainAll(membersWithCurrentMemberCanInteract);
-            request.setAttribute("matchesSocialEntity", listSE);
-            request.setAttribute("completeUsers", completeUser);
-            res = mapping.findForward("success");
-        }
-        return res;
-    }
+public class Members extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private HttpServletRequest request;
+	private String searchText;
+
+	@Override
+	public String execute() throws Exception {
+		if (form != null) {
+			int index = searchText.lastIndexOf(',');
+			String completeUser = (index == -1) ? ("") : (searchText.substring(
+					0, index + 1));
+			String searchText = (index == -1) ? (searchText) : (searchText
+					.substring(index + 1));
+			EntityManager em = PersistenceProvider.createEntityManager();
+			SocialEntityFacade sef = new SocialEntityFacade(em);
+			SocialGroupFacade sgf = new SocialGroupFacade(em);
+			Set<SocialEntity> listSE = sef.searchSocialEntity(searchText);
+			SocialEntity member = UserUtils.getAuthenticatedUser(request, em);
+			List<SocialEntity> membersWithCurrentMemberCanInteract = sgf
+					.getPersonsWithWhoMemberCanInteract(member);
+			em.close();
+			listSE.retainAll(membersWithCurrentMemberCanInteract);
+			request.setAttribute("matchesSocialEntity", listSE);
+			request.setAttribute("completeUsers", completeUser);
+		}
+		return SUCCESS;
+	}
+
+	/**
+	 * @return the searchText
+	 */
+	public String getSearchText() {
+		return searchText;
+	}
+
+	/**
+	 * @param searchText
+	 *            the searchText to set
+	 */
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
 }
