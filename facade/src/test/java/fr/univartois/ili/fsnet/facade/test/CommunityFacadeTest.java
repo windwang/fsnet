3b58e4ca55cdf66ab5160391cb5dfa2ce0444c6d
Guillaume W <guillaume.waloszek@gmail.com>
2013-03-08 10:19:48 +0100

    tests added for CommunityFacade. Full covered now.

--- facade/src/test/java/fr/univartois/ili/fsnet/facade/test/CommunityFacadeTest.java
+++ facade/src/test/java/fr/univartois/ili/fsnet/facade/test/CommunityFacadeTest.java
@@ -51,6 +51,35 @@ public class CommunityFacadeTest {
 		assertEquals(community.getCreator(), compare.getCreator());
 		assertEquals(community.getTitle(), compare.getTitle());
 	}
+	
+	
+	@Test
+	public void testGetCommunity() {
+		em.getTransaction().begin();
+		SocialEntity creatorCommunity = sef.createSocialEntity(
+				"creatorGetCommunity", "creatorGetCommunity",
+		"creatorGetCommunity@gmail.com");
+		Community community = cf.createCommunity(creatorCommunity,
+		"nameGetCommunity");		
+		em.getTransaction().commit();
+		
+		Community compare = cf.getCommunity(community.getId());
+		assertEquals(community,compare);
+	}
+	
+	@Test
+	public void testGetCommunityByName(){
+		em.getTransaction().begin();
+		SocialEntity creatorCommunity = sef.createSocialEntity(
+				"creatorGetCommunityName", "creatorGetCommunityName",
+		"creatorGetCommunityName@gmail.com");
+		Community community = cf.createCommunity(creatorCommunity,
+		"nameGetCommunityName");		
+		em.getTransaction().commit();
+		
+		Community compare = cf.getCommunityByName(community.getTitle());
+		assertEquals(community,compare);
+	}
 
 	@Test
 	public void testSearch() {
@@ -68,7 +97,22 @@ public class CommunityFacadeTest {
 		assertEquals(community.getCreator(), cRes.getCreator());
 		assertEquals(community.getTitle(), cRes.getTitle());
 	}
+	
 
+	@Test
+	public void testModifyNameOfCommunity(){
+		em.getTransaction().begin();
+		SocialEntity creatorCommunity = sef.createSocialEntity("creatorModify",
+				"communnauteModify", "creatorCommunityModify@gmail.com");
+		Community community = cf.createCommunity(creatorCommunity,
+		"nameCommunityModify");
+		em.getTransaction().commit();
+		em.getTransaction().begin();
+		cf.modifyCommunity("newNameCommunityModify", community);
+		em.getTransaction().commit();
+		assertEquals("newNameCommunityModify",community.getTitle());
+	}
+	
 	@Test
 	public void testDelete1() {
 		em.getTransaction().begin();
@@ -97,4 +141,27 @@ public class CommunityFacadeTest {
 		interactionFacade.deleteInteraction(notcreator, community);
 		em.getTransaction().commit();
 	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSearchCommunityWithNullPattern(){
+		cf.searchCommunity(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testModifyCommunityWithNullName(){
+		cf.modifyCommunity(null, new Community());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testModifyCommunityWithNullCommunity(){
+		cf.modifyCommunity("communaute", null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetCommunityByNameWithNullName(){
+		cf.getCommunityByName(null);
+	}
+	
+	
+
 }
