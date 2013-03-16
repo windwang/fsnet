a504b426dbab8d40a765d5907009c149e060d63a
Marc Leborgne <leborgneprojetsm2@gmail.com>
2013-03-16 19:21:02 +0100

    Merge

--- fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageConsultations.java
+++ fsnetWeb/src/main/java/fr/univartois/ili/fsnet/actions/ManageConsultations.java
@@ -125,7 +125,6 @@ public class ManageConsultations extends ActionSupport {
 			return FAILED_ACTION_NAME;
 		}
 		
-		// END TODO
 		EntityManager em = PersistenceProvider.createEntityManager();
 		SocialEntity member = UserUtils.getAuthenticatedUser(request, em);
 		SocialGroupFacade fascade = new SocialGroupFacade(em);
